7b49f20e69ee2b8dccc2156d9041dcb153a3eae1
nbokhari <najia@digitify.com>
2019-06-25 17:19:54 +0500

    YM-1078 remove comments from activity

--- app/src/main/java/co/yap/app/onboarding/activities/OnboardingActivity.kt
+++ app/src/main/java/co/yap/app/onboarding/activities/OnboardingActivity.kt
@@ -2,7 +2,6 @@ package co.yap.app.onboarding.activities
 
 import android.content.res.Resources
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -18,27 +17,6 @@ class OnboardingActivity : BaseActivity<IBase.ViewModel<IBase.State>>() {
     override val viewModel: IBase.ViewModel<IBase.State>
         get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
 
-//    private lateinit var viewDataBinding: ViewDataBinding
-//
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_onboarding_navigation)
-//
-//        btnBusiness.setOnClickListener {
-//            showToast("start b2b onboarding Flow")
-//        }
-//
-//        btnPersonal.setOnClickListener {
-//            showToast("start b2c onboarding Flow")
-//        }
-//
-//        tvSignIn.setOnClickListener {
-//
-//            showToast("start sign in screen here")
-//        }
-//    }
-
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,7 +29,7 @@ class OnboardingActivity : BaseActivity<IBase.ViewModel<IBase.State>>() {
         val host: NavHostFragment = supportFragmentManager
             .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
 
-         val navController = host.navController
+        val navController = host.navController
 
         appBarConfiguration = AppBarConfiguration(navController.graph)
 
@@ -64,8 +42,6 @@ class OnboardingActivity : BaseActivity<IBase.ViewModel<IBase.State>>() {
             } catch (e: Resources.NotFoundException) {
                 Integer.toString(destination.id)
             }
-
-            Log.d("NavigationActivity", "Navigated to $dest")
         }
     }
 
@@ -74,18 +50,18 @@ class OnboardingActivity : BaseActivity<IBase.ViewModel<IBase.State>>() {
         navController: NavController,
         appBarConfig: AppBarConfiguration
     ) {
-       /*
-         This allows NavigationUI to decide what label to show in the action bar
-         By using appBarConfig, it will also determine whether to
-         show the up arrow or drawer menu icon
-        */
+        /*
+          This allows NavigationUI to decide what label to show in the action bar
+          By using appBarConfig, it will also determine whether to
+          show the up arrow or drawer menu icon
+         */
         setupActionBarWithNavController(navController, appBarConfig)
-     }
+    }
 
 
-     override fun onSupportNavigateUp(): Boolean {
+    override fun onSupportNavigateUp(): Boolean {
         // Allows NavigationUI to support proper up navigation or the drawer layout
         // drawer menu, depending on the situation
         return findNavController(R.id.my_nav_host_fragment).navigateUp(appBarConfiguration)
     }
- }
+}
