7e09252682b302168ff8e11e6541ecb8a1f75350
Sufyan <sufyan@digitify.com>
2019-06-26 12:08:01 +0500

    YM-1131 wip

---
+++ app/src/main/java/co/yap/app/modules/login/activities/LogInActivity.kt
@@ -0,0 +1,15 @@
+package co.yap.app.modules.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.yap.app.R
+import co.yap.yapcore.BaseBindingActivity
+
+class LogInActivity : BaseBindingActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+    }
+}
