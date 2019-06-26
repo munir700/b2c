f80ed96f1beca70ae5ff9f90a9e5e0816bc0d6be
Bilal Shabbir <alienslab02@gmail.com>
2019-06-26 12:13:33 +0500

    YM-1080 new fragment added

--- yapcore/src/main/java/co/yap/yapcore/BaseFragment.kt
+++ yapcore/src/main/java/co/yap/yapcore/BaseFragment.kt
@@ -2,6 +2,9 @@ package co.yap.yapcore
 
 import android.content.Context
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 
@@ -88,4 +91,6 @@ abstract class BaseFragment<V: IBase.ViewModel<*>> : Fragment(), IBase.View<V> {
     override fun requestPermissions() {
         getBaseView()?.requestPermissions()
     }
+
+
 }
