da4a19848a6be2a62e6025f8d09f9934d659e1a1
Bilal Shabbir <alienslab02@gmail.com>
2019-06-27 12:29:47 +0500

    YM-1080 Phone Verification UI complete

--- yap/src/main/java/co/yap/modules/onboarding/fragments/PhoneVerificationFragment.kt
+++ yap/src/main/java/co/yap/modules/onboarding/fragments/PhoneVerificationFragment.kt
@@ -1,28 +1,19 @@
 package co.yap.modules.onboarding.fragments
 
-import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import co.yap.BR
 import co.yap.R
 import co.yap.modules.onboarding.interfaces.IPhoneVerification
 import co.yap.modules.onboarding.viewmodels.PhoneVerificationViewModel
-import co.yap.yapcore.BaseFragment
-import co.yap.yapcore.BaseNavFragment
+import co.yap.yapcore.BaseBindingFragment
 
 
-class PhoneVerificationFragment : BaseFragment<IPhoneVerification.ViewModel>(), IPhoneVerification.View {
+class PhoneVerificationFragment : BaseBindingFragment<IPhoneVerification.ViewModel>(), IPhoneVerification.View {
 
     override val viewModel: IPhoneVerification.ViewModel
         get() = ViewModelProviders.of(this).get(PhoneVerificationViewModel::class.java)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_phone_verification, container, false)
-    }
+    override fun getBindingVariable(): Int = BR.viewModel
 
+    override fun getLayoutId(): Int = R.layout.fragment_phone_verification
 }
