30e36cf2ada8880b589e6caa0ce46efa368c5a22
Sufyan <sufyan@digitify.com>
2019-06-27 18:02:59 +0500

    -> Design touch id permission screen.

---
+++ app/src/main/java/co/yap/app/modules/login/activities/BiometricPermissionActivity.kt
@@ -0,0 +1,30 @@
+package co.yap.app.modules.login.activities
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
+import co.yap.app.R
+import co.yap.app.modules.login.interfaces.IBiometricPermission
+import co.yap.app.modules.login.viewmodels.BiometricPermissionViewModel
+import co.yap.yapcore.BaseBindingActivity
+
+class BiometricPermissionActivity : BaseBindingActivity<IBiometricPermission.ViewModel>(),IBiometricPermission.View {
+    override fun getBindingVariable(): Int =0
+
+    override fun getLayoutId(): Int =R.layout.screen_biometric_permission
+
+    companion object{
+        fun newIntent(context: Context):Intent{
+            val intent=Intent(context,BiometricPermissionActivity::class.java);
+            return intent
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override val viewModel:IBiometricPermission.ViewModel
+    get() = ViewModelProviders.of(this).get(BiometricPermissionViewModel::class.java)
+}
