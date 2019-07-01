a7d6ae7df4d0a88c1de247474fd28ffbf367cb28
Bilal Shabbir <bilal.shabbir@digitify.com>
2019-07-01 15:35:13 +0500

    YM-1076 BaseState binding fix

--- yap/src/main/java/co/yap/modules/onboarding/interfaces/IOnboarding.kt
+++ yap/src/main/java/co/yap/modules/onboarding/interfaces/IOnboarding.kt
@@ -5,5 +5,9 @@ import co.yap.yapcore.IBase
 interface IOnboarding {
     interface View : IBase.View<ViewModel>
     interface ViewModel : IBase.ViewModel<State>
-    interface State : IBase.State
+    interface State : IBase.State {
+        val totalProgress: Int
+            get() = 100
+        var currentProgress: Int
+    }
 }
--- yapcore/src/main/java/co/yap/yapcore/BaseState.kt
+++ yapcore/src/main/java/co/yap/yapcore/BaseState.kt
@@ -5,49 +5,32 @@ import androidx.databinding.Bindable
 
 
 abstract class BaseState : BaseObservable(), IBase.State {
-    private var loading = false
-    private lateinit var error: String
-    private lateinit var toolbarTitle: String
+
+    @get:Bindable
+    override var loading: Boolean = false
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.loading)
+        }
+
+    @get:Bindable
+    override var toolbarTitle: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.toolbarTitle)
+        }
+
+    @get:Bindable
+    override var error: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.error)
+        }
 
     override fun reset() {
         loading = false
         toolbarTitle = ""
         error = ""
-    }
-
-    @Bindable
-    fun setLoading(loading: Boolean) {
-        this.loading = loading
-        notifyPropertyChanged(BR.loading)
-    }
-
-    override fun isLoading(): Boolean {
-        return this.loading
-    }
-
-    fun setToolbarTitle(title: String) {
-        toolbarTitle = title
-        notifyPropertyChanged(BR.toolbarTitle)
-    }
-
-    @Bindable
-    override fun getToolbarTitle(): String {
-        return toolbarTitle
-    }
-
-    override fun getString(key: String): String {
-//        return Translator.getInstance().getString(key)
-        return ""
-    }
-
-    @Bindable
-    override fun getError(): String {
-        return error
-    }
-
-    fun setError(error: String) {
-        this.error = error
-        notifyPropertyChanged(BR.error)
     }
 
     override fun destroy() {
@@ -65,4 +48,5 @@ abstract class BaseState : BaseObservable(), IBase.State {
     override fun pause() {
 
     }
+
 }
