f9280638e00895f775fb6c7e587c2e0bff21f15f
Bilal Shabbir <bilal.shabbir@digitify.com>
2019-07-10 13:41:28 +0500

    YM-1159 Error on dialer pad fixed

--- yapcore/src/main/java/co/yap/yapcore/BaseNavFragment.kt
+++ yapcore/src/main/java/co/yap/yapcore/BaseNavFragment.kt
@@ -2,25 +2,37 @@ package co.yap.yapcore
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavOptions
-import androidx.navigation.Navigation
+import androidx.navigation.*
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.navOptions
 
 abstract class BaseNavFragment : Fragment() {
 
-    private val options = navOptions {
-        anim {
-            enter = R.anim.slide_in_right
-            exit = R.anim.slide_out_left
-            popEnter = R.anim.slide_in_left
-            popExit = R.anim.slide_out_right
-        }
+    val defaultAnimation: AnimBuilder = anim {
+        enter = R.anim.slide_in_right
+        exit = R.anim.slide_out_left
+        popEnter = R.anim.slide_in_left
+        popExit = R.anim.slide_out_right
     }
 
-    protected fun navigate(navFragmentId: Int, args: Bundle? = null, navOptions: NavOptions? = options) {
-        findNavController().navigate(navFragmentId, args, navOptions)
+    val defaultNavOptions = navOptions {
+        anim { defaultAnimation }
     }
+
+    protected fun navigate(destinationId: Int, args: Bundle? = null, navOptions: NavOptions? = defaultNavOptions) {
+        findNavController().navigate(destinationId, args, navOptions)
+
+    }
+
+//    protected fun navigate(destinationId: Int, args: Bundle? = null, optionsBuilder: NavOptions.Builder.() -> Unit?) {
+//        findNavController().navigate(destinationId, args, NavOptions.Builder().apply {
+//            setEnterAnim(defaultAnimation.enter)
+//            setExitAnim(defaultAnimation.exit)
+//            setPopEnterAnim(defaultAnimation.popEnter)
+//            setPopExitAnim(defaultAnimation.popExit)
+//        }.let{
+//            optionsBuilder
+//        }.build())
+//    }
 
     protected fun navigateBack(destinationId: Int = -1, inclusive: Boolean = false) {
         if (destinationId != -1) {
@@ -29,4 +41,7 @@ abstract class BaseNavFragment : Fragment() {
             findNavController().popBackStack()
         }
     }
+
+    fun anim(animBuilder: AnimBuilder.() -> Unit): AnimBuilder = AnimBuilder().apply(animBuilder)
+
 }
