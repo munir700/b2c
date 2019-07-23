88858bfaede9bcae65d0e34af52d20a27a5194e5
nbokhari <najia@digitify.com>
2019-07-23 09:41:40 +0500

    fix validation icons

--- yap/src/main/java/co/yap/modules/onboarding/fragments/MobileFragment.kt
+++ yap/src/main/java/co/yap/modules/onboarding/fragments/MobileFragment.kt
@@ -1,7 +1,6 @@
 package co.yap.modules.onboarding.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -38,68 +37,22 @@ class MobileFragment : OnboardingChildFragment<IMobile.ViewModel>() {
             CountryCodePicker.PhoneNumberValidityChangeListener {
             override fun onValidityChanged(isValidNumber: Boolean) {
                 if (isValidNumber) {
-                    if (etMobileNumber.getText().trim().length >= 7) {
-                        setErrorLayout()
-//                        ccpContainer.setBackgroundResource(R.drawable.bg_round_error_layout)
-//
-//                        etMobileNumber.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.invalid_name, 0)
-//                        tvMobileError.setText("Invalid Number")
-                    } else if (etMobileNumber.text.length == 11) {
-                        etMobileNumber.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.path, 0)
+
+                    if (etMobileNumber.text.length == 11) {
                         //enable btn blue
                         setNormalUI()
+                        etMobileNumber.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.path, 0)
 
                     } else {
-                        etMobileNumber.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+                        setNormalUI()
                         //disable btn grey
                     }
-                    Log.i("tvValidity", "Valid Number")
-
-//                    ccpContainer.isActivated = true
-//                    ccpContainer.setBackgroundResource(R.drawable.bg_round_edit_text)
-//                    tvMobileError.visibility = View.GONE
-//                    ccpContainer.setPadding(
-//                        resources.getDimensionPixelSize(R.dimen.margin_medium),
-//                        0,
-//                        resources.getDimensionPixelSize(R.dimen.margin_medium),
-//                        0
-//                    )
                     setNormalUI()
-
                 } else {
-//                    ccpContainer.isActivated = true
-//                    ccpContainer.setBackgroundResource(R.drawable.bg_round_edit_text)
-//                    ccpContainer.setPadding(
-//                        resources.getDimensionPixelSize(R.dimen.margin_medium),
-//                        0,
-//                        resources.getDimensionPixelSize(R.dimen.margin_medium),
-//                        0
-//                    )
-//                    etMobileNumber.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
                     setNormalUI()
-                    Log.i("tvValidity", "Invalid Number" + etMobileNumber.text.trim().length.toString())
-
-                    if (etMobileNumber.getText().trim().length >= 7) {
-                        Log.i("tvValidity", "Invalid")
-                        Log.i(
-                            "tvValidity",
-                            etMobileNumber.text.trim().toString() + "   " + etMobileNumber.text.trim().length.toString()
-                        )
-                        Log.i(
-                            "tvValidity",
-                            etMobileNumber.text.toString() + "   " + etMobileNumber.text.length.toString()
-                        )
-
+                    if (etMobileNumber.getText().toString().replace(" ", "").trim().length >= 9) {
                         setErrorLayout()
                     } else {
-                        etMobileNumber.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
-
-                        Log.i(
-                            "tvValidity",
-                            "else" + etMobileNumber.text.trim().toString() + "   " + etMobileNumber.text.trim().length.toString()
-                        )
-                        //
-
                         setNormalUI()
                     }
 
