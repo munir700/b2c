2117af6d36dbe770744278fa7abf2a12643fe8cc
nbokhari <najia@digitify.com>
2019-07-29 14:27:59 +0500

    YM-1481 work on creating Base location fragment,WIP

---
+++ yap/src/main/java/co/yap/modules/onboarding/fragments/LocationBaseFragment.kt
@@ -0,0 +1,10 @@
+package co.yap.modules.onboarding.fragments
+
+import co.yap.yapcore.BaseBindingFragment
+import co.yap.yapcore.BaseFragment
+import co.yap.yapcore.defaults.IDefault
+
+
+class LocationBaseFragment /*: BaseBindingFragment<ILocation.ViewModel>(), IDefault.View*/ {
+
+}
