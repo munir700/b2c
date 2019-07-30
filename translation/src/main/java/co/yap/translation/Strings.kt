02472d09f9f322f6932edb34cce30396a2e8064f
nbokhari <najia@digitify.com>
2019-07-30 17:45:38 +0500

    YM-1480 add UI for confirm selected location card layout on map

--- translation/src/main/java/co/yap/translation/Strings.kt
+++ translation/src/main/java/co/yap/translation/Strings.kt
@@ -168,6 +168,8 @@ object Strings {
         "screen_meeting_location_button_change_location"
     const val screen_meeting_location_display_text_selected_subtitle: String =
         "screen_meeting_location_display_text_selected_subtitle"
+    const val screen_meeting_location_button_confirm_selected_location: String =
+        "screen_meeting_location_button_confirm_selected_location"
 
     //Eid Info Review screen
     const val screen_b2c_eid_info_review_display_text_title: String = "screen_b2c_eid_info_review_display_text_title"
