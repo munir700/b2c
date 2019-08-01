a5ab4d6938e025e090d111dfe29f77c54c5fb058
nbokhari <najia@digitify.com>
2019-08-01 11:02:00 +0500

    YM-1478 add details on card on cards UI

--- yap/src/main/java/co/yap/modules/kyc/activities/MapDetailViewActivity.kt
+++ yap/src/main/java/co/yap/modules/kyc/activities/MapDetailViewActivity.kt
@@ -2,24 +2,23 @@ package co.yap.modules.kyc.activities
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.ContextWrapper
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
-import android.graphics.drawable.BitmapDrawable
 import android.location.Location
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import android.widget.Toast
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import co.yap.R
-import co.yap.widgets.photokit.RoundedCornersTransformation
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -40,9 +39,8 @@ import com.google.android.libraries.places.api.net.FetchPhotoRequest
 import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
 import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse
 import com.google.android.libraries.places.api.net.PlacesClient
-import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_maps.*
-import java.io.*
+import java.io.ByteArrayOutputStream
 import java.util.*
 
 
@@ -62,30 +60,15 @@ class MapDetailViewActivity : AppCompatActivity(), OnMapReadyCallback {
     lateinit var mLastKnownLocation: Location
     var animationFrequency: Int = 1                 //can be set to 2000
 
+    var markerSnippet: String = ""
     var placeName: String = ""
-    var pictureURL: String = "abc"
     var placeTitle: String = ""
     var placeSubTitle: String = ""
     var placePhoto: Bitmap? = null
-    var plcaOhotoUri: Uri? = null
+    lateinit var markerOptions: MarkerOptions
 
     companion object {
         fun newIntent(context: Context): Intent = Intent(context, MapDetailViewActivity::class.java)
-
-        //
-
-//
-//        PhotoMetadata
-//        {
-//            attributions = < a href =
-//                "https://maps.google.com/maps/contrib/108951521001893080162/photos" >
-//                        Md Emdadullah < / a >,
-//            height = 1067,
-//            width = 1600,
-//            photoReference = CmRaAAAAf3mAHblb83dfK_4Vy4vnc56wrexiUtSWl_wAVXfuTR6lCpMNhI6ai_TGoZDS1X2WWvXPgjg6k14J7_WhCSqUMWJ8YWSN-Pt6uVNfqxVD-9NVnUSMxpkOfWyyX406PNRaEhDiBWIPJ5D2J0zZfmbnhtGhGhSRy_Bk7ayDnmpxZJjpomw-7LhNbw
-//        }
-
-        //
     }
 
 
@@ -94,6 +77,7 @@ class MapDetailViewActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.activity_maps)
 
         icon = this!!.bitmapDescriptorFromVector(this, R.drawable.ic_pin)!!
+        setUpMarker(mDefaultLocation, placeName, markerSnippet)
 
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
@@ -102,10 +86,10 @@ class MapDetailViewActivity : AppCompatActivity(), OnMapReadyCallback {
         Places.initialize(applicationContext, apiKey)
         placesClient = Places.createClient(this)
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-        Picasso.get()
-            .load(R.drawable.black_white_tile)
-            .resize(90, 90)
-            .transform(RoundedCornersTransformation(10, 0, RoundedCornersTransformation.CornerType.ALL))
+
+        Glide.with(this)
+            .asBitmap().load(R.drawable.black_white_tile)
+            .transforms(CenterCrop(), RoundedCorners(15))
             .into(ivLocationPhoto)
     }
 
@@ -124,20 +108,44 @@ class MapDetailViewActivity : AppCompatActivity(), OnMapReadyCallback {
         getDeviceLocation()
     }
 
+    fun setUpMarker(
+        markerLatLng: LatLng?,
+        placeName: String?,
+        markerSnippet: String?
+    ) {
+        markerOptions = MarkerOptions()
+            .icon(icon)
+             .position(markerLatLng!!)
+
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        mMap.addMarker(MarkerOptions().position(mDefaultLocation).title("Marker in Sydney").icon(icon))
+        mMap.addMarker(markerOptions)
         mMap.uiSettings.isZoomControlsEnabled = false
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM.toFloat()), 1500, null)
+        mMap.uiSettings.isMapToolbarEnabled = false
+        mMap.uiSettings.isCompassEnabled=false
+        mMap.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM.toFloat()),
+            animationFrequency,
+            null
+        )
+
+
         mMap.setOnMapClickListener { point ->
-            Toast.makeText(applicationContext, point.toString(), Toast.LENGTH_SHORT).show()
+            cvLocationCard.visibility = View.VISIBLE
+        }
+
+
+        ivClose.setOnClickListener {
+            cvLocationCard.visibility = View.GONE
+
         }
 
         if (!(::mLastKnownLocation.isInitialized && mLastKnownLocation != null)) {
             getLocationPermission()
             getDeviceLocation()
         }
-
     }
 
 
@@ -209,45 +217,16 @@ class MapDetailViewActivity : AppCompatActivity(), OnMapReadyCallback {
                                 placeSubTitle = markerSnippet
                             }
                             if (!currPlace.photoMetadatas.isNullOrEmpty() && currPlace.photoMetadatas!!.size > 0) {
-                                var photoMetadata: PhotoMetadata = currPlace.getPhotoMetadatas()!!.get(0)
-
-                                ///
-//                                getPhotoz(photoMetadata)
                                 attemptFetchPhoto(currPlace)
-//                                setImageBitmap
                             }
 
                             placeName = currPlace.name!!
                             placeTitle = currPlace.address!!
                             var currentAddress: String = currPlace.address!!
-//                            placeTitle=currPlace.name!!
-//here add picture
-//                            if (null!=plcaOhotoUri) {
-//
-//                                Picasso.get()
-//                                    .load(plcaOhotoUri)
-//
-//                                    .placeholder(R.drawable.black_white_tile)
-//                                    .resize(90, 90)
-//                                    .transform(
-//                                        RoundedCornersTransformation(
-//                                            10,
-//                                            0,
-//                                            RoundedCornersTransformation.CornerType.ALL
-//                                        )
-//                                    )
-//                                    .into(ivLocationPhoto)
-//                            }
-//
 
+                            setUpMarker(markerLatLng!!, placeName, markerSnippet)
+                            mMap.addMarker(markerOptions)
 
-                            mMap.addMarker(
-                                MarkerOptions()
-                                    .icon(icon)
-                                    .title(placeName)
-                                    .position(markerLatLng!!)
-                                    .snippet(markerSnippet)
-                            )
                             mMap.animateCamera(
                                 CameraUpdateFactory.newLatLngZoom(
                                     mDefaultLocation,
@@ -293,93 +272,35 @@ class MapDetailViewActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     private fun fetchPhoto(photoMetadata: PhotoMetadata) {
         var photoMetadata = photoMetadata
-//        photoView!!.setImageBitmap(null)
-//        setLoading(true).............///////////loader
-
-//        val customPhotoReference = customPhotoReference
-//        if (!TextUtils.isEmpty(customPhotoReference)) {
-//            photoMetadata = PhotoMetadata.Builder.build()
-//        }
-
         val photoRequestBuilder = FetchPhotoRequest.builder(photoMetadata)
 
         val photoTask = placesClient!!.fetchPhoto(photoRequestBuilder.build())
 
         photoTask.addOnSuccessListener { response ->
-            //            photoView!!.setImageBitmap(response.bitmap)
-//            StringUtil.prepend(responseView, StringUtil.stringify(response.bitmap))
 
             placePhoto = response.bitmap
-            plcaOhotoUri = bitmapToFile(placePhoto!!)
-            if (null != plcaOhotoUri) {
-//                var drawable: Int = BitmapDrawable(getResources(), placePhoto)
-//                Picasso.get().load(R.drawable.black_white_tile).into(ivLocationPhoto);
-//                val resourceId = data.resourceId
-//                Picasso.get()
-//                    .asBitmap
-//                    .load(R.drawable.black_white_tile)
-//                    .error(R.drawable.black_white_tile)
-//
-//                    .placeholder(drawable)
-//                    .resize(90, 90)
-//                    .transform(
-//                        RoundedCornersTransformation(
-//                            10,
-//                            0,
-//                            RoundedCornersTransformation.CornerType.ALL
-//                        )
-//                    )
-//                    .into(ivLocationPhoto)
 
-//                ivLocationPhoto.setImageBitmap(bitmap
-//                ivLocationPhoto.trans
-            }
-//            plcaOhotoUri= placePhoto?.let { getImageUri(this, it) }
-
-
-//            Glide.with(this)
-//                .load("http://example.com/imageurl")
-//                .Bitmap
-//                .into(ivLocationPhoto)
-
-            Glide.with(this)
-                .asBitmap().load(placePhoto)
-
-                .into(ivLocationPhoto)
+            setUpCardFields()
         }
 
         photoTask.addOnFailureListener { exception ->
             exception.printStackTrace()
-//            StringUtil.prepend(responseView, "Photo: " + exception.message)
-            val strPic = exception.message
+            //hide loader here
         }
 
         photoTask.addOnCompleteListener {
-            //                response -> setLoading(false)
+            //hide loader here
         }
     }
 
-    // Method to save an bitmap to a file
-    private fun bitmapToFile(bitmap: Bitmap): Uri {
-        // Get the context wrapper
-        val wrapper = ContextWrapper(applicationContext)
+    private fun setUpCardFields() {
 
-        // Initialize a new file instance to save bitmap object
-        var file = wrapper.getDir("Images", Context.MODE_PRIVATE)
-        file = File(file, "${UUID.randomUUID()}.jpg")
-
-        try {
-            // Compress the bitmap and save in jpg format
-            val stream: OutputStream = FileOutputStream(file)
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
-            stream.flush()
-            stream.close()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
-        // Return the saved bitmap uri
-        return Uri.parse(file.absolutePath)
+        tvAddressTitle.text = this.placeName
+        tvAddressSubTitle.text = this.placeTitle
+        Glide.with(this)
+            .asBitmap().load(placePhoto)
+            .transforms(CenterCrop(), RoundedCorners(15))
+            .into(ivLocationPhoto)
     }
 
     private fun pickCurrentPlace() {
