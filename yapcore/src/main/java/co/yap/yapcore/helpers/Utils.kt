afa9243bb2da2ab2d7e1339a61d3c31bac3b08b5
Bilal Shabbir <bilal.shabbir@digitify.com>
2019-08-02 12:23:25 +0500

    YM-1428 KYC UI attached with Info

--- yapcore/src/main/java/co/yap/yapcore/helpers/Utils.kt
+++ yapcore/src/main/java/co/yap/yapcore/helpers/Utils.kt
@@ -63,29 +63,5 @@ object Utils {
     }
 
 
-    fun stringToDate(date: String): Date? {
-        val format = SimpleDateFormat("dd-mm-yyyy", Locale.UK)
-        var convertedDate: Date? = null
-        try {
-            convertedDate = format.parse(date)
-            println(date)
-        } catch (e: ParseException) {
-            e.printStackTrace()
-        }
-        return convertedDate
-    }
 
-    fun getAgeFromDate(year: Int, month: Int, day: Int): Int {
-        val dob = Calendar.getInstance()
-        val today = Calendar.getInstance()
-
-        dob.set(year, month, day)
-
-        var age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR)
-
-        if (today.get(Calendar.DAY_OF_YEAR) < dob.get(Calendar.DAY_OF_YEAR)) {
-            age--
-        }
-        return age
-    }
 }
