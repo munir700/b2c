3dcb10c4bb9a41bdb3b49f1f3fc61fed9ae97adc
Bilal Shabbir <bilal.shabbir@digitify.com>
2019-08-09 11:49:00 +0500

    YM-1528 Navigation animation fix in onboarding

--- app/src/main/java/co/yap/app/modules/startup/fragments/AccountSelectionFragment.kt
+++ app/src/main/java/co/yap/app/modules/startup/fragments/AccountSelectionFragment.kt
@@ -1,15 +1,10 @@
 package co.yap.app.modules.startup.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
 import co.yap.app.R
-import co.yap.app.modules.forgotpasscode.activities.ForgotPasscodeActivity
-import co.yap.modules.kyc.activities.DocumentsDashboardActivity
-import co.yap.modules.onboarding.activities.OnboardingActivity
 import co.yap.modules.onboarding.enums.AccountType
 import co.yap.yapcore.BaseBindingFragment
 import co.yap.yapcore.defaults.DefaultViewModel
@@ -28,8 +23,7 @@ class AccountSelectionFragment : BaseBindingFragment<IDefault.ViewModel>(), IDef
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         btnBusiness.setOnClickListener { }
-        btnPersonal.setOnClickListener (
-//            startActivity(Intent(context, ForgotPasscodeActivity::class.java))
+        btnPersonal.setOnClickListener(
             Navigation.createNavigateOnClickListener(
                 R.id.action_accountSelectionFragment_to_welcomeFragment,
                 Bundle().apply { putSerializable(getString(R.string.arg_account_type), AccountType.B2C_ACCOUNT) })
