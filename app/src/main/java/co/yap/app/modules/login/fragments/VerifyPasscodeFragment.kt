04d6205b9dbfd5fcbf7e46e6259a3b736efe2413
hassanakhtar <hassan@digitify.com>
2019-08-22 14:03:05 +0500

    YM-1529 Bug Fix 1633

--- app/src/main/java/co/yap/app/modules/login/fragments/VerifyPasscodeFragment.kt
+++ app/src/main/java/co/yap/app/modules/login/fragments/VerifyPasscodeFragment.kt
@@ -76,15 +76,22 @@ class VerifyPasscodeFragment : BaseBindingFragment<IVerifyPasscode.ViewModel>(),
     override fun setObservers() {
         viewModel.forgotPasscodeButtonPressEvent.observe(this, Observer {
             when (it) {
-               R.id.tvForgotPassword->{
-                   val action =
-                       VerifyPasscodeFragmentDirections.actionVerifyPasscodeFragmentToForgotPasscodeNavigation(
-                           viewModel.state.username,
-                           viewModel.emailOtp,
-                           viewModel.mobileNumber
-                       )
-                   findNavController().navigate(action)
-               }
+                R.id.tvForgotPassword -> {
+                    if (sharedPreferenceManager.getValueBoolien(SharedPreferenceManager.KEY_IS_USER_LOGGED_IN, false)) {
+                        viewModel.state.username = EncryptionUtils.decrypt(
+                            context as MainActivity,
+                            sharedPreferenceManager.getValueString(SharedPreferenceManager.KEY_USERNAME) as String
+                        ) as String
+                    }
+
+                    val action =
+                        VerifyPasscodeFragmentDirections.actionVerifyPasscodeFragmentToForgotPasscodeNavigation(
+                            viewModel.state.username,
+                            viewModel.emailOtp,
+                            viewModel.mobileNumber
+                        )
+                    findNavController().navigate(action)
+                }
             }
         })
     }
@@ -140,12 +147,18 @@ class VerifyPasscodeFragment : BaseBindingFragment<IVerifyPasscode.ViewModel>(),
                     && BiometricUtil.isPermissionGranted(context as MainActivity)
                     && BiometricUtil.isFingerprintAvailable(context as MainActivity)
                 ) {
-                    val action = VerifyPasscodeFragmentDirections.actionVerifyPasscodeFragmentToSystemPermissionFragment(Constants.TOUCH_ID_SCREEN_TYPE)
+                    val action =
+                        VerifyPasscodeFragmentDirections.actionVerifyPasscodeFragmentToSystemPermissionFragment(
+                            Constants.TOUCH_ID_SCREEN_TYPE
+                        )
                     findNavController().navigate(action)
                     sharedPreferenceManager.save(SharedPreferenceManager.KEY_IS_FINGERPRINT_PERMISSION_SHOWN, true)
                 } else {
                     sharedPreferenceManager.save(SharedPreferenceManager.KEY_IS_FINGERPRINT_PERMISSION_SHOWN, true)
-                    val action = VerifyPasscodeFragmentDirections.actionVerifyPasscodeFragmentToSystemPermissionFragment(Constants.NOTIFICATION_SCREEN_TYPE)
+                    val action =
+                        VerifyPasscodeFragmentDirections.actionVerifyPasscodeFragmentToSystemPermissionFragment(
+                            Constants.NOTIFICATION_SCREEN_TYPE
+                        )
                     findNavController().navigate(action)
                 }
 
@@ -159,12 +172,15 @@ class VerifyPasscodeFragment : BaseBindingFragment<IVerifyPasscode.ViewModel>(),
     }
 
     private val createOtpObserver = Observer<Boolean> {
-        val action = VerifyPasscodeFragmentDirections.actionVerifyPasscodeFragmentToPhoneVerificationSignInFragment(viewModel.state.username,  viewModel.state.passcode)
+        val action = VerifyPasscodeFragmentDirections.actionVerifyPasscodeFragmentToPhoneVerificationSignInFragment(
+            viewModel.state.username,
+            viewModel.state.passcode
+        )
         findNavController().navigate(action)
     }
 
     private fun setUsername() {
-         viewModel.state.username = arguments?.let { VerifyPasscodeFragmentArgs.fromBundle(it).username } as String
+        viewModel.state.username = arguments?.let { VerifyPasscodeFragmentArgs.fromBundle(it).username } as String
     }
 
 
