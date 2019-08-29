1f2af7c1733320a83974d9ead9d5b09e25ae42c4
hassanakhtar <hassan@digitify.com>
2019-08-29 18:21:02 +0500

    YM-1701 Notification scrolling functionality added

---
+++ yap/src/main/java/co/yap/modules/dashboard/adapters/NotificationAdapter.kt
@@ -0,0 +1,40 @@
+package co.yap.modules.dashboard.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import co.yap.R
+import co.yap.modules.dashboard.models.Notification
+import kotlinx.android.synthetic.main.view_notifications.view.*
+
+
+class NotificationAdapter(private val listItems: ArrayList<Notification>, val context: Context) :
+    RecyclerView.Adapter<NotificationAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        val v = LayoutInflater.from(p0?.context).inflate(R.layout.view_notifications, p0, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return listItems.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val notification: Notification = listItems[position]
+
+        holder.tvTitle.text = notification.title
+        holder.tvDescription.text = notification.description
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val ivNotification: ImageView = itemView.ivNotification
+        val ivCross: ImageView = itemView.ivCross
+        val tvTitle: TextView = itemView.tvTitle
+        val tvDescription: TextView = itemView.tvDescription
+    }
+}
