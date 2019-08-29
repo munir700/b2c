5aafe9d66b45348326643fc97c398f94ff1eb9b1
Najia Bokhari <najia@digitify.com>
2019-08-29 11:47:23 +0500

    YM-1703 add on graphh bar item touch listener

--- yap/src/main/java/co/yap/modules/dashboard/fragments/YapHomeFragment.kt
+++ yap/src/main/java/co/yap/modules/dashboard/fragments/YapHomeFragment.kt
@@ -1,10 +1,12 @@
 package co.yap.modules.dashboard.fragments
 
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import co.yap.BR
 import co.yap.R
 import co.yap.modules.dashboard.adapters.DashboardAdapter
@@ -15,7 +17,10 @@ import co.yap.yapcore.BaseBindingFragment
 import kotlinx.android.synthetic.main.fragment_yap_home.*
 import kotlinx.android.synthetic.main.view_graph.*
 
-class YapHomeFragment : BaseBindingFragment<IYapHome.ViewModel>(), IYapHome.View {
+
+class YapHomeFragment : BaseBindingFragment<IYapHome.ViewModel>(), IYapHome.View,
+    RecyclerView.OnItemTouchListener {
+
 
     private var transactionAdapter: TransactionAdapter? = null
 
@@ -53,5 +58,53 @@ class YapHomeFragment : BaseBindingFragment<IYapHome.ViewModel>(), IYapHome.View
                 false
             )
         )
+
+        rvTransactionsBarChart.setOnTouchListener(object : RecyclerView.OnItemTouchListener,
+            View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                when (event!!.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        rvTransaction.smoothScrollToPosition((viewModel.loadJSONDummyList().size - 1))
+                    }
+                }
+                return true
+            }
+
+            override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
+                when (e!!.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        rvTransaction.smoothScrollToPosition((viewModel.loadJSONDummyList().size - 1))
+                    }
+                }
+            }
+
+            override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+                return true
+            }
+
+            override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+            }
+
+        })
+    }
+
+    override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+        when (e!!.action) {
+            MotionEvent.ACTION_DOWN -> {
+                rvTransaction.smoothScrollToPosition((viewModel.loadJSONDummyList().size - 1))
+            }
+        }
+        return true
+    }
+
+    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+    }
+
+    override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
+        when (e!!.action) {
+            MotionEvent.ACTION_DOWN -> {
+                rvTransaction.smoothScrollToPosition((viewModel.loadJSONDummyList().size - 1))
+            }
+        }
     }
 }
