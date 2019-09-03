f5dafe0324d59f9e50534abb7ebd3a63bbf70122
Sufyan <sufyan@digitify.com>
2019-09-03 10:09:52 +0500

    -> Update transaction list with color and + sign -> hide + sign from drawer -> hide cross sign from notification UI

--- yap/src/main/java/co/yap/modules/dashboard/adapters/TransactionsListingAdapter.kt
+++ yap/src/main/java/co/yap/modules/dashboard/adapters/TransactionsListingAdapter.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import co.yap.R
 import co.yap.modules.dashboard.models.Transaction
+import co.yap.yapcore.helpers.Utils
 import kotlinx.android.synthetic.main.item_transaction_list.view.*
 
 
@@ -32,12 +34,28 @@ class TransactionsListingAdapter(
 
     override fun onBindViewHolder(holder: TransactionsListingAdapter.ViewHolder, position: Int) {
         val transaction: Transaction = transactionsList[position]
-
+        if (transaction.type == "Credit") {
+            holder.tvTransactionAmount?.setTextColor(
+                ContextCompat.getColor(
+                    context,
+                    R.color.colorSecondaryGreen
+                )
+            )
+            holder.tvTransactionAmount?.text = "+" + Utils.getFormattedCurrency(transaction.amount)
+        } else if (transaction.type == "Debit") {
+            holder.tvTransactionAmount?.setTextColor(
+                ContextCompat.getColor(
+                    context,
+                    R.color.colorPrimaryDark
+                )
+            )
+            holder.tvTransactionAmount?.text = "-" + Utils.getFormattedCurrency(transaction.amount)
+        }
         holder.tvTransactionName?.text = transaction.vendor
-        holder.tvNameInitials?.text = shortName(transaction.vendor)
+        holder.tvNameInitials?.text = shortName(transaction.vendor.toUpperCase())
         holder.tvTransactionTime?.text = transaction.time
         holder.tvTransactionCategory?.text = transaction.category
-        holder.tvTransactionAmount?.text = transaction.amount
+        // holder.tvTransactionAmount?.text = transaction.amount
         holder.tvCurrency?.text = transaction.currency
     }
 
