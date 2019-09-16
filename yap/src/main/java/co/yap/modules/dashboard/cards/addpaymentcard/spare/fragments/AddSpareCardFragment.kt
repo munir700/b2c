34680e3418e200faa7921f049f5a94a3e9e35e2a
Najia Bokhari <najia@digitify.com>
2019-09-16 15:06:16 +0500

    YM-1894 refactor Add spare card fragment

--- yap/src/main/java/co/yap/modules/dashboard/cards/addpaymentcard/spare/fragments/AddSpareCardFragment.kt
+++ yap/src/main/java/co/yap/modules/dashboard/cards/addpaymentcard/spare/fragments/AddSpareCardFragment.kt
@@ -12,6 +12,7 @@ import co.yap.modules.dashboard.cards.addpaymentcard.spare.interfaces.IAddSpareC
 import co.yap.modules.dashboard.cards.addpaymentcard.spare.viewmodels.AddSpareCardViewModel
 import com.daimajia.androidanimations.library.Techniques
 import com.daimajia.androidanimations.library.YoYo
+import kotlinx.android.synthetic.main.fragment_add_spare_card.*
 import kotlinx.android.synthetic.main.layout_add_spare_virtaul_card_confirm_purchase.*
 import kotlinx.android.synthetic.main.layout_add_spare_virtual_card_success.*
 
@@ -30,6 +31,12 @@ class AddSpareCardFragment : AddPaymentChildFragment<IAddSpareCard.ViewModel>(),
             arguments?.let { AddSpareCardFragmentArgs.fromBundle(it).cardType } as String
 
         viewModel.state.cardType = viewModel.cardType
+        if (viewModel.cardType.equals(getString(R.string.screen_spare_card_landing_display_text_virtual_card))){
+            layoutVirtualCardConfirmPurchase.visibility=View.VISIBLE
+        }else if (viewModel.cardType.equals(getString(R.string.screen_spare_card_landing_display_text_physical_card))){
+            layoutVirtualCardConfirmPurchase.visibility=View.GONE
+        }
+
         fadeOutView(tvTransactionComplete)
         fadeOutView(flTransactionComplete)
         fadeOutView(btnDoneAddingSpareVirtualCard)
@@ -44,15 +51,11 @@ class AddSpareCardFragment : AddPaymentChildFragment<IAddSpareCard.ViewModel>(),
 
                 }
                 R.id.btnDoneAddingSpareVirtualCard -> {
-                     onBackPressed()
+                    onBackPressed()
                 }
                 R.id.btnConfirmVirtualCardPurchase -> {
-// hide tha success layout or kill this fragment and go back
-
-
                     llConfirmVirtualCardPurchase.visibility = View.GONE
                     layoutVirtualCardOnSuccess.visibility = View.VISIBLE
-//                    tvTransactionComplete.visibility = View.VISIBLE
                     YoYo.with(Techniques.FadeOut)
                         .withListener(object : Animator.AnimatorListener {
                             override fun onAnimationStart(animation: Animator?) {
@@ -72,14 +75,7 @@ class AddSpareCardFragment : AddPaymentChildFragment<IAddSpareCard.ViewModel>(),
                         .duration(50)
                         .repeat(0)
                         .playOn(llConfirmVirtualCardPurchase)
-
-                    // now remove old include layout_add_spare_virtualcard_confirm_purchase : hide llConfirmVirtualCardPurchase
-                    //then animate IN new layout layout_add_spare_virtual_card_success  : show layoutVirtualCardOnSuccess
-
-
                 }
-
-
             }
         })
 
@@ -96,7 +92,6 @@ class AddSpareCardFragment : AddPaymentChildFragment<IAddSpareCard.ViewModel>(),
 
                 override fun onAnimationEnd(animation: Animator?) {
                     FadeInSuccessImage()
-//                    slideInDownTitle()
                 }
 
                 override fun onAnimationCancel(animation: Animator?) {
@@ -131,7 +126,6 @@ class AddSpareCardFragment : AddPaymentChildFragment<IAddSpareCard.ViewModel>(),
 
 
     fun FadeInSuccessImage() {
-//        flTransactionComplete.visibility=View.VISIBLE
 
         YoYo.with(Techniques.ZoomIn)
             .withListener(object : Animator.AnimatorListener {
@@ -142,7 +136,6 @@ class AddSpareCardFragment : AddPaymentChildFragment<IAddSpareCard.ViewModel>(),
                 }
 
                 override fun onAnimationEnd(animation: Animator?) {
-//                    slideInDownImage()
                     slideInBtn()
                 }
 
@@ -175,11 +168,9 @@ class AddSpareCardFragment : AddPaymentChildFragment<IAddSpareCard.ViewModel>(),
             .repeat(0)
             .playOn(flTransactionComplete)
     }
-//
 
 
     fun slideInBtn() {
-//        btnDoneAddingSpareVirtualCard.visibility=View.VISIBLE
 
         YoYo.with(Techniques.SlideInUp)
             .withListener(object : Animator.AnimatorListener {
@@ -190,7 +181,6 @@ class AddSpareCardFragment : AddPaymentChildFragment<IAddSpareCard.ViewModel>(),
                 }
 
                 override fun onAnimationEnd(animation: Animator?) {
-//                    slideInDownBtn()
                 }
 
                 override fun onAnimationCancel(animation: Animator?) {
@@ -201,26 +191,6 @@ class AddSpareCardFragment : AddPaymentChildFragment<IAddSpareCard.ViewModel>(),
             .playOn(btnDoneAddingSpareVirtualCard)
     }
 
-    fun slideInDownBtn() {
-        YoYo.with(Techniques.SlideInDown)
-            .withListener(object : Animator.AnimatorListener {
-                override fun onAnimationStart(animation: Animator?) {
-                }
-
-                override fun onAnimationRepeat(animation: Animator?) {
-                }
-
-                override fun onAnimationEnd(animation: Animator?) {
-//                    FadeInSuccessImage()
-                }
-
-                override fun onAnimationCancel(animation: Animator?) {
-                }
-            })
-            .duration(1000)
-            .repeat(0)
-            .playOn(btnDoneAddingSpareVirtualCard)
-    }
 
     fun fadeOutView(view: View) {
         YoYo.with(Techniques.FadeOut)
