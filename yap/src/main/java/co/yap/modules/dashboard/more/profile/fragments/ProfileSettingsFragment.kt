fc4ce5133e2f079cc644a1e3326a22108ac55e3c
Najia Bokhari <najia@digitify.com>
2019-09-30 12:02:02 +0500

    YM-2217 add click listener on profile settings screen

--- yap/src/main/java/co/yap/modules/dashboard/more/profile/fragments/ProfileSettingsFragment.kt
+++ yap/src/main/java/co/yap/modules/dashboard/more/profile/fragments/ProfileSettingsFragment.kt
@@ -2,13 +2,14 @@ package co.yap.modules.dashboard.more.profile.fragments
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import co.yap.BR
 import co.yap.R
 import co.yap.modules.dashboard.cards.addpaymentcard.viewmodels.AddPaymentCardViewModel
 import co.yap.modules.dashboard.more.fragments.MoreBaseFragment
 import co.yap.modules.dashboard.more.profile.intefaces.IProfile
-import co.yap.modules.dashboard.more.profile.viewmodels.ProfileViewModel
+import co.yap.modules.dashboard.more.profile.viewmodels.ProfileSettingsViewModel
 import co.yap.yapcore.helpers.SharedPreferenceManager
 
 class ProfileSettingsFragment : MoreBaseFragment<IProfile.ViewModel>(), IProfile.View {
@@ -18,7 +19,7 @@ class ProfileSettingsFragment : MoreBaseFragment<IProfile.ViewModel>(), IProfile
     override fun getLayoutId(): Int = R.layout.fragment_profile
 
     override val viewModel: IProfile.ViewModel
-        get() = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+        get() = ViewModelProviders.of(this).get(ProfileSettingsViewModel::class.java)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,17 +36,38 @@ class ProfileSettingsFragment : MoreBaseFragment<IProfile.ViewModel>(), IProfile
                 .state.tootlBarTitle = "Profile"
         }
 
+        viewModel.clickEvent.observe(this, Observer {
+            when (it) {
 
-    }
+                R.id.tvPersonalDetailView -> {
+//                    findNavController().navigate(R.string.screen_spare_card_landing_display_text_physical_card)
+                }
 
-    override fun onPause() {
-//        viewModel.clickEvent.removeObservers(this)
-        super.onPause()
+                R.id.tvPrivacyView -> {
+
+                }
+                R.id.tvChangePasscode -> {
+
+                }
+                R.id.tvTermsAndConditionView -> {
+
+                }
+                R.id.tvFollowOnInstagram -> {
+
+                }
+                R.id.tvFollowOnTwitter -> {
+
+                }
+                R.id.tvLikeUsOnFaceBook -> {
+
+                }
+            }
+        })
 
     }
 
     override fun onDestroy() {
-//        viewModel.clickEvent.removeObservers(this)
+        viewModel.clickEvent.removeObservers(this)
         super.onDestroy()
     }
 }
--- yap/src/main/java/co/yap/modules/dashboard/more/profile/intefaces/IProfile.kt
+++ yap/src/main/java/co/yap/modules/dashboard/more/profile/intefaces/IProfile.kt
@@ -1,13 +1,41 @@
 package co.yap.modules.dashboard.more.profile.intefaces
 
 import co.yap.yapcore.IBase
+import co.yap.yapcore.SingleClickEvent
 
 interface IProfile {
     interface State : IBase.State {
     }
 
     interface ViewModel : IBase.ViewModel<State> {
+
+        val clickEvent: SingleClickEvent
+
         fun handlePressOnBackButton()
+
+        fun handlePressOnPersonalDetail(id: Int)
+
+        fun handlePressOnPrivacy(id: Int)
+
+        fun handlePressOnPasscode(id: Int)
+
+        fun handlePressOnAppNotification(id: Int)
+
+        fun handlePressOnTermsAndConditions(id: Int)
+
+        fun handlePressOnInstagram(id: Int)
+
+        fun handlePressOnTwitter(id: Int)
+
+        fun handlePressOnFaceBook(id: Int)
+
+        fun handlePressOnLogOut(id: Int)
+
+        fun handlePressOnAddNewPhoto(id: Int)
+
+        fun handlePressOnPhoto(id: Int)
+
+
     }
 
     interface View : IBase.View<ViewModel>
