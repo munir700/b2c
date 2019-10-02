1cb86d2d207de58f4c1bf35b4a6f15eee9e451c8
Najia Bokhari <najia@digitify.com>
2019-10-02 10:20:35 +0500

    YM-2222 add logout alert

--- yap/src/main/java/co/yap/modules/dashboard/more/profile/fragments/ProfileSettingsFragment.kt
+++ yap/src/main/java/co/yap/modules/dashboard/more/profile/fragments/ProfileSettingsFragment.kt
@@ -14,6 +14,9 @@ import co.yap.modules.dashboard.constants.Constants
 import co.yap.modules.dashboard.more.fragments.MoreBaseFragment
 import co.yap.modules.dashboard.more.profile.intefaces.IProfile
 import co.yap.modules.dashboard.more.profile.viewmodels.ProfileSettingsViewModel
+import co.yap.networking.cards.responsedtos.CardBalance
+import co.yap.yapcore.helpers.AuthUtils
+import co.yap.yapcore.managers.MyUserManager
 
 class ProfileSettingsFragment : MoreBaseFragment<IProfile.ViewModel>(), IProfile.View,
     CardClickListener {
@@ -108,12 +111,11 @@ class ProfileSettingsFragment : MoreBaseFragment<IProfile.ViewModel>(), IProfile
 
     fun logoutAlert() {
         AlertDialog.Builder(this!!.activity!!)
-            .setIcon(android.R.drawable.ic_dialog_alert)
             .setTitle(getString(R.string.screen_profile_settings_logout_display_text_alert_title))
             .setMessage(getString(R.string.screen_profile_settings_logout_display_text_alert_message))
             .setPositiveButton(getString(R.string.screen_profile_settings_logout_display_text_alert_logout),
                 DialogInterface.OnClickListener { dialog, which ->
-
+                    doLogout()
                 })
 
             .setNegativeButton(
@@ -123,4 +125,10 @@ class ProfileSettingsFragment : MoreBaseFragment<IProfile.ViewModel>(), IProfile
             .show()
     }
 
+    private fun doLogout() {
+        AuthUtils.navigateToHardLogin(requireContext())
+        MyUserManager.cardBalance.value = CardBalance()
+        MyUserManager.cards.value?.clear()
+        activity?.finish()
+    }
 }
