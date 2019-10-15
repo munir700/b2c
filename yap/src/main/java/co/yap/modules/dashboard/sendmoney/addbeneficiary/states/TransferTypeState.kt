cd07a1e93f5392a02eecc500bdcabb8197c8db2b
Najia Bokhari <najia@digitify.com>
2019-10-15 15:20:51 +0500

    add structure for transfer type

---
+++ yap/src/main/java/co/yap/modules/dashboard/sendmoney/addbeneficiary/states/TransferTypeState.kt
@@ -0,0 +1,8 @@
+package co.yap.modules.dashboard.sendmoney.addbeneficiary.states
+
+import co.yap.modules.dashboard.sendmoney.addbeneficiary.interfaces.ITransferType
+import co.yap.yapcore.BaseState
+
+class TransferTypeState : BaseState(), ITransferType.State  {
+
+}
---
+++ yap/src/main/java/co/yap/modules/dashboard/sendmoney/addbeneficiary/viewmodels/TransferTypeViewModel.kt
@@ -0,0 +1,31 @@
+package co.yap.modules.dashboard.sendmoney.addbeneficiary.viewmodels
+
+import android.app.Application
+import co.yap.modules.dashboard.sendmoney.addbeneficiary.interfaces.ITransferType
+import co.yap.modules.dashboard.sendmoney.addbeneficiary.states.TransferTypeState
+import co.yap.modules.dashboard.sendmoney.viewmodels.SendMoneyBaseViewModel
+import co.yap.networking.customers.CustomersRepository
+import co.yap.networking.interfaces.IRepositoryHolder
+import co.yap.translation.Strings
+import co.yap.yapcore.SingleClickEvent
+
+class TransferTypeViewModel(application: Application) :
+    SendMoneyBaseViewModel<ITransferType.State>(application), ITransferType.ViewModel,
+    IRepositoryHolder<CustomersRepository> {
+
+    override val repository: CustomersRepository = CustomersRepository
+
+    override val state: TransferTypeState = TransferTypeState()
+
+    override var clickEvent: SingleClickEvent = SingleClickEvent()
+
+    override fun handlePressOnType(id: Int) {
+        clickEvent.setValue(id)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setToolBarTitle(getString(Strings.screen_add_beneficiary_display_text_title))
+        toggleAddButtonVisibility(false)
+    }
+}
