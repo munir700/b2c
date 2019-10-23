e1efbbabac2a218bbab0cef90c81444fc6c0cbcb
sufyanshabbir <sufyan@digitify.com>
2019-10-23 09:20:04 +0500

    YM - 2523 Design amount screen and implement business logic.

--- yap/src/main/java/co/yap/modules/yapit/y2ytransfer/transfer/fragments/Y2YTransferFragment.kt
+++ yap/src/main/java/co/yap/modules/yapit/y2ytransfer/transfer/fragments/Y2YTransferFragment.kt
@@ -1,6 +1,10 @@
 package co.yap.modules.yapit.y2ytransfer.transfer.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
+import android.view.Gravity
 import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -8,9 +12,15 @@ import co.yap.R
 import co.yap.modules.yapit.y2ytransfer.fragments.Y2YBaseFragment
 import co.yap.modules.yapit.y2ytransfer.transfer.interfaces.IY2YTransfer
 import co.yap.modules.yapit.y2ytransfer.transfer.viewmodels.Y2YTransferViewModel
+import co.yap.translation.Strings
 import co.yap.yapcore.BR
+import co.yap.yapcore.helpers.CustomSnackbar
+import co.yap.yapcore.helpers.DecimalDigitsInputFilter
+import co.yap.yapcore.helpers.Utils
+import kotlinx.android.synthetic.main.fragment_yap_to_yap_transfer.*
 
-class Y2YTransferFragment : Y2YBaseFragment<IY2YTransfer.ViewModel>() {
+class Y2YTransferFragment : Y2YBaseFragment<IY2YTransfer.ViewModel>(), IY2YTransfer.View {
+
     override fun getBindingVariable(): Int = BR.viewModel
     override fun getLayoutId(): Int = R.layout.fragment_yap_to_yap_transfer
 
@@ -18,16 +28,61 @@ class Y2YTransferFragment : Y2YBaseFragment<IY2YTransfer.ViewModel>() {
         get() = ViewModelProviders.of(this).get(Y2YTransferViewModel::class.java)
 
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-       // viewModel.clickEvent.observe(this, clickEventObserver)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setObservers()
     }
 
-    private val clickEventObserver = Observer<Int> {
-        when (it) {
-            R.id.btnInvite -> {
-                showToast("Invitie Button Clicked")
-            }
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpData()
     }
+
+    override fun setObservers() {
+        viewModel.clickEvent.observe(this, Observer {
+            showToast("success state")
+        })
+        viewModel.errorEvent.observe(this, Observer {
+            showErrorSnackBar()
+        })
+
+
+    }
+
+    private fun setUpData() {
+
+        viewModel.state.availableBalanceText =
+            " " + getString(Strings.common_text_currency_type) + " " + Utils.getFormattedCurrency(
+                viewModel.state.availableBalance
+            )
+        etAmount.filters =
+            arrayOf<InputFilter>(InputFilter.LengthFilter(7), DecimalDigitsInputFilter(2))
+        etAmount.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (p0?.length!! > 0) {
+                    // puts the caret after the text when unempty
+                    etAmount.setGravity(Gravity.CENTER)
+                } else {
+                    etAmount.setGravity(Gravity.START or Gravity.CENTER_VERTICAL)
+                }
+            }
+        })
+    }
+
+    private fun showErrorSnackBar() {
+        CustomSnackbar.showErrorCustomSnackbar(
+            context = requireContext(),
+            layout = clFTSnackbar,
+            message = viewModel.state.errorDescription
+        )
+    }
+
+
 }
--- yap/src/main/java/co/yap/modules/yapit/y2ytransfer/transfer/states/Y2YTransferState.kt
+++ yap/src/main/java/co/yap/modules/yapit/y2ytransfer/transfer/states/Y2YTransferState.kt
@@ -1,7 +1,134 @@
 package co.yap.modules.yapit.y2ytransfer.transfer.states
 
+import android.app.Application
+import android.content.Context
+import android.graphics.drawable.Drawable
+import androidx.databinding.Bindable
+import co.yap.BR
 import co.yap.modules.yapit.y2ytransfer.transfer.interfaces.IY2YTransfer
+import co.yap.translation.Strings
+import co.yap.translation.Translator
 import co.yap.yapcore.BaseState
+import co.yap.yapcore.helpers.Utils
 
-class Y2YTransferState : BaseState() , IY2YTransfer.State{
+class Y2YTransferState(application: Application) : BaseState() , IY2YTransfer.State{
+
+    val context:Context=application.applicationContext
+   @get:Bindable
+    override var amountBackground: Drawable? =
+        context.resources.getDrawable(co.yap.yapcore.R.drawable.bg_funds, null)
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.amountBackground)
+        }
+
+    @get:Bindable
+    override var amount: String? = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.amount)
+            clearError()
+        }
+
+    @get:Bindable
+    override var valid: Boolean = false
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.valid)
+        }
+    @get:Bindable
+    override var minLimit: Double = 0.00
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.minLimit)
+        }
+
+    @get:Bindable
+    override var availableBalance: String = "500"
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.availableBalance)
+        }
+
+    @get:Bindable
+    override var errorDescription: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.errorDescription)
+        }
+    @get:Bindable
+    override var currencyType: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.currencyType)
+        }
+
+    @get:Bindable
+    override var maxLimit: Double = 999999.00
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.maxLimit)
+        }
+
+    @get:Bindable
+    override var availableBalanceText: String=""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.availableBalanceText)
+        }
+
+    @get:Bindable
+    override var availableBalanceGuide: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.availableBalanceGuide)
+        }
+
+
+    fun checkValidity(): String {
+        if (amount != "") {
+            if (amount?.toDouble()!! > availableBalance.toDouble()) {
+                amountBackground =
+                    context.resources.getDrawable(co.yap.yapcore.R.drawable.bg_funds_error, null)
+                    errorDescription = Translator.getString(
+                        context,
+                        Strings.screen_add_funds_display_text_available_balance_error,
+                        currencyType,
+                        Utils.getFormattedCurrency(availableBalance)
+                    )
+
+                return errorDescription
+            } else if (amount?.toDouble()!! > maxLimit) {
+                amountBackground =
+                    context.resources.getDrawable(co.yap.yapcore.R.drawable.bg_funds_error, null)
+
+                errorDescription = Translator.getString(
+                    context,
+                    Strings.screen_add_funds_display_text_max_limit_error,
+                    currencyType,
+                    Utils.getFormattedCurrency(maxLimit.toString())
+                )
+                return errorDescription
+
+            } else {
+                amountBackground =
+                    context.resources.getDrawable(co.yap.yapcore.R.drawable.bg_funds, null)
+            }
+        }
+        return ""
+    }
+
+
+    private fun clearError() {
+        if (amount != "") {
+            if (amount != ".") {
+                valid = amount?.toDouble()!! >= minLimit
+                amountBackground =
+                    context.resources.getDrawable(co.yap.yapcore.R.drawable.bg_funds, null)
+            }
+        } else if (amount == "") {
+            valid = false
+        }
+    }
+
 }
