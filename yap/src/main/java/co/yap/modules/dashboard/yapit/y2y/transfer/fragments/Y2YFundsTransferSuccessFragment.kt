6a34853e03555943c508f6d8d77099f6c4c28a12
sufyanshabbir <sufyan@digitify.com>
2019-10-24 18:00:06 +0500

    YM - 2550 minor merging fixes

--- yap/src/main/java/co/yap/modules/dashboard/yapit/y2y/transfer/fragments/Y2YFundsTransferSuccessFragment.kt
+++ yap/src/main/java/co/yap/modules/dashboard/yapit/y2y/transfer/fragments/Y2YFundsTransferSuccessFragment.kt
@@ -10,7 +10,6 @@ import co.yap.R
 import co.yap.modules.dashboard.yapit.y2y.main.fragments.Y2YBaseFragment
 import co.yap.modules.dashboard.yapit.y2y.transfer.interfaces.IY2YFundsTransferSuccess
 import co.yap.modules.dashboard.yapit.y2y.transfer.viewmodels.Y2YFundsTransferSuccessViewModel
-import co.yap.modules.dashboard.yapit.y2ytransfer.transfer.fragments.Y2YFundsTransferSuccessFragmentArgs
 
 class Y2YFundsTransferSuccessFragment : Y2YBaseFragment<IY2YFundsTransferSuccess.ViewModel>(),
     IY2YFundsTransferSuccess.View {
