107fd84509a864d6fcc0788cfa1133562f5bb5de
sufyanshabbir <sufyan@digitify.com>
2019-10-25 21:19:10 +0500

    YM - 2550 Design transaction details screen and implement business logic for the flow.

--- yap/src/main/java/co/yap/modules/dashboard/transaction/activities/TransactionDetailsActivity.kt
+++ yap/src/main/java/co/yap/modules/dashboard/transaction/activities/TransactionDetailsActivity.kt
@@ -1,11 +1,14 @@
 package co.yap.modules.dashboard.transaction.activities
 
+import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import co.yap.R
 import co.yap.modules.dashboard.transaction.interfaces.ITransactionDetails
 import co.yap.modules.dashboard.transaction.viewmodels.TransactionDetailsViewModel
 import co.yap.yapcore.BR
 import co.yap.yapcore.BaseBindingActivity
+import co.yap.yapcore.SingleClickEvent
 
 class TransactionDetailsActivity : BaseBindingActivity<ITransactionDetails.ViewModel>(),
     ITransactionDetails.View {
@@ -15,4 +18,17 @@ class TransactionDetailsActivity : BaseBindingActivity<ITransactionDetails.ViewM
 
     override val viewModel: ITransactionDetails.ViewModel
         get() = ViewModelProviders.of(this).get(TransactionDetailsViewModel::class.java)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.clickEvent.observe(this, clickEvent)
+
+    }
+
+    var clickEvent = Observer<Int> {
+        when (it) {
+            R.id.ivClose -> showToast("ivClose clicked")
+            R.id.ivShare -> showToast("ivShare clicked")
+        }
+    }
 }
--- yap/src/main/java/co/yap/modules/dashboard/transaction/viewmodels/TransactionDetailsViewModel.kt
+++ yap/src/main/java/co/yap/modules/dashboard/transaction/viewmodels/TransactionDetailsViewModel.kt
@@ -4,9 +4,24 @@ import android.app.Application
 import co.yap.modules.dashboard.transaction.interfaces.ITransactionDetails
 import co.yap.modules.dashboard.transaction.states.TransactionDetailsState
 import co.yap.yapcore.BaseViewModel
+import co.yap.yapcore.SingleClickEvent
 
 
 class TransactionDetailsViewModel(application: Application) :
     BaseViewModel<ITransactionDetails.State>(application), ITransactionDetails.ViewModel {
     override val state: TransactionDetailsState = TransactionDetailsState()
+    override var clickEvent: SingleClickEvent = SingleClickEvent()
+
+    override fun onCreate() {
+        super.onCreate()
+        state.toolBarTitle="Jan 29, 2019・10:35am"
+    }
+
+    override fun handlePressOnBackButton(id: Int) {
+        clickEvent.postValue(id)
+    }
+
+    override fun handlePressOnShareButton(id: Int) {
+        clickEvent.postValue(id)
+    }
 }
