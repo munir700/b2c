a58a3ff77716f1ca2818c2fdad1ea191d4acac71
sufyanshabbir <sufyan@digitify.com>
2019-10-29 19:01:50 +0500

    YM-2526 Add flow for transaction details(wip).

--- yap/src/main/java/co/yap/modules/dashboard/transaction/interfaces/ITransactionDetails.kt
+++ yap/src/main/java/co/yap/modules/dashboard/transaction/interfaces/ITransactionDetails.kt
@@ -8,11 +8,21 @@ interface ITransactionDetails {
     interface ViewModel : IBase.ViewModel<State> {
         fun handlePressOnBackButton(id: Int)
         fun handlePressOnShareButton(id: Int)
+        fun handlePressOnEditNoteClickEvent(id: Int)
         var clickEvent: SingleClickEvent
 
     }
 
     interface State : IBase.State {
         var toolBarTitle: String
+        var transactionTitle: String
+        var spentTitle: String
+        var spentAmount: String
+        var feeTitle: String
+        var feeAmount: String
+        var totalTitle: String
+        var totalAmount: String
+        var addNoteTitle: String
+        var noteValue: String
     }
 }
