053d99bff476828dfdcfeca1f2ffe5dfbfc2c1d8
Sohail Zahid <sohail@digitify.com>
2019-11-06 12:24:47 +0500

    YM-2601 App is being crashed while updating mobile number.Fixed

--- yap/src/main/java/co/yap/modules/others/generic_otp/GenericOtpFragment.kt
+++ yap/src/main/java/co/yap/modules/others/generic_otp/GenericOtpFragment.kt
@@ -7,7 +7,6 @@ import androidx.navigation.fragment.navArgs
 import co.yap.modules.dashboard.more.main.activities.MoreActivity
 import co.yap.modules.forgotpasscode.fragments.ForgotPasscodeOtpFragment
 import co.yap.modules.forgotpasscode.interfaces.IForgotPasscodeOtp
-import co.yap.modules.generic_otp.GenericOtpFragmentArgs
 import co.yap.yapcore.helpers.Utils
 
 class GenericOtpFragment : ForgotPasscodeOtpFragment() {
