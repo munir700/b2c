b6d5885fa6805fad8972bf6c37ac52a723735250
Najia Bokhari <najia@digitify.com>
2019-11-12 10:05:40 +0500

    add base structure classes data

--- yap/src/main/java/co/yap/modules/yapit/sendmoney/addbeneficiary/interfaces/IInternationalFundsTransfer.kt
+++ yap/src/main/java/co/yap/modules/yapit/sendmoney/addbeneficiary/interfaces/IInternationalFundsTransfer.kt
@@ -1,4 +1,24 @@
 package co.yap.modules.yapit.sendmoney.addbeneficiary.interfaces
 
+import co.yap.yapcore.IBase
+import co.yap.yapcore.SingleClickEvent
+
 interface IInternationalFundsTransfer {
+
+    interface State : IBase.State {
+        var senderCurrency: String
+        var beneficiaryCurrency: String
+        var beneficiaryCountry: String
+        var senderAmount: String
+        var beneficiaryAmount: String
+        var valid: Boolean
+
+    }
+
+    interface ViewModel : IBase.ViewModel<State> {
+        var clickEvent: SingleClickEvent
+        fun handlePressOnNext(id: Int)
+    }
+
+    interface View : IBase.View<ViewModel>
 }
--- yap/src/main/java/co/yap/modules/yapit/sendmoney/addbeneficiary/states/InternationalFundsTransferState.kt
+++ yap/src/main/java/co/yap/modules/yapit/sendmoney/addbeneficiary/states/InternationalFundsTransferState.kt
@@ -1,4 +1,52 @@
 package co.yap.modules.yapit.sendmoney.addbeneficiary.states
 
-class InternationalFundsTransferState {
+import androidx.databinding.Bindable
+import androidx.databinding.library.baseAdapters.BR
+import co.yap.modules.yapit.sendmoney.addbeneficiary.interfaces.IInternationalFundsTransfer
+import co.yap.yapcore.BaseState
+
+class InternationalFundsTransferState : BaseState(), IInternationalFundsTransfer.State {
+
+
+    @get:Bindable
+    override var senderCurrency: String = "AED"
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.senderCurrency)
+        }
+
+    @get:Bindable
+    override var beneficiaryCurrency: String = "AED"
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.beneficiaryCurrency)
+        }
+
+    @get:Bindable
+    override var beneficiaryCountry: String = "AED"
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.beneficiaryCountry)
+        }
+
+    @get:Bindable
+    override var senderAmount: String = "AED"
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.senderAmount)
+        }
+
+    @get:Bindable
+    override var beneficiaryAmount: String = "AED"
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.beneficiaryAmount)
+        }
+
+    @get:Bindable
+    override var valid: Boolean = false
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.valid)
+        }
 }
