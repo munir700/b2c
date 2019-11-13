a62823bc74e8aad695a48827aa3de1005803ddf8
Faheem Riaz <faheem@digitify.com>
2019-11-13 11:04:02 +0500

    YM-2783 payment card model added

---
+++ yap/src/main/java/co/yap/modules/dashboard/models/CardInfo.kt
@@ -0,0 +1,8 @@
+package co.yap.modules.dashboard.models
+
+data class CardInfo(
+    var cardNickname: String,
+    var cardNo: String,
+    var cardExpiry: String,
+    var cardType: String
+)
--- yap/src/main/java/co/yap/modules/dashboard/yapit/topup/main/carddetail/TopupCardDetailActivity.kt
+++ yap/src/main/java/co/yap/modules/dashboard/yapit/topup/main/carddetail/TopupCardDetailActivity.kt
@@ -3,10 +3,10 @@ package co.yap.modules.dashboard.yapit.topup.main.carddetail
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import co.yap.R
+import co.yap.modules.dashboard.models.CardInfo
 import co.yap.yapcore.BR
 import co.yap.yapcore.BaseBindingActivity
 
@@ -26,14 +26,21 @@ class TopupCardDetailActivity : BaseBindingActivity<ITopUpCardDetail.ViewModel>(
         super.onCreate(savedInstanceState)
         setObservers()
         setupView()
+
     }
 
     private fun setupView() {
+        val card = CardInfo(
+            cardNickname = "Citi Bank Card",
+            cardNo = "1234 4567 8901 1122",
+            cardExpiry = "12/2021",
+            cardType = "VISA"
+        )
         //TODO: set these values from Selected Card
-        viewModel.state.cardNickname.set("Citi Bank Card")
-        viewModel.state.cardNo.set("Citi Bank *8765")
-        viewModel.state.cardType.set("Visa")
-        viewModel.state.cardExpiry.set("12/2021")
+        viewModel.state.cardNickname.set(card.cardNickname)
+        viewModel.state.cardNo.set(card.cardNo)
+        viewModel.state.cardType.set(card.cardType)
+        viewModel.state.cardExpiry.set(card.cardExpiry)
     }
 
     private fun setObservers() {
