9e2126411206b87ddaa805cea37facced106b9ec
Faheem Riaz <faheem@digitify.com>
2019-11-15 16:29:05 +0500

    add card dialog issue fixed

--- yap/src/main/java/co/yap/modules/dashboard/yapit/topup/topupcards/TopUpCardsActivity.kt
+++ yap/src/main/java/co/yap/modules/dashboard/yapit/topup/topupcards/TopUpCardsActivity.kt
@@ -1,14 +1,10 @@
 package co.yap.modules.dashboard.yapit.topup.topupcards
 
 import android.app.Activity
-import android.app.Dialog
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
-import android.view.Window
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.RecyclerView
@@ -17,7 +13,7 @@ import co.yap.R
 import co.yap.databinding.ActivityTopupCardsBinding
 import co.yap.modules.dashboard.yapit.topup.main.carddetail.TopupCardDetailActivity
 import co.yap.modules.dashboard.yapit.topup.main.topupamount.activities.TopUpCardActivity
-import co.yap.modules.dashboard.yapit.topup.topupcards.addtopupcard.AddTopUpCardDialog
+import co.yap.modules.dashboard.yapit.topup.topupcards.addtopupcard.activities.AddTopUpCardActivity
 import co.yap.modules.others.helper.Constants
 import co.yap.networking.customers.responsedtos.beneficiary.TopUpCard
 import co.yap.yapcore.BaseBindingActivity
@@ -94,19 +90,12 @@ class TopUpCardsActivity : BaseBindingActivity<ITopUpCards.ViewModel>() {
     private val clickEventObserver = Observer<Int> {
         when (it) {
             R.id.tbBtnAddCard -> {
-//                startActivityForResult(
-//                    Intent(
-//                        this,
-//                        AddTopUpCardActivity::class.java
-//                    ), Constants.EVENT_ADD_TOPUP_CARD
-//                )
-//                AddTopUpCardDialog.newInstance(object : AddTopUpCardDialog.OnProceedListener{
-//                    override fun onProceed(view: View) {
-//                    }
-//
-//                },this).show()
-                showPopupDialog()
-//                AddTopUpCardDialog(this).show()
+                startActivityForResult(
+                    Intent(
+                        this,
+                        AddTopUpCardActivity::class.java
+                    ), Constants.EVENT_ADD_TOPUP_CARD
+                )
             }
             R.id.btnSelect -> {
                 val item = mAdapter.getDataForPosition(getBinding().rvTopUpCards.currentItem)
@@ -160,14 +149,5 @@ class TopUpCardsActivity : BaseBindingActivity<ITopUpCards.ViewModel>() {
                 viewModel.getPaymentCards()
             }
         }
-    }
-
-    private fun showPopupDialog() {
-        val dialog = Dialog(this)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setCancelable(true)
-        dialog.setContentView(R.layout.dialog_add_topup_card)
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        dialog.show()
     }
 }
--- yap/src/main/java/co/yap/modules/dashboard/yapit/topup/topupcards/addtopupcard/AddTopUpCardDialog.kt
+++ yap/src/main/java/co/yap/modules/dashboard/yapit/topup/topupcards/addtopupcard/AddTopUpCardDialog.kt
@@ -4,7 +4,6 @@ import android.app.Dialog
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import android.view.View
 import android.view.Window
 import co.yap.R
 import kotlinx.android.synthetic.main.dialog_add_topup_card.*
@@ -12,39 +11,33 @@ import kotlinx.android.synthetic.main.dialog_add_topup_card.*
 class AddTopUpCardDialog(context: Context) : Dialog(context) {
     private var onProceedListener: OnProceedListener? = null
 
-    override fun setContentView(layoutResID: Int) {
-        super.setContentView(layoutResID)
-        setContentView(R.layout.dialog_add_topup_card)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        setCancelable(false)
-        setListeners()
-    }
 
     private fun setListeners() {
         btnLater.setOnClickListener {
-            onProceedListener?.onProceed(it)
+            onProceedListener?.onProceed(it.id)
             dismiss()
         }
         btnTopUpNow.setOnClickListener {
-            onProceedListener?.onProceed(it)
+            onProceedListener?.onProceed(it.id)
             dismiss()
         }
     }
 
 
     interface OnProceedListener {
-        fun onProceed(view: View)
+        fun onProceed(id: Int)
     }
 
     companion object {
         @JvmStatic
-
         fun newInstance(callback: OnProceedListener?, context: Context): AddTopUpCardDialog {
             val dialog = AddTopUpCardDialog(context)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setCancelable(false)
+            dialog.setContentView(R.layout.dialog_add_topup_card)
             dialog.onProceedListener = callback
-            //  dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-//            dialog.show()
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialog.setListeners()
             return dialog
         }
     }
