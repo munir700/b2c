4ef59ab37fd53609b33f6f8c3b531f9ca36f7a8c
sufyanshabbir <sufyan@digitify.com>
2019-11-20 15:10:25 +0500

    Create merchant analytics fragment and implement business logic for flow i.e states view model ec

---
+++ yap/src/main/java/co/yap/modules/dashboard/cards/analytics/fragments/MerchantAnalyticsFragment.kt
@@ -0,0 +1,18 @@
+package co.yap.modules.dashboard.cards.analytics.fragments
+
+import androidx.lifecycle.ViewModelProviders
+import co.yap.BR
+import co.yap.R
+import co.yap.modules.dashboard.cards.analytics.interfaces.IMerchantAnalytics
+import co.yap.modules.dashboard.cards.analytics.main.fragments.CardAnalyticsBaseFragment
+import co.yap.modules.dashboard.cards.analytics.viewmodels.MerchantAnalyticsViewModel
+
+class MerchantAnalyticsFragment : CardAnalyticsBaseFragment<IMerchantAnalytics.ViewModel>(),
+    IMerchantAnalytics.View {
+    override fun getBindingVariable(): Int = BR.viewModel
+
+    override fun getLayoutId(): Int = R.layout.fragment_merchant_analytics
+
+    override val viewModel: IMerchantAnalytics.ViewModel
+        get() = ViewModelProviders.of(this).get(MerchantAnalyticsViewModel::class.java)
+}
