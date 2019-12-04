e117cc391de20263e7cf48e44fb67477355d9d43
Mirza Adil <adilmirza@digitify.com>
2019-12-04 14:35:50 +0500

    Update CardLimitConfigRequest Class Null Exception .

--- networking/src/main/java/co/yap/networking/cards/requestdtos/CardLimitConfigRequest.kt
+++ networking/src/main/java/co/yap/networking/cards/requestdtos/CardLimitConfigRequest.kt
@@ -1,5 +1,9 @@
 package co.yap.networking.cards.requestdtos
 
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
 data class CardLimitConfigRequest(
-	val cardSerialNumber: String
-)
+    @SerializedName("cardSerialNumber")
+    val cardSerialNumber: String
+) : Serializable
