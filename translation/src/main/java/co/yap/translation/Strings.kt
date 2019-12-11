44cb33250dae3a45f5db2b4fdc30adac4452b023
Faheem Riaz <faheem@digitify.com>
2019-12-11 15:42:21 +0500

    YM-3113 Added BaseBindingAdapter for search in core Added Business logic for searching list items no result case handled code refactoring

--- translation/src/main/java/co/yap/translation/Strings.kt
+++ translation/src/main/java/co/yap/translation/Strings.kt
@@ -818,6 +818,9 @@ object Strings {
         "common_display_text_y2y_share"
     const val common_display_text_y2y_general_share: String =
         "common_display_text_y2y_general_share"
+    const val screen_y2y_display_text_no_result: String =
+        "screen_y2y_display_text_no_result"
+
 
     //    Y2Y funds Transfer success screen
     const val screen_y2y_funds_transfer_success_display_text_title: String =
