25a20bc9a83f26ca59900ba59259511832cdc71f
Irfan <irfan@digitify.com>
2019-12-17 11:34:34 +0500

    hotfix

--- yap/src/main/java/co/yap/modules/dashboard/yapit/sendmoney/home/activities/SendMoneyLandingActivity.kt
+++ yap/src/main/java/co/yap/modules/dashboard/yapit/sendmoney/home/activities/SendMoneyLandingActivity.kt
@@ -203,7 +203,7 @@ class SendMoneyLandingActivity : BaseBindingActivity<ISendMoneyHome.ViewModel>()
         beneficiary?.let {
             val intent = EditBeneficiaryActivity.newIntent(context = this)
             val bundle = Bundle()
-            bundle.putBoolean(OVERVIEW_BENEFICIARY,true)
+            bundle.putBoolean(OVERVIEW_BENEFICIARY,false)
             bundle.putParcelable(Beneficiary::class.java.name, beneficiary)
             intent.putExtra(Bundle_EXTRA, bundle)
             startActivityForResult(intent, REQUEST_CODE)
