99db45e25e4a9d23ebbbee33a12a1c77d09dcddf
Najia Bokhari <najia@digitify.com>
2020-01-13 17:25:01 +0500

    YH-93 update kyc theme, WIP

--- identityscanner/src/main/java/com/digitify/identityscanner/core/arch/ScanLimitReachedException.java
+++
@@ -1,7 +0,0 @@
-package com.digitify.identityscanner.core.arch;
-
-public class ScanLimitReachedException extends IllegalStateException {
-    public ScanLimitReachedException() {
-        super("Scan Limit reached");
-    }
-}
--- identityscanner/src/main/java/com/digitify/identityscanner/core/detection/interfaces/IResult.java
+++
@@ -1,5 +0,0 @@
-package com.digitify.identityscanner.core.detection.interfaces;
-
-public interface IResult {
-    double getConfidence();
-}
