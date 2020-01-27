d806f51cf6cd0f38f11d5b71c3f90a6bd879acbd
Sohail Zahid <sohail@digitify.com>
2020-01-27 12:11:30 +0500

    Events class added

--- yap/src/main/java/co/yap/modules/onboarding/fragments/MobileFragment.kt
+++ yap/src/main/java/co/yap/modules/onboarding/fragments/MobileFragment.kt
@@ -8,6 +8,7 @@ import co.yap.R
 import co.yap.modules.onboarding.interfaces.IMobile
 import co.yap.modules.onboarding.viewmodels.MobileViewModel
 import co.yap.yapcore.helpers.extentions.trackEvent
+import co.yap.yapcore.leanplum.TrackEvents
 import kotlinx.android.synthetic.main.fragment_mobile.*
 
 
@@ -23,7 +24,7 @@ class MobileFragment : OnboardingChildFragment<IMobile.ViewModel>() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel.nextButtonPressEvent.observe(this, Observer {
-            trackEvent("mobile-number-entered")
+            trackEvent(TrackEvents.MOBILE_NUMBER_ENTERED)
             navigate(R.id.phoneVerificationFragment)
         })
         viewModel.getCcp(etMobileNumber)!!
