9b06894acdff461f51121b73f241a50a20966230
Faheem Riaz <faheem@digitify.com>
2020-02-07 15:59:16 +0500

    YM-3908 Adapter package added

---
+++ yap/src/main/java/co/yap/modules/dashboard/more/yapforyou/adapters/AchievementAdaptor.kt
@@ -0,0 +1,10 @@
+package co.yap.modules.dashboard.more.yapforyou.adapters
+
+import co.yap.modules.yapnotification.models.Notification
+import co.yap.yapcore.BaseBindingRecyclerAdapter
+
+class AchievementAdaptor(private val list: MutableList<Notification>) :
+    BaseBindingRecyclerAdapter<Notification, {
+
+
+}
