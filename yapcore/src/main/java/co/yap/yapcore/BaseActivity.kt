aaa98450f237a72b2b5e87700c758aaba363799a
Najia Bokhari <najia@digitify.com>
2020-02-08 11:33:24 +0500

    no message

--- yapcore/src/main/java/co/yap/yapcore/BaseActivity.kt
+++ yapcore/src/main/java/co/yap/yapcore/BaseActivity.kt
@@ -221,9 +221,9 @@ abstract class BaseActivity<V : IBase.ViewModel<*>> : AppCompatActivity(), IBase
     override fun onResume() {
         super.onResume()
 //         if (DeviceUtils().isDeviceRooted(applicationContext)) {
-//         if (DeviceUtils().isDeviceRooted()) {
-//            showAlertDialogAndExitApp("This device is rooted. You can't use this app.")
-//        }
+         if (DeviceUtils().isDeviceRooted()) {
+            showAlertDialogAndExitApp("This device is rooted. You can't use this app.")
+        }
     }
 
     open fun showAlertDialogAndExitApp(message: String?) {
