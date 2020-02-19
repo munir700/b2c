8c3aaccb15464ccf92ca0d77e4fa9c25c47cd083
Najia Bokhari <najia@digitify.com>
2020-02-19 16:05:13 +0500

    update certificate keys

--- networking/src/main/java/co/yap/networking/RetroNetwork.kt
+++ networking/src/main/java/co/yap/networking/RetroNetwork.kt
@@ -60,12 +60,20 @@ object RetroNetwork : Network {
                 "sha256/ZrRL6wSXl/4lm1KItkcZyh56BGOoxMWUDJr7YVqE4no=",
                 "sha256/8Rw90Ej3Ttt8RRkrg+WYDS9n7IS03bk5bjP/UXPtaY8=",
                 "sha256/jr1RBEN+F3KtPTYBMhudiTGBRAg8k2qZPEg3WbSerXU="*/
-            //for qa debug
+
+//            for qa debug
             .add("*.yap.co", "sha256/e5L5CAoQjV0HFzAnunk1mPHVx1HvPxcfJYI0UtLyBwY=")
             .add("*.yap.co", "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA")
             .add("*.yap.co", "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
-            .add("*.yap.co", "sha256/jr1RBEN+F3KtPTYBMhudiTGBRAg8k2qZPEg3WbSerXU=")
-//for stg debug
+            .add(
+                "*.yap.co",
+                "sha256/jr1RBEN+F3KtPTYBMhudiTGBRAg8k2qZPEg3WbSerXU="
+            )//from yap cert file
+            .add(
+                "*.yap.co",
+                "sha256/yJcy2FrimDcAjQrvDDImmFJna4OjlPQ4LAee9Vj2C74="
+            )//from yap cert file
+//           for stg debug
             .add("*.yap.co", "sha256/Ko8tivDrEjiY90yGasP6ZpBU4jwXvHqVvQI0GS3GNdA=")
             .add("*.yap.co", "sha256/ZrRL6wSXl/4lm1KItkcZyh56BGOoxMWUDJr7YVqE4no=")
             .add("*.yap.co", "sha256/8Rw90Ej3Ttt8RRkrg+WYDS9n7IS03bk5bjP/UXPtaY8=")
