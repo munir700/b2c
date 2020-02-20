0827f3a068704e7e5182ae71a13d6662aef43715
Faheem Riaz <faheem@digitify.com>
2020-02-20 16:47:50 +0500

    Merge branch 'dev' into feature/YM-4134-android-expired-card-application
    # Conflicts:
    #	yap/src/main/java/co/yap/modules/dashboard/home/adaptor/TransactionsHeaderAdapter.kt

---
+++ yapcore/src/main/java/co/yap/modules/webview/IWebViewFragment.kt
@@ -0,0 +1,17 @@
+package co.yap.modules.webview
+
+import co.yap.yapcore.IBase
+
+interface IWebViewFragment {
+    interface View : IBase.View<ViewModel> {
+
+    }
+
+    interface ViewModel : IBase.ViewModel<State> {
+
+    }
+
+    interface State : IBase.State {
+
+    }
+}
