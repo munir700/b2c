da6371263192329921471a733dd57a75e9c1cbdd
Sohail Zahid <sohail@digitify.com>
2020-02-27 15:32:14 +0500

    Update live chat

--- yap/src/main/java/co/yap/modules/dashboard/more/help/fragments/HelpSupportFragment.kt
+++ yap/src/main/java/co/yap/modules/dashboard/more/help/fragments/HelpSupportFragment.kt
@@ -7,7 +7,6 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.annotation.Nullable
 import androidx.core.os.bundleOf
 import androidx.lifecycle.Observer
@@ -23,10 +22,10 @@ import co.yap.modules.dashboard.more.main.fragments.MoreBaseFragment
 import co.yap.modules.webview.WebViewFragment
 import co.yap.networking.customers.requestdtos.Contact
 import co.yap.yapcore.constants.Constants
-import co.yap.yapcore.helpers.LivePersonStorage
 import co.yap.yapcore.helpers.Utils
 import co.yap.yapcore.helpers.extentions.startFragment
 import co.yap.yapcore.helpers.extentions.toast
+import co.yap.yapcore.managers.MyUserManager
 import com.liveperson.infra.*
 import com.liveperson.infra.callbacks.InitLivePersonCallBack
 import com.liveperson.messaging.sdk.api.LivePerson
@@ -40,6 +39,10 @@ class HelpSupportFragment : MoreBaseFragment<IHelpSupport.ViewModel>(), IHelpSup
     override fun getBindingVariable(): Int = BR.viewModel
 
     override fun getLayoutId(): Int = R.layout.fragment_help_support
+
+    val accountId = "17038977"
+    val appInstallId = "17038977"
+    val FCMID = "17038977"
 
     override val viewModel: IHelpSupport.ViewModel
         get() = ViewModelProviders.of(this).get(HelpSupportViewModel::class.java)
@@ -75,11 +78,9 @@ class HelpSupportFragment : MoreBaseFragment<IHelpSupport.ViewModel>(), IHelpSup
                 viewModel.getFaqsUrl()
             }
             R.id.lyChat -> {
-                //Utils.showComingSoon(requireContext())
                 chatSetup()
             }
             R.id.lyLiveWhatsApp -> {
-                // Utils.showComingSoon(requireContext())
                 OpenWhatsApp()
             }
             R.id.lyCall -> {
@@ -117,53 +118,19 @@ class HelpSupportFragment : MoreBaseFragment<IHelpSupport.ViewModel>(), IHelpSup
     }
 
     private fun chatSetup() {
-        storeParams()
         LivePerson.initialize(
-            requireContext().applicationContext,
+            requireContext(),
             InitLivePersonProperties(
-                LivePersonStorage.getInstance(context!!)?.account,
-                LivePersonStorage.SDK_SAMPLE_FCM_APP_ID,
+                accountId,
+                FCMID,
                 null,
                 object : InitLivePersonCallBack {
-
                     override fun onInitSucceed() {
-                        LivePersonStorage.getInstance(requireContext())?.sdkMode =
-                            (LivePersonStorage.SDKMode.ACTIVITY)
-                        LivePersonStorage.getInstance(requireContext())?.firstName = ""
-                        LivePersonStorage.getInstance(requireContext())?.lastName = ""
-                        LivePersonStorage.getInstance(requireContext())?.phoneNumber = ""
-                        LivePersonStorage.getInstance(requireContext())?.authCode = ""
-                        LivePersonStorage.getInstance(requireContext())?.publicKey = ""
-                        activity!!.runOnUiThread(Runnable {
-                            openActivity()
-                        })
+                        openActivity()
                     }
 
                     override fun onInitFailed(e: Exception) {
-                        Toast.makeText(context!!, "Init failed", Toast.LENGTH_SHORT).show()
-                    }
-                })
-        )
-    }
-
-    private fun storeParams() {
-        LivePersonStorage.getInstance(requireContext())?.account = "17038977"
-        LivePersonStorage.getInstance(requireContext())?.appInstallId = "17038977"
-    }
-
-    private fun initActivityConversation() {
-
-        LivePerson.initialize(
-            context!!,
-            InitLivePersonProperties(
-                LivePersonStorage.getInstance(context!!)?.account,
-                LivePersonStorage.SDK_SAMPLE_FCM_APP_ID,
-                object : InitLivePersonCallBack {
-                    override fun onInitSucceed() {
-
-                    }
-
-                    override fun onInitFailed(e: Exception) {
+                        toast("Unable to open chat")
                     }
                 })
         )
@@ -171,14 +138,14 @@ class HelpSupportFragment : MoreBaseFragment<IHelpSupport.ViewModel>(), IHelpSup
 
     private fun openActivity() {
 
-        val authCode = LivePersonStorage.getInstance(context!!)?.authCode
-        val publicKey = LivePersonStorage.getInstance(context!!)?.publicKey
+        val authCode = "authCode"
+        val publicKey = "publicKey"
 
         val authParams = LPAuthenticationParams()
         authParams.authKey = authCode
         authParams.addCertificatePinningKey(publicKey)
 
-        val campaignInfo = getCampaignInfo(context!!)
+        val campaignInfo = getCampaignInfo()
         val params = ConversationViewParams()
             .setHistoryConversationsStateToDisplay(LPConversationsHistoryStateToDisplay.ALL)
             .setCampaignInfo(campaignInfo).setReadOnlyMode(isReadOnly())
@@ -186,9 +153,9 @@ class HelpSupportFragment : MoreBaseFragment<IHelpSupport.ViewModel>(), IHelpSup
         LivePerson.showConversation(requireActivity(), authParams, params)
 
         val consumerProfile = ConsumerProfile.Builder()
-            .setFirstName(LivePersonStorage.getInstance(requireContext())?.firstName)
-            .setLastName(LivePersonStorage.getInstance(requireContext())?.lastName)
-            .setPhoneNumber(LivePersonStorage.getInstance(requireContext())?.phoneNumber)
+            .setFirstName(MyUserManager.user?.currentCustomer?.firstName)
+            .setLastName(MyUserManager.user?.currentCustomer?.lastName)
+            .setPhoneNumber(MyUserManager.user?.currentCustomer?.getCompletePhone())
             .build()
 
         LivePerson.setUserProfile(consumerProfile)
@@ -206,30 +173,12 @@ class HelpSupportFragment : MoreBaseFragment<IHelpSupport.ViewModel>(), IHelpSup
     }
 
     @Nullable
-    fun getCampaignInfo(context: Context): CampaignInfo? {
-        var campaignInfo: CampaignInfo? = null
-        if (LivePersonStorage.getInstance(context)?.campaignId != null || LivePersonStorage.getInstance(
-                context
-            )?.engagementId != null ||
-            LivePersonStorage.getInstance(context)?.sessionId != null || LivePersonStorage.getInstance(
-                context
-            )?.visitorId != null
-        ) {
-
-            try {
-                campaignInfo = CampaignInfo(
-                    LivePersonStorage.getInstance(context)?.campaignId!!,
-                    LivePersonStorage.getInstance(context)?.engagementId!!,
-                    LivePersonStorage.getInstance(context)?.interactionContextId!!,
-                    LivePersonStorage.getInstance(context)?.sessionId,
-                    LivePersonStorage.getInstance(context)?.visitorId
-                )
-            } catch (e: BadArgumentException) {
-                return null
-            }
-
-        }
-        return campaignInfo
+    fun getCampaignInfo(): CampaignInfo? {
+        return CampaignInfo(
+            "campaignId".toLong(), "engagementId".toLong(),
+            "interactionContextId", "sessionId",
+            "visitorId"
+        )
     }
 
     private fun openFaqsPage(url: String) {
@@ -239,7 +188,6 @@ class HelpSupportFragment : MoreBaseFragment<IHelpSupport.ViewModel>(), IHelpSup
                 Constants.PAGE_URL to url
             ), toolBarTitle = viewModel.state.title.get() ?: "", showToolBar = true
         )
-        //Utils.openWebPage(url, viewModel.state.title.get() ?: "", activity)
     }
 
     private fun openDialer() {
