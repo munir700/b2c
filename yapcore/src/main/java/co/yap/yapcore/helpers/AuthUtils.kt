530d3cd9e0d6f572e4ba0587a0d6651c4252f9bb
Najia Bokhari <najia@digitify.com>
2020-03-18 14:31:49 +0500

    YM-4472 prevent deep linking information on clearing shared preferences

--- yapcore/src/main/java/co/yap/yapcore/helpers/AuthUtils.kt
+++ yapcore/src/main/java/co/yap/yapcore/helpers/AuthUtils.kt
@@ -2,6 +2,9 @@ package co.yap.yapcore.helpers
 
 import android.content.Context
 import android.content.Intent
+import co.yap.yapcore.constants.Constants
+import co.yap.yapcore.constants.Constants.INVITEE_RECEIEVED_DATE
+import co.yap.yapcore.constants.Constants.INVITER_ADJUST_ID
 import co.yap.yapcore.constants.Constants.KEY_APP_UUID
 import co.yap.yapcore.constants.Constants.KEY_IS_FINGERPRINT_PERMISSION_SHOWN
 import co.yap.yapcore.constants.Constants.KEY_IS_FIRST_TIME_USER
@@ -27,6 +30,22 @@ object AuthUtils {
         //val uuid: String? =
         //    sharedPreferenceManager.getValueString(SharedPreferenceManager.KEY_APP_UUID)
 
+        var inviterLinkDate:String?= null
+        var inviterId:String?= null
+        if (!SharedPreferenceManager(context).getValueString(
+                Constants.INVITEE_RECEIEVED_DATE
+            ).isNullOrEmpty() && !SharedPreferenceManager(context).getValueString(
+                Constants.INVITER_ADJUST_ID
+            ).isNullOrEmpty()
+        ){
+
+            inviterLinkDate = SharedPreferenceManager(context).getValueString(
+                Constants.INVITEE_RECEIEVED_DATE
+            )
+            inviterId = SharedPreferenceManager(context).getValueString(
+                Constants.INVITER_ADJUST_ID
+            )
+        }
         sharedPreferenceManager.clearSharedPreference()
         //sharedPreferenceManager.save(SharedPreferenceManager.KEY_APP_UUID, uuid.toString())
         sharedPreferenceManager.save(
@@ -41,6 +60,19 @@ object AuthUtils {
             KEY_IS_FIRST_TIME_USER,
             isFirstTimeUser
         )
+        inviterId?.let {
+            sharedPreferenceManager.save(
+                INVITER_ADJUST_ID,
+                it
+            )
+        }
+        
+        inviterLinkDate?.let {
+            sharedPreferenceManager.save(
+                INVITEE_RECEIEVED_DATE,
+                it
+            )
+        }
     }
 
     fun navigateToSoftLogin(context: Context) {
