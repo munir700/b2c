9d3e6c5d7db4bebd8409ff3d5f99bb0dd126c60e
Sufyan Shabbir <sufyan@digitify.com>
2020-04-06 11:20:08 +0500

    YH-190 Implement dialog for subscription screen

--- yap/src/main/java/co/yap/modules/subaccounts/paysalary/subscription/SubscriptionVM.kt
+++ yap/src/main/java/co/yap/modules/subaccounts/paysalary/subscription/SubscriptionVM.kt
@@ -4,7 +4,8 @@ import android.content.Context
 import android.os.Bundle
 import androidx.navigation.NavController
 import co.yap.yapcore.dagger.base.viewmodel.DaggerBaseViewModel
-import co.yap.yapcore.helpers.extentions.showAlert
+import co.yap.yapcore.helpers.confirm
+import co.yap.yapcore.helpers.extentions.toast
 import javax.inject.Inject
 
 class SubscriptionVM @Inject constructor(override val state: ISubscription.State) :
@@ -16,19 +17,14 @@ class SubscriptionVM @Inject constructor(override val state: ISubscription.State
     }
 
     override fun handlePressOnClick(context: Context) {
-        /*
-        * This should be replaced
-        * */
-        context.showAlert(
-            "Are you sure you want to cancel this subscription?",
-            "Cancellation will come into effect following the 12-month contract period"
-        )
-
-        /*     context.confirm(
-                 getString(R.string.location_permission_msg),
-                 getString(R.string.location_permission)
-             ) {
-
-             }*/
+        context.confirm(
+            title = "Are you sure you want to cancel this subscription?",
+            message = "Cancellation will come into effect following the 12-month contract period",
+            positiveButton = "YES",
+            negativeButton = "NO",
+            cancelable = false
+        ) {
+            toast(context, "Cancel subscription.")
+        }
     }
 }
