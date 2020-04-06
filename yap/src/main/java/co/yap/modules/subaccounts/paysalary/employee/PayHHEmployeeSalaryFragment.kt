80a28bb0ead6ffbe9a64f786117659947e191bb7
Sufyan Shabbir <sufyan@digitify.com>
2020-04-06 16:07:15 +0500

    YH-190 minor change

--- yap/src/main/java/co/yap/modules/subaccounts/paysalary/employee/PayHHEmployeeSalaryFragment.kt
+++ yap/src/main/java/co/yap/modules/subaccounts/paysalary/employee/PayHHEmployeeSalaryFragment.kt
@@ -1,5 +1,7 @@
 package co.yap.modules.subaccounts.paysalary.employee
 
+import android.view.Menu
+import android.view.MenuInflater
 import co.yap.BR
 import co.yap.R
 import co.yap.databinding.FragmentPayHhemployeeSalaryBinding
@@ -10,4 +12,14 @@ class PayHHEmployeeSalaryFragment :
     override fun getBindingVariable() = BR.payHHEmployeeSalaryVM
 
     override fun getLayoutId() = R.layout.fragment_pay_hhemployee_salary
+
+    override fun postExecutePendingBindings() {
+        super.postExecutePendingBindings()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.add_menu,menu)
+    }
 }
