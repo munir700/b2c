8b6c572bf81043e03de41b4aa6ec0c8493ec6360
Sufyan Shabbir <sufyan@digitify.com>
2020-04-06 17:17:53 +0500

    YH-190 Complete bottom sheet implementation.

--- yap/src/main/java/co/yap/modules/subaccounts/paysalary/profile/HHSalaryProfileFragment.kt
+++ yap/src/main/java/co/yap/modules/subaccounts/paysalary/profile/HHSalaryProfileFragment.kt
@@ -1,29 +1,41 @@
 package co.yap.modules.subaccounts.paysalary.profile
 
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.databinding.ViewDataBinding
 import androidx.navigation.NavController
 import co.yap.BR
 import co.yap.R
 import co.yap.databinding.FragmentHhsalaryProfileBinding
+import co.yap.modules.subaccounts.HouseHoldMainAccountBottomSheet
+import co.yap.modules.subaccounts.HouseHoldMainAccountBottomSheetClick
+import co.yap.modules.subaccounts.paysalary.subscription.SubscriptionFragment
 import co.yap.translation.Strings
 import co.yap.yapcore.BaseRVAdapter
 import co.yap.yapcore.BaseViewHolder
 import co.yap.yapcore.dagger.base.BaseRecyclerViewFragment
-import co.yap.yapcore.interfaces.OnItemClickListener
+import co.yap.yapcore.helpers.extentions.startFragment
+import co.yap.yapcore.helpers.extentions.toast
 import kotlin.reflect.KClass
 import kotlin.reflect.full.primaryConstructor
 
 class HHSalaryProfileFragment :
-    BaseRecyclerViewFragment<FragmentHhsalaryProfileBinding, IHHSalaryProfile.State, HHSalaryProfileVM, HHSalaryProfileFragment.Adapter, PaySalaryModel>() {
+    BaseRecyclerViewFragment<FragmentHhsalaryProfileBinding, IHHSalaryProfile.State, HHSalaryProfileVM, HHSalaryProfileFragment.Adapter, PaySalaryModel>(),
+    HouseHoldMainAccountBottomSheetClick {
 
     override fun getBindingVariable() = BR.hhSalaryProfileVM
+    private lateinit var houseHoldMainAccountBottomSheet: HouseHoldMainAccountBottomSheet
 
     override fun getLayoutId() = R.layout.fragment_hhsalary_profile
 
     override fun postExecutePendingBindings() {
         super.postExecutePendingBindings()
+        houseHoldMainAccountBottomSheet =
+            HouseHoldMainAccountBottomSheet(this)
         viewModel.setUpData(getPaySalaryData())
+        setHasOptionsMenu(true)
     }
 
     fun getPaySalaryData(): ArrayList<PaySalaryModel> {
@@ -51,6 +63,22 @@ class HHSalaryProfileFragment :
 
     override var toolBarTitle: String? = "Your Name"
     //override var toolBarVisibility: Boolean? = false
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.add_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.cart -> {
+                houseHoldMainAccountBottomSheet.show(fragmentManager!!, "")
+            }
+            else -> return false
+        }
+        return true
+
+    }
 
     class Adapter(mValue: MutableList<PaySalaryModel>, navigation: NavController?) :
         BaseRVAdapter<PaySalaryModel, HHSalaryProfileItemVM, Adapter.ViewHolder>(
@@ -80,5 +108,14 @@ class HHSalaryProfileFragment :
             BaseViewHolder<PaySalaryModel, HHSalaryProfileItemVM>(view, viewModel, mDataBinding)
     }
 
+    override fun onSubscriptionClick() {
+        startFragment(SubscriptionFragment::class.java.name,showToolBar = true)
+        houseHoldMainAccountBottomSheet.dismiss()
+    }
+
+    override fun salaryStatementClick() {
+        houseHoldMainAccountBottomSheet.dismiss()
+
+    }
 
 }
