49d68e806d4430c1c5eb7422c0f0f377f8525920
Najia Bokhari <najia@digitify.com>
2020-04-15 18:42:56 +0500

    YM-4718 fix un named addresses issue

--- yapcore/src/main/java/co/yap/modules/location/viewmodels/LocationSelectionViewModel.kt
+++ yapcore/src/main/java/co/yap/modules/location/viewmodels/LocationSelectionViewModel.kt
@@ -13,6 +13,10 @@ import co.yap.yapcore.SingleClickEvent
 class LocationSelectionViewModel(application: Application) :
     BaseViewModel<ILocationSelection.State>(application),
     ILocationSelection.ViewModel {
+    override var isUnNamedLocation: Boolean = false
+    override var hasSeletedLocation: Boolean = false
+    override var unNamed: String = "Unnamed"
+    override var defaultHeading: String = ""
     override var clickEvent: SingleClickEvent = SingleClickEvent()
     override var isMapExpanded: MutableLiveData<Boolean> = MutableLiveData()
     override val state: LocationSelectionState = LocationSelectionState(application)
@@ -28,16 +32,39 @@ class LocationSelectionViewModel(application: Application) :
     }
 
     override fun onLocationSelected() {
-        state.addressTitle = state.placeSubTitle.get() ?: ""
-        state.addressSubtitle.set(state.placeTitle.get() ?: "")
-        address?.address1 = state.addressTitle
-        address?.address2 = state.addressSubtitle.get()
-        state.headingTitle.set(state.addressSubtitle.get())
-        state.subHeadingTitle.set(
-            Translator.getString(
-                getApplication(),
-                R.string.screen_meeting_location_display_text_selected_subtitle
+        hasSeletedLocation = true
+
+        if (state.placeTitle.get()?.toLowerCase()?.contains(unNamed.toLowerCase()) == true) {
+            state.headingTitle.set(defaultHeading)
+
+            isUnNamedLocation = true
+            state.subHeadingTitle.set(
+                Translator.getString(
+                    getApplication(),
+                    R.string.screen_meeting_location_display_text_add_manual_address_subtitle
+                )
             )
-        )
+            state.addressTitle = ""
+            if (state.placeSubTitle.get()?.toLowerCase()
+                    ?.contains(unNamed.toLowerCase()) == true
+            ) {
+                state.addressSubtitle.set("")
+
+            }
+        } else {
+
+            state.addressTitle = state.placeSubTitle.get() ?: ""
+            state.addressSubtitle.set(state.placeTitle.get() ?: "")
+            address?.address1 = state.addressTitle
+            address?.address2 = state.addressSubtitle.get()
+            state.headingTitle.set(state.addressSubtitle.get())
+            state.subHeadingTitle.set(
+                Translator.getString(
+                    getApplication(),
+                    R.string.screen_meeting_location_display_text_selected_subtitle
+                )
+            )
+        }
+
     }
 }
