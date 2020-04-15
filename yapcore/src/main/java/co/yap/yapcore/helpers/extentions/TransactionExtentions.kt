add50415a8597f4f578a7a9ffad921a0546f606e
Sohail Zahid <sohail@digitify.com>
2020-04-15 20:00:20 +0500

    transaction image updated

--- yapcore/src/main/java/co/yap/yapcore/helpers/extentions/TransactionExtentions.kt
+++ yapcore/src/main/java/co/yap/yapcore/helpers/extentions/TransactionExtentions.kt
@@ -194,7 +194,7 @@ fun Content?.getMapImage(): Int {
         }
         return (when (transaction.productCode) {
             TransactionProductCode.Y2Y_TRANSFER.pCode -> R.drawable.ic_image_blue_background
-            TransactionProductCode.TOP_UP_SUPPLEMENTARY_CARD.pCode, TransactionProductCode.WITHDRAW_SUPPLEMENTARY_CARD.pCode -> R.drawable.ic_image_blue_background
+            TransactionProductCode.TOP_UP_SUPPLEMENTARY_CARD.pCode, TransactionProductCode.WITHDRAW_SUPPLEMENTARY_CARD.pCode -> R.drawable.ic_image_brown_background
             TransactionProductCode.UAEFTS.pCode, TransactionProductCode.DOMESTIC.pCode, TransactionProductCode.RMT.pCode, TransactionProductCode.SWIFT.pCode, TransactionProductCode.CASH_PAYOUT.pCode, TransactionProductCode.TOP_UP_VIA_CARD.pCode, TransactionProductCode.INWARD_REMITTANCE.pCode, TransactionProductCode.LOCAL_INWARD_TRANSFER.pCode -> R.drawable.ic_image_light_blue_background
             TransactionProductCode.CARD_REORDER.pCode -> R.drawable.ic_image_light_red_background
             TransactionProductCode.POS_PURCHASE.pCode, TransactionProductCode.CASH_DEPOSIT_AT_RAK.pCode, TransactionProductCode.MASTER_CARD_ATM_WITHDRAWAL.pCode, TransactionProductCode.CHEQUE_DEPOSIT_AT_RAK.pCode, TransactionProductCode.FUND_LOAD.pCode -> R.drawable.ic_map
