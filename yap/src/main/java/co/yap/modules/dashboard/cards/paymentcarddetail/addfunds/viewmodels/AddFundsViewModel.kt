dac16113ce484e811fc9a29209ff331d4efbc98f
Faheem Riaz <faheem@digitify.com>
2020-04-16 17:06:16 +0500

    YM-4756 Understanding of api calling

--- yap/src/main/java/co/yap/modules/dashboard/cards/paymentcarddetail/addfunds/viewmodels/AddFundsViewModel.kt
+++ yap/src/main/java/co/yap/modules/dashboard/cards/paymentcarddetail/addfunds/viewmodels/AddFundsViewModel.kt
@@ -6,9 +6,10 @@ import co.yap.yapcore.constants.Constants
 
 class AddFundsViewModel(application: Application) : FundActionsViewModel(application) {
 
-
     override fun onCreate() {
         super.onCreate()
+        //TODO: call api for threshold here and save that response in viewmodel just like y2y
+
         state.toolBarHeader = getString(Strings.screen_add_funds_display_text_add_funds)
         state.enterAmountHeading = getString(Strings.screen_add_funds_display_text_enter_amount)
         state.currencyType = getString(Strings.common_text_currency_type)
