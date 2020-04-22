515109a7eb59695828e043f51bc71f293cb4ca36
Irfan <irfan@digitify.com>
2020-04-22 14:37:20 +0500

    Drag and Drop animation and motion  implement Add new recyclerview library for use of multi purpose

--- yap/src/main/java/co/yap/modules/subaccounts/account/card/SubAccountCardFragment.kt
+++ yap/src/main/java/co/yap/modules/subaccounts/account/card/SubAccountCardFragment.kt
@@ -2,44 +2,123 @@ package co.yap.modules.subaccounts.account.card
 
 import android.os.Bundle
 import android.view.*
-import androidx.databinding.ViewDataBinding
-import androidx.navigation.NavController
+import androidx.recyclerview.widget.RecyclerView
 import co.yap.BR
 import co.yap.R
 import co.yap.databinding.FragmentSubAccountCardBinding
 import co.yap.modules.dashboard.store.household.activities.HouseHoldLandingActivity
 import co.yap.modules.subaccounts.account.dashboard.SubAccountDashBoardFragmentDirections
 import co.yap.networking.customers.responsedtos.SubAccount
-import co.yap.yapcore.BaseRVAdapter
-import co.yap.yapcore.BaseViewHolder
+import co.yap.widgets.State
+import co.yap.widgets.advrecyclerview.animator.DraggableItemAnimator
+import co.yap.widgets.advrecyclerview.draggable.RecyclerViewDragDropManager
+import co.yap.widgets.advrecyclerview.utils.WrapperAdapterUtils
 import co.yap.yapcore.constants.RequestCodes
 import co.yap.yapcore.dagger.base.BaseRecyclerViewFragment
 import co.yap.yapcore.enums.AccountType
-import co.yap.yapcore.enums.CardDeliveryStatus
 import co.yap.yapcore.enums.PartnerBankStatus
-import co.yap.yapcore.helpers.DateUtils
 import co.yap.yapcore.helpers.alert
 import co.yap.yapcore.helpers.confirm
 import co.yap.yapcore.helpers.extentions.launchActivity
-import co.yap.yapcore.helpers.extentions.onDrag
-import co.yap.yapcore.helpers.extentions.startDrag
-import co.yap.yapcore.interfaces.OnItemDropListener
 import com.daimajia.androidanimations.library.Techniques
 import com.daimajia.androidanimations.library.YoYo
 import kotlinx.android.synthetic.main.item_sub_account_card.*
-import java.util.*
 
 
 class SubAccountCardFragment :
     BaseRecyclerViewFragment<FragmentSubAccountCardBinding, ISubAccountCard.State, SubAccountCardVM,
-            SubAccountCardFragment.Adapter, SubAccount>(), OnItemDropListener {
+            SubAccountAdapter, SubAccount>(), RecyclerViewDragDropManager.OnItemDragEventListener {
+    private var mWrappedAdapter: RecyclerView.Adapter<*>? = null
+    private var mRecyclerViewDragDropManager: RecyclerViewDragDropManager? = null
 
     override fun getBindingVariable() = BR.subAccountCardVM
 
     override fun getLayoutId() = R.layout.fragment_sub_account_card
     override fun postExecutePendingBindings() {
+        setRefreshEnabled(false)
         super.postExecutePendingBindings()
         setHasOptionsMenu(true)
+
+        initDragDropAdapter()
+    }
+
+    override fun handleState(state: State?) {
+        super.handleState(state)
+        recyclerView?.adapter = mWrappedAdapter
+
+    }
+
+    private fun initDragDropAdapter() {
+        // adapter.onItemClickListener = null
+
+        mRecyclerViewDragDropManager = RecyclerViewDragDropManager().apply {
+            mWrappedAdapter = createWrappedAdapter(adapter)
+            setInitiateOnLongPress(true)
+            setInitiateOnMove(false)
+            setLongPressTimeout(750)
+            dragEdgeScrollSpeed = 2.0f
+            dragStartItemAnimationDuration = 250
+            draggingItemAlpha = 1f
+            isCheckCanDropEnabled = true
+            draggingItemRotation = 15.0f
+            onItemDragEventListener = this@SubAccountCardFragment
+            itemSettleBackIntoPlaceAnimationDuration = 1000
+            itemMoveMode = RecyclerViewDragDropManager.ITEM_MOVE_MODE_DEFAULT
+        }
+        val animator = DraggableItemAnimator()
+        recyclerView?.adapter = mWrappedAdapter // requires *wrapped* adapter
+        recyclerView?.itemAnimator = animator
+        recyclerView?.let { mRecyclerViewDragDropManager?.attachRecyclerView(it) }
+    }
+
+    override fun onItemDragStarted(position: Int) {
+    }
+
+    override fun onItemDragPositionChanged(
+        fromPosition: Int,
+        toPosition: Int,
+        draggingItemHolder: RecyclerView.ViewHolder?,
+        swapTargetHolder: RecyclerView.ViewHolder?
+    ) {
+    }
+
+    override fun onItemDragFinished(fromPosition: Int, toPosition: Int, result: Boolean) {
+        val subAccount = adapter.getData()[toPosition]
+        val args = Bundle()
+        args.putParcelable(SubAccount::class.simpleName, subAccount)
+        subAccount.accountType?.let {
+            when (it) {
+                AccountType.B2C_HOUSEHOLD.name -> {
+                    navigateForwardWithAnimation(
+                        SubAccountDashBoardFragmentDirections.actionSubAccountDashBoardFragmentToHHIbanSendMoneyFragment(),
+                        args
+                    )
+
+                }
+
+            }
+        }
+    }
+
+    override fun onItemDragMoveDistanceUpdated(offsetX: Int, offsetY: Int) {
+    }
+
+    override fun onPause() {
+        mRecyclerViewDragDropManager?.cancelDrag()
+        super.onPause()
+    }
+
+    override fun onDestroyView() {
+        mRecyclerViewDragDropManager?.let {
+            it.release()
+            mRecyclerViewDragDropManager = null
+        }
+
+        mWrappedAdapter?.let {
+            WrapperAdapterUtils.releaseAll(it)
+            mWrappedAdapter = null
+        }
+        super.onDestroyView()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -68,10 +147,7 @@ class SubAccountCardFragment :
                             args
                         )
                     else showRequestDeclinedPopup(subAccount)
-
-
                 }
-
             }
         }
             ?: launchActivity<HouseHoldLandingActivity>(requestCode = RequestCodes.REQUEST_ADD_HOUSE_HOLD)
@@ -90,45 +166,6 @@ class SubAccountCardFragment :
             layout_swipe_image.visibility = View.INVISIBLE
             tv_drag_and_drop_label.visibility = View.INVISIBLE
         }
-    }
-
-    override fun onItemDrag(view: View, pos: Int, event: DragEvent, data: Any): Boolean? {
-        swipeViews(false)
-
-        return recyclerView?.let { onDrag(view, pos, event, data, this) }
-    }
-
-    override fun onItemDrop(view: View, pos: Int, data: Any) {
-        val subAccount = data as SubAccount
-        val args = Bundle()
-        args.putParcelable(SubAccount::class.simpleName, subAccount)
-        subAccount.accountType?.let {
-            when (it) {
-                AccountType.B2C_HOUSEHOLD.name -> {
-                    navigateForwardWithAnimation(
-                        SubAccountDashBoardFragmentDirections.actionSubAccountDashBoardFragmentToHHIbanSendMoneyFragment(),
-                        args
-                    )
-
-                }
-
-            }
-        }
-    }
-
-    override fun onItemExited(view: View) {
-        view.background = context?.getDrawable(R.drawable.card_border_container)
-    }
-
-    override fun onItemEntered(view: View) {
-        view.background = context?.getDrawable(R.drawable.bg_gray_rounded_border)
-    }
-
-    override fun onItemLongClick(view: View, pos: Int, id: Long, data: Any): Boolean? {
-        if (pos == 0) {
-            return context?.let { startDrag(view, it) }
-        }
-        return true
     }
 
     private fun showRequestDeclinedPopup(data: SubAccount) {
@@ -154,21 +191,6 @@ class SubAccountCardFragment :
             ),
             getString(R.string.screen_house_hold_sub_account_popup_remove_refund_button_text)
         ) { viewModel.RemoveRefundHouseHoldUser(data) }
-    }
-
-    class Adapter(mValue: MutableList<SubAccount>, navigation: NavController?) :
-        BaseRVAdapter<SubAccount, SubAccountCardItemVM, BaseViewHolder<SubAccount, SubAccountCardItemVM>>(
-            mValue, navigation
-        ) {
-        override fun getLayoutId(viewType: Int) = getViewModel(viewType).layoutRes()
-        override fun getViewHolder(
-            view: View,
-            viewModel: SubAccountCardItemVM,
-            mDataBinding: ViewDataBinding, viewType: Int
-        ) = BaseViewHolder(view, viewModel, mDataBinding)
-
-        override fun getViewModel(viewType: Int) = SubAccountCardItemVM()
-        override fun getVariableId() = BR.subAccountCardItemVm
     }
 
 }
