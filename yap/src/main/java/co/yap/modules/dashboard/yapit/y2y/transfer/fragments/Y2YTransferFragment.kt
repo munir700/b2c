f7277699fb8a78617e4765fae84a5e7fb4957bca
Faheem Riaz <faheem@digitify.com>
2020-05-05 14:00:58 +0500

    YM-5043 Remaining limit amount formatted in case of y2y fund transfer

--- yap/src/main/java/co/yap/modules/dashboard/yapit/y2y/transfer/fragments/Y2YTransferFragment.kt
+++ yap/src/main/java/co/yap/modules/dashboard/yapit/y2y/transfer/fragments/Y2YTransferFragment.kt
@@ -93,6 +93,9 @@ class Y2YTransferFragment : Y2YBaseFragment<IY2YFundsTransfer.ViewModel>(), IY2Y
     private val enterAmountObserver = Observer<String> {
         viewModel.updateFees(it)
         when {
+            it.isBlank() -> {
+                viewModel.state.valid = false
+            }
             isBalanceAvailable(it) -> showErrorSnackBar(
                 viewModel.state.errorDescription,
                 Snackbar.LENGTH_INDEFINITE
@@ -106,7 +109,7 @@ class Y2YTransferFragment : Y2YBaseFragment<IY2YFundsTransfer.ViewModel>(), IY2Y
                 viewModel.state.valid = false
                 showErrorSnackBar(viewModel.state.errorDescription, Snackbar.LENGTH_INDEFINITE)
             }
-            viewModel.enteredAmount.value?.toDoubleOrNull() ?: 0.0 < viewModel.state.minLimit || viewModel.enteredAmount.value?.toDoubleOrNull() ?: 0.0 > viewModel.state.maxLimit -> {
+            it.isNotBlank() && viewModel.enteredAmount.value?.toDoubleOrNull() ?: 0.0 < viewModel.state.minLimit || viewModel.enteredAmount.value?.toDoubleOrNull() ?: 0.0 > viewModel.state.maxLimit -> {
                 setUpperLowerLimitError()
                 viewModel.state.amountBackground =
                     requireContext().resources.getDrawable(
@@ -214,7 +217,7 @@ class Y2YTransferFragment : Y2YBaseFragment<IY2YFundsTransfer.ViewModel>(), IY2Y
                         viewModel.state.errorDescription = Translator.getString(
                             requireContext(),
                             Strings.common_display_text_daily_limit_error
-                        ).format(remainingDailyLimit)
+                        ).format(remainingDailyLimit.toString().toFormattedCurrency())
                         return enteredAmount > remainingDailyLimit
                     } ?: return false
                 } ?: return false
