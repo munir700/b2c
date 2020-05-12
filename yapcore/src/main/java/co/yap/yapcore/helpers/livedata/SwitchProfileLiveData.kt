e06dc7e3f1434ceaaed273a525aeda2e2319377d
HabibaNaushahi <habiba@digitify.com>
2020-05-12 09:32:04 +0500

    Extra logs removed

--- yapcore/src/main/java/co/yap/yapcore/helpers/livedata/SwitchProfileLiveData.kt
+++ yapcore/src/main/java/co/yap/yapcore/helpers/livedata/SwitchProfileLiveData.kt
@@ -1,6 +1,5 @@
 package co.yap.yapcore.helpers.livedata
 
-import android.util.Log
 import androidx.annotation.MainThread
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
@@ -8,7 +7,8 @@ import co.yap.networking.authentication.AuthRepository
 import co.yap.networking.customers.responsedtos.AccountInfo
 import co.yap.networking.models.RetroApiResponse
 
-class SwitchProfileLiveData(private val uuid: String?, private val owner:LifecycleOwner) : LiveDataCallAdapter<AccountInfo?>() {
+class SwitchProfileLiveData(private val uuid: String?, private val owner: LifecycleOwner) :
+    LiveDataCallAdapter<AccountInfo?>() {
     private val authRepository: AuthRepository = AuthRepository
 
     override fun onActive() {
@@ -16,9 +16,7 @@ class SwitchProfileLiveData(private val uuid: String?, private val owner:Lifecyc
         launch {
             when (val response = uuid?.let { authRepository.switchProfile(it) }) {
                 is RetroApiResponse.Success -> {
-                    Log.d("TAG", "Switch Profile Success")
                     GetAccountInfoLiveData.get().observe(owner, Observer<AccountInfo?> {
-                        Log.d("TAG", "Account Info: "+(it as AccountInfo).accountType)
                         value = it
                     })
                 }
@@ -39,7 +37,8 @@ class SwitchProfileLiveData(private val uuid: String?, private val owner:Lifecyc
 
         @MainThread
         fun get(uuid: String, owner: LifecycleOwner): SwitchProfileLiveData {
-            sInstance = if (::sInstance.isInitialized) sInstance else SwitchProfileLiveData(uuid, owner)
+            sInstance =
+                if (::sInstance.isInitialized) sInstance else SwitchProfileLiveData(uuid, owner)
             return sInstance
         }
     }
