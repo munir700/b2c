dcba6a6ccaabdce341d8f1455dd929c13ffb7a8d
Najia Bokhari <najia@digitify.com>
2020-06-19 15:41:41 +0500

    YH-693 add HH dummy tracking events

--- yapcore/src/main/java/co/yap/yapcore/adjust/AdjustEvents.kt
+++ yapcore/src/main/java/co/yap/yapcore/adjust/AdjustEvents.kt
@@ -14,6 +14,20 @@ enum class AdjustEvents(val type: String) {
     SIGN_UP_START("73mcc8"),
     TOP_UP_END("jw0tz5"),
     TOP_UP_START("cadxmk"),
-    INVITER("sgy2ni");
+    INVITER("sgy2ni"),
 
+    // House Hold Events
+    HOUSE_HOLD_MAIN_USER_SUBSCRIPTION("temp"),
+    HOUSE_HOLD_MAIN_SUB_PLAN_CONFIRM("temp"),
+    HOUSE_HOLD_MAIN_PLAN_NAME("temp"),
+    HOUSE_HOLD_MAIN_SHARE("temp"),
+    ONBOARDING_START_NEW_HH_USER("temp"),
+    ONBOARDING_NEW_HH_USER_MAIN_USER_PHONE_CORRECT("temp"),
+    ONBOARDING_NEW_HH_USER_EMAIL("temp"),
+    ONBOARDING_NEW_HH_USER_SIGNUP("temp"),
+    ONBOARDING_NEW_HH_USER_EID("temp"),
+    ONBOARDING_NEW_HH_USER_EID_DECLINED("temp"),
+    ONBOARD_NEW_HH_USER_ONBOARDING_SUCCESS("temp"),
+    HH_USER_ACCOUNT_ACTIVE("temp"),
+    HH_USER_EXISTING_ACCOUNT_ACTIVE("temp");
 }
