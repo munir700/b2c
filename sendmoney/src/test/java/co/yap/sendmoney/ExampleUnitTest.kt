36ae306866f99df20f7caa35de532b87439cd5aa
Sohail Zahid <sohail@digitify.com>
2020-07-04 17:53:48 +0500

    Amount test case added.

--- sendmoney/src/test/java/co/yap/sendmoney/ExampleUnitTest.kt
+++ sendmoney/src/test/java/co/yap/sendmoney/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
-package co.yap.sendmoney
+package co.yap.sendMoney
 
+import co.yap.yapcore.helpers.extentions.roundVal
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,6 +12,13 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertEquals(4.1, 2 + 2)
+    }
+
+    @Test
+    fun isValidRoundUp() {
+        val value: Double = 101.385
+        value.roundVal().toString()
+        assertEquals("101.39", value.roundVal().toString())
     }
 }
