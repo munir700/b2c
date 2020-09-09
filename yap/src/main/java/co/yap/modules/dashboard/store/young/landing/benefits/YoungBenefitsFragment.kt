e341a16a48db259331f5ff2a6608beaac32b53c9
Gulwisha Ahmed <gulwisha.ahmed@digitify.com>
2020-09-09 10:27:07 +0500

    YG-83 Android Yap Young Benefits Complete

--- yap/src/main/java/co/yap/modules/dashboard/store/young/landing/benefits/YoungBenefitsFragment.kt
+++ yap/src/main/java/co/yap/modules/dashboard/store/young/landing/benefits/YoungBenefitsFragment.kt
@@ -1,37 +1,16 @@
 package co.yap.modules.dashboard.store.young.landing.benefits
 
-import android.os.Bundle
-import android.view.View
 import co.yap.R
 import co.yap.BR
 import co.yap.databinding.FragmentYoungBenefitsBinding
 import co.yap.modules.dashboard.store.young.landing.benefits.adapter.YoungBenefitsAdapter
 import co.yap.modules.dashboard.store.young.landing.benefits.adapter.YoungBenefitsModel
-import co.yap.networking.store.responsedtos.Store
-import co.yap.yapcore.SingleClickEvent
 import co.yap.yapcore.dagger.base.BaseRecyclerViewFragment
-import androidx.lifecycle.Observer
+
 class YoungBenefitsFragment :
     BaseRecyclerViewFragment<FragmentYoungBenefitsBinding, IYoungBenefits.State, YoungBenefitsVM, YoungBenefitsAdapter, YoungBenefitsModel>() {
     override fun getBindingVariable() = BR.youngBenefitsVM
     override fun getLayoutId() = R.layout.fragment_young_benefits
- 
-    private fun onClick(it: Int?) {
-    }
-    override fun onItemClick(view: View, data: Any, pos: Int) {
-        if (data is Store) {
-            if (data.name == "YAP Young Benefits") {
-                viewModel.clickEvent.setPayload(
-                    SingleClickEvent.AdaptorPayLoadHolder(
-                        view,
-                        data,
-                        pos
-                    )
-                )
-                viewModel.clickEvent.setValue(view.id)
-            }
-        }
-    }
     override fun onDestroyView() {
         viewModel.clickEvent.removeObservers(this)
         super.onDestroyView()
