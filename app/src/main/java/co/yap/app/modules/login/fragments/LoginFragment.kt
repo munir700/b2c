86c6d8e8bbf37e30a5eda3dd4596cad761e91f41
Gulwisha Ahmed <gulwisha.ahmed@digitify.com>
2020-09-10 17:30:27 +0500

    YG-41 Android Yap Young Confirm Relation Ship Screen UI/UX design Class Structure creation Navigation

--- app/src/main/java/co/yap/app/modules/login/fragments/LoginFragment.kt
+++ app/src/main/java/co/yap/app/modules/login/fragments/LoginFragment.kt
@@ -11,6 +11,7 @@ import co.yap.app.R
 import co.yap.app.main.MainChildFragment
 import co.yap.app.modules.login.interfaces.ILogin
 import co.yap.app.modules.login.viewmodels.LoginViewModel
+import co.yap.modules.dashboard.store.young.confirmrelationship.YoungConfirmRelationshipFragment
 import co.yap.modules.dashboard.store.young.contact.fragments.YoungContactDetailsFragment
 import co.yap.yapcore.constants.Constants.KEY_IS_USER_LOGGED_IN
 import co.yap.yapcore.helpers.SharedPreferenceManager
@@ -72,7 +73,7 @@ class LoginFragment : MainChildFragment<ILogin.ViewModel>(), ILogin.View {
 //                    )
 //                }
 //            }
-            startFragment(YoungContactDetailsFragment::class.java.name)
+            startFragment(YoungConfirmRelationshipFragment::class.java.name)
         }
     }
 
