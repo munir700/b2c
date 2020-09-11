9e2eb211cb0ed261bf9a562f16b6755402060015
Gulwisha Ahmed <gulwisha.ahmed@digitify.com>
2020-09-11 17:15:20 +0500

    YG-45 Android Yap Young Edit Card UI/UX design Class Structure creation Navigation

--- yap/src/main/java/co/yap/modules/dashboard/store/young/card/IYoungCardEditDetails.kt
+++ yap/src/main/java/co/yap/modules/dashboard/store/young/card/IYoungCardEditDetails.kt
@@ -3,7 +3,6 @@ package co.yap.modules.dashboard.store.young.card
 import androidx.databinding.ObservableField
 import androidx.lifecycle.MutableLiveData
 import co.yap.networking.cards.responsedtos.Address
-import co.yap.networking.customers.household.requestdtos.SignUpFss
 import co.yap.networking.customers.responsedtos.HouseHoldCardsDesign
 import co.yap.yapcore.IBase
 import co.yap.yapcore.SingleClickEvent
@@ -13,14 +12,12 @@ interface IYoungCardEditDetails {
     }
 
     interface ViewModel : IBase.ViewModel<State> {
+        val adapter: ObservableField<YoungCardEditAdapter>?
         val clickEvent: SingleClickEvent
         fun handlePressOnClick(id: Int)
-        fun getCardsDesignListRequest(accountType: String,apiResponse: ((MutableList<HouseHoldCardsDesign>?) -> Unit?)?)
-        fun orderHouseHoldPhysicalCardRequest(address: Address, apiResponse: ((Boolean) -> Unit?)?)
     }
 
     interface State : IBase.State {
-        var address: MutableLiveData<Address>?
         var designCode: MutableLiveData<String>?
         var cardDesigns: MutableLiveData<MutableList<HouseHoldCardsDesign>>?
     }
--- yap/src/main/java/co/yap/modules/dashboard/store/young/card/YoungCardEditDetailsVM.kt
+++ yap/src/main/java/co/yap/modules/dashboard/store/young/card/YoungCardEditDetailsVM.kt
@@ -1,4 +1,23 @@
 package co.yap.modules.dashboard.store.young.card
 
-class YoungCardEditDetailsVM {
+import android.os.Bundle
+import androidx.databinding.ObservableField
+import androidx.navigation.NavController
+import co.yap.yapcore.SingleClickEvent
+import co.yap.yapcore.dagger.base.viewmodel.DaggerBaseViewModel
+import javax.inject.Inject
+
+class YoungCardEditDetailsVM @Inject constructor(
+    override val state: IYoungCardEditDetails.State
+) : DaggerBaseViewModel<IYoungCardEditDetails.State>(), IYoungCardEditDetails.ViewModel {
+    override val adapter: ObservableField<YoungCardEditAdapter>? = ObservableField()
+
+    override val clickEvent: SingleClickEvent = SingleClickEvent()
+
+    override fun handlePressOnClick(id: Int) {
+        clickEvent.setValue(id)
+    }
+
+    override fun onFirsTimeUiCreate(bundle: Bundle?, navigation: NavController?) {
+    }
 }
