02fd1308822fb757679a551b361597ce73febdfd
Gulwisha Ahmed <gulwisha.ahmed@digitify.com>
2020-09-11 11:02:11 +0500

    YG-98 Android Yap Young Confirm Contact Screen Complete UI/UX design Class Structure creation Navigation

--- translation/src/main/java/co/yap/translation/Strings.kt
+++ translation/src/main/java/co/yap/translation/Strings.kt
@@ -1912,8 +1912,6 @@ object Strings {
     const val screen_yap_young_label_text: String =
         "screen_yap_young_label_text"
 
-
-
     // Young Contact Details
     const val screen_young_contact_details_description_text: String =
         "screen_young_contact_details_description_text"
