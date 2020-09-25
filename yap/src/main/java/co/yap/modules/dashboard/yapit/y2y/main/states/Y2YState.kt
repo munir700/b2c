144a5f4740837e4aaf53459927b9cdd4e75e3bfe
Tanveer Munir <tanveer.munir@digitify.com>
2020-09-25 12:44:51 +0500

    YM-6354 Tool bar changed in yap to yap flow screens

--- yap/src/main/java/co/yap/modules/dashboard/yapit/y2y/main/states/Y2YState.kt
+++ yap/src/main/java/co/yap/modules/dashboard/yapit/y2y/main/states/Y2YState.kt
@@ -8,14 +8,6 @@ import co.yap.yapcore.BaseState
 class Y2YState : BaseState(), IY2Y.State {
 
     @get:Bindable
-    override var tootlBarTitle: String = ""
-        set(value) {
-            field = value
-            notifyPropertyChanged(BR.tootlBarTitle)
-
-        }
-
-    @get:Bindable
     override var tootlBarVisibility: Int = 0x00000000
         set(value) {
             field = value
@@ -24,13 +16,14 @@ class Y2YState : BaseState(), IY2Y.State {
         }
 
     @get:Bindable
-    override var rightButtonVisibility: Int=0x00000000
+    override var rightButtonVisibility: Boolean = true
         set(value) {
             field = value
             notifyPropertyChanged(BR.rightButtonVisibility)
         }
+
     @get:Bindable
-    override var leftButtonVisibility:Int=0x00000000
+    override var leftButtonVisibility: Boolean = true
         set(value) {
             field = value
             notifyPropertyChanged(BR.leftButtonVisibility)
