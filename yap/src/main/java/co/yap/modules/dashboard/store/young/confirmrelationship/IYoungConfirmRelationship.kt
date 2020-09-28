4590d3e6093a534da0a11f51b0e8ed8c865e142f
Gulwisha Ahmed <gulwisha.ahmed@digitify.com>
2020-09-28 17:49:45 +0500

    YG-145 Bug fix- Next Button Validation Working

--- yap/src/main/java/co/yap/modules/dashboard/store/young/confirmrelationship/IYoungConfirmRelationship.kt
+++ yap/src/main/java/co/yap/modules/dashboard/store/young/confirmrelationship/IYoungConfirmRelationship.kt
@@ -6,6 +6,7 @@ import co.yap.yapcore.IBase
 interface IYoungConfirmRelationship {
     interface State : IBase.State {
         var realtion: MutableLiveData<String>
+        var valid: MutableLiveData<Boolean>
     }
     interface ViewModel : IBase.ViewModel<State>
     interface View : IBase.View<ViewModel>
