252f276bb700540f081d078a0f45f9b337421ea1
Faheem Riaz <faheem@digitify.com>
2020-10-02 10:57:49 +0500

    YM-6962 Feature enums added need to be blocked in case of TDC

---
+++ yapcore/src/main/java/co/yap/yapcore/enums/FeatureSet.kt
@@ -0,0 +1,16 @@
+package co.yap.yapcore.enums
+
+enum class FeatureSet {
+    DOMESTIC,
+    UAEFTS,
+    CBWSI,
+    RMT,
+    SWIFT,
+    TOP_UP_BY_EXTERNAL_CARD,
+    ADD_FUNDS,
+    REMOVE_FUNDS,
+    Y2Y_TRANSFER,
+    UNFREEZE_CARD,
+    CHANGE_PIN,
+    FORGOT_PIN
+}
