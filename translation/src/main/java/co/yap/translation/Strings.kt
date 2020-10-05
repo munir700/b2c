039382622fbe4f89a7a726f7a05422232271a1b2
Gulwisha Ahmed <gulwisha.ahmed@digitify.com>
2020-10-05 13:06:36 +0500

    YM-6616 Screen Fixing

--- translation/src/main/java/co/yap/translation/Strings.kt
+++ translation/src/main/java/co/yap/translation/Strings.kt
@@ -428,10 +428,16 @@ object Strings {
     const val screen_passcode_success_heading: String =
         "screen_passcode_success_heading"
 
+    //Change Passcode screen
+    const val screen_change_passcode_success_display_text_sub_heading: String =
+        "screen_change_passcode_success_display_text_sub_heading"
+
 
     //Create new passcode screen
     const val screen_create_new_passcode_button_text: String =
         "screen_create_new_passcode_button_text"
+    const val screen_create_passcode_display_heading: String =
+        "screen_create_passcode_display_heading"
 
     //Yap Store screen
     const val screen_yap_store_display_text_title: String = "screen_yap_store_display_text_title"
