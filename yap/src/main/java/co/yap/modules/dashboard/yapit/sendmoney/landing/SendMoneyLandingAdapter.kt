b63ca8fe6e5d22669801500527014ecb97da3ded
Gulwisha Ahmed <gulwisha.ahmed@digitify.com>
2020-11-05 17:55:47 +0500

    YM-8032 Send Money UI Working

--- yap/src/main/java/co/yap/modules/dashboard/yapit/sendmoney/landing/SendMoneyLandingAdapter.kt
+++ yap/src/main/java/co/yap/modules/dashboard/yapit/sendmoney/landing/SendMoneyLandingAdapter.kt
@@ -2,20 +2,52 @@ package co.yap.modules.dashboard.yapit.sendmoney.landing
 
 import android.content.Context
 import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import co.yap.R
+import co.yap.databinding.ItemYapItSendMoneyLandingBinding
 import co.yap.modules.dashboard.yapit.addmoney.landing.AddMoneyLandingOptions
+import co.yap.modules.dashboard.yapit.sendmoney.landing.viewmodels.SendMoneyLandingItemViewModel
 import co.yap.yapcore.BaseBindingRecyclerAdapter
+import co.yap.yapcore.helpers.Utils
+import co.yap.yapcore.interfaces.OnItemClickListener
 
 class SendMoneyLandingAdapter(
     context: Context,
     private val list: MutableList<AddMoneyLandingOptions>
 ) :
     BaseBindingRecyclerAdapter<AddMoneyLandingOptions, RecyclerView.ViewHolder>(list) {
+    private var dimensions: IntArray = Utils.getCardDimensions(context, 43, 20)
+
     override fun onCreateViewHolder(binding: ViewDataBinding): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        return ViewHolder(
+            binding as ItemYapItSendMoneyLandingBinding
+        )
     }
 
-    override fun getLayoutIdForViewType(viewType: Int): Int {
-        TODO("Not yet implemented")
+    override fun getLayoutIdForViewType(viewType: Int): Int =
+        R.layout.item_yap_it_send_money_landing
+
+    class ViewHolder(private val itemYapItSendMoneyBinding: ItemYapItSendMoneyLandingBinding) :
+        RecyclerView.ViewHolder(itemYapItSendMoneyBinding.root) {
+        fun onBind(
+            addMoneyOptions: AddMoneyLandingOptions,
+            position: Int,
+            dimensions: IntArray,
+            onItemClickListener: OnItemClickListener?
+        ) {
+            val params =
+                itemYapItSendMoneyBinding.clMain.layoutParams as GridLayoutManager.LayoutParams
+            params.width = dimensions[0]
+            params.height = dimensions[1]
+            itemYapItSendMoneyBinding.clMain.layoutParams = params
+            itemYapItSendMoneyBinding.viewModel =
+                SendMoneyLandingItemViewModel(
+                    addMoneyOptions,
+                    position,
+                    onItemClickListener
+                )
+            itemYapItSendMoneyBinding.executePendingBindings()
+        }
     }
 }
--- yap/src/main/java/co/yap/modules/dashboard/yapit/sendmoney/landing/viewmodels/SendMoneyLandingItemViewModel.kt
+++ yap/src/main/java/co/yap/modules/dashboard/yapit/sendmoney/landing/viewmodels/SendMoneyLandingItemViewModel.kt
@@ -1,4 +1,15 @@
 package co.yap.modules.dashboard.yapit.sendmoney.landing.viewmodels
 
-class SendMoneyLandingItemViewModel {
+import android.view.View
+import co.yap.modules.dashboard.yapit.addmoney.landing.AddMoneyLandingOptions
+import co.yap.yapcore.interfaces.OnItemClickListener
+
+class SendMoneyLandingItemViewModel(
+    var addMoneyOptions: AddMoneyLandingOptions,
+    var position: Int,
+    private val onItemClickListener: OnItemClickListener?
+) {
+    fun handlePressOnView(view: View) {
+        onItemClickListener?.onItemClick(view, addMoneyOptions, position)
+    }
 }
