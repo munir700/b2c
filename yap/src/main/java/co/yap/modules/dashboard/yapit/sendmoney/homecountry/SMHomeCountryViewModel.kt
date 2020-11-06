85862a3e1585c0553d7370b201808c909f795bf3
habiba <habiba@digitify.com>
2020-11-06 17:47:16 +0500

    YM-8100 title updated

--- yap/src/main/java/co/yap/modules/dashboard/yapit/sendmoney/homecountry/SMHomeCountryViewModel.kt
+++ yap/src/main/java/co/yap/modules/dashboard/yapit/sendmoney/homecountry/SMHomeCountryViewModel.kt
@@ -1,9 +1,15 @@
 package co.yap.modules.dashboard.yapit.sendmoney.homecountry
 
 import android.app.Application
+import co.yap.R
 import co.yap.yapcore.BaseViewModel
 
 class SMHomeCountryViewModel (application: Application) : BaseViewModel<ISMHomeCountry.State>(application), ISMHomeCountry.ViewModel {
     override val state: ISMHomeCountry.State = SMHomeCountryState()
+
+    override fun onCreate() {
+        super.onCreate()
+        state.toolbarTitle = getString(R.string.screen_send_money_home_title)
+    }
 
 }
