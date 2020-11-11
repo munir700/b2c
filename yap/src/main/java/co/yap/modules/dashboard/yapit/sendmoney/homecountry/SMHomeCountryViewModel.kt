33e52670f083ee0193ddf394f9924d887fd8c5d9
Faheem Riaz <faheem@digitify.com>
2020-11-11 14:14:38 +0500

    YM-8803 countries api integrated

--- yap/src/main/java/co/yap/modules/dashboard/yapit/sendmoney/homecountry/SMHomeCountryViewModel.kt
+++ yap/src/main/java/co/yap/modules/dashboard/yapit/sendmoney/homecountry/SMHomeCountryViewModel.kt
@@ -2,6 +2,7 @@ package co.yap.modules.dashboard.yapit.sendmoney.homecountry
 
 import android.app.Application
 import co.yap.R
+import co.yap.countryutils.country.Country
 import co.yap.networking.customers.CustomersRepository
 import co.yap.networking.interfaces.IRepositoryHolder
 import co.yap.networking.models.RetroApiResponse
@@ -9,13 +10,14 @@ import co.yap.widgets.recent_transfers.CoreRecentTransferAdapter
 import co.yap.yapcore.BaseViewModel
 import co.yap.yapcore.Dispatcher
 import co.yap.yapcore.SingleClickEvent
+import co.yap.yapcore.managers.SessionManager
 
 class SMHomeCountryViewModel(application: Application) :
     BaseViewModel<ISMHomeCountry.State>(application), ISMHomeCountry.ViewModel,
     IRepositoryHolder<CustomersRepository> {
     override val clickEvent: SingleClickEvent = SingleClickEvent()
     override val repository: CustomersRepository = CustomersRepository
-
+    override var homeCountry: Country? = null
     override var recentsAdapter: CoreRecentTransferAdapter = CoreRecentTransferAdapter(
         context,
         mutableListOf()
@@ -29,13 +31,14 @@ class SMHomeCountryViewModel(application: Application) :
 
     override fun onCreate() {
         super.onCreate()
+        homeCountry = SessionManager.getCountries()
+            .find { it.isoCountryCode2Digit == SessionManager.user?.currentCustomer?.homeCountry ?: "" }
         getHomeCountryRecentBeneficiaries()
         state.toolbarTitle = getString(R.string.screen_send_money_home_title)
         state.rightButtonText.set(getString(R.string.screen_send_money_home_display_text_compare))
-        state.name?.set("Canada")
-        state.countryCode?.set("")
+        state.name?.set(homeCountry?.getName())
         state.rate?.set("0.357014")
-        state.symbol?.set("CAD")
+        state.symbol?.set(homeCountry?.getCurrency()?.code)
         state.time?.set("04/10/2020, 2:30 PM")
     }
 
