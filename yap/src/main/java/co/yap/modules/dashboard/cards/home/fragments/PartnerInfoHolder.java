daca43293e98dc6f1c4c811e2f8f8345081f6d55
Najia Bokhari <najia@digitify.com>
2020-11-26 12:16:58 +0500

    YM-7739 add partner info

---
+++ yap/src/main/java/co/yap/modules/dashboard/cards/home/fragments/PartnerInfoHolder.java
@@ -0,0 +1,36 @@
+package co.yap.modules.dashboard.cards.home.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+import com.samsung.android.sdk.samsungpay.v2.PartnerInfo;
+import com.samsung.android.sdk.samsungpay.v2.SamsungPay;
+import com.samsung.android.sdk.samsungpay.v2.SpaySdk;
+
+public class PartnerInfoHolder {
+    private static PartnerInfoHolder sInstance;
+    private PartnerInfo mPartnerInfo;
+
+    private PartnerInfoHolder(Context context) {
+        String serviceId = "553f9a2fbd0244e4a9c7ab";
+//        String serviceId = context.getResources().getString(R.string.gradle_product_id);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(SamsungPay.PARTNER_SERVICE_TYPE, SpaySdk.ServiceType.APP2APP.toString());
+
+        mPartnerInfo = new PartnerInfo(serviceId, bundle);
+    }
+
+    public static synchronized PartnerInfoHolder getInstance(@NonNull Context context) {
+        if (sInstance == null) {
+            sInstance = new PartnerInfoHolder(context);
+        }
+        return sInstance;
+    }
+
+    public PartnerInfo getPartnerInfo() {
+        return mPartnerInfo;
+    }
+}
