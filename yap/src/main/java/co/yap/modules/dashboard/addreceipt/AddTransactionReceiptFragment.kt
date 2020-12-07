3eff33817a159d4ce3f7997981e8096410ec714d
Irfan <irfan@digitify.com>
2020-12-07 13:35:35 +0500

    YM-9279 take a photo of Recepit implemnet camera permission handled

--- yap/src/main/java/co/yap/modules/dashboard/addreceipt/AddTransactionReceiptFragment.kt
+++ yap/src/main/java/co/yap/modules/dashboard/addreceipt/AddTransactionReceiptFragment.kt
@@ -1,16 +1,78 @@
 package co.yap.modules.dashboard.addreceipt
 
+import android.graphics.PointF
+import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.ViewModelProviders
 import co.yap.BR
 import co.yap.R
+import co.yap.databinding.FragmentAddTransactionReceiptBinding
 import co.yap.yapcore.BaseBindingFragment
+import co.yap.yapcore.helpers.extentions.createTempFile
+import com.digitify.identityscanner.camera.CameraException
+import com.digitify.identityscanner.camera.CameraListener
+import com.digitify.identityscanner.camera.CameraOptions
+import com.digitify.identityscanner.camera.PictureResult
 
 class AddTransactionReceiptFragment : BaseBindingFragment<IAddTransactionReceipt.ViewModel>(),
-    IAddTransactionReceipt.View {
+    IAddTransactionReceipt.View, CameraListener {
     override fun getBindingVariable() = BR.viewModel
 
     override fun getLayoutId() = R.layout.fragment_add_transaction_receipt
     override val viewModel: IAddTransactionReceipt.ViewModel
         get() = ViewModelProviders.of(this).get(AddTransactionReceiptViewModel::class.java)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getBindingView().camera.setLifecycleOwner(this)
+        getBindingView().camera.setLifecycleOwner(this)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        getBindingView().camera.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun getBindingView() = (viewDataBinding as FragmentAddTransactionReceiptBinding)
+    override fun onCaptureProcessCompleted(filename: String?) {
+
+    }
+
+    override fun onCameraOpened(options: CameraOptions) {
+    }
+
+    override fun onCameraClosed() {
+    }
+
+    override fun onCameraError(exception: CameraException) {
+    }
+
+    override fun onPictureTaken(result: PictureResult) {
+        result.toFile(
+            requireContext().createTempFile(".jpg")
+        ) { }
+    }
+
+    override fun onOrientationChanged(orientation: Int) {
+    }
+
+    override fun onAutoFocusStart(point: PointF) {
+    }
+
+    override fun onAutoFocusEnd(successful: Boolean, point: PointF) {
+    }
+
+    override fun onZoomChanged(newValue: Float, bounds: FloatArray, fingers: Array<out PointF>?) {
+    }
+
+    override fun onExposureCorrectionChanged(
+        newValue: Float,
+        bounds: FloatArray,
+        fingers: Array<out PointF>?
+    ) {
+    }
 }
--- yap/src/main/java/co/yap/modules/dashboard/addreceipt/IAddTransactionReceipt.kt
+++ yap/src/main/java/co/yap/modules/dashboard/addreceipt/IAddTransactionReceipt.kt
@@ -4,10 +4,10 @@ import co.yap.yapcore.IBase
 
 class IAddTransactionReceipt {
     interface View : IBase.View<ViewModel> {
+        fun onCaptureProcessCompleted(filename: String?)
     }
 
     interface ViewModel : IBase.ViewModel<State> {
-
     }
 
     interface State : IBase.State {
