49697129f1c51175788c58d1e66c4bf0d3974f3e
Gulwisha Ahmed <gulwisha.ahmed@digitify.com>
2020-12-11 18:14:31 +0500

    YM-9749 Yap For  You Manager class modification YapForYouManager.kt Strings.kt strings.xml

--- yap/src/main/java/co/yap/modules/dashboard/more/yapforyou/YapForYouManager.kt
+++ yap/src/main/java/co/yap/modules/dashboard/more/yapforyou/YapForYouManager.kt
@@ -1,11 +1,18 @@
 package co.yap.modules.dashboard.more.yapforyou
 
+import android.content.Context
 import co.yap.R
 import co.yap.modules.dashboard.more.yapforyou.models.YapForYouDataModel
+import co.yap.translation.Strings
+import co.yap.translation.Translator.getString
 import co.yap.yapcore.enums.YFYAchievementType
 
-class YapForYouManager {
+class YapForYouManager(val context: Context) {
     private var yfyContentMap: HashMap<String, YapForYouDataModel>? = null
+
+    val contxt: Context by lazy {
+        context
+    }
 
     fun initiateHasMap(tag: String) {
         when (tag) {
@@ -41,31 +48,31 @@ class YapForYouManager {
         val hashMap: HashMap<String, YapForYouDataModel> = HashMap()
         hashMap[YFYAchievementType.OPEN_YAP_ACCOUNT.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description =getString(contxt,Strings.screen_yfy_open_yap_account_description),
             image = R.drawable.card_spare,
             buttonVisibility = false,
             buttonTitle = ""
         )
         hashMap[YFYAchievementType.TOP_UP.type] = YapForYouDataModel(
             title = "",
-            description = "",
-            image = R.drawable.card_spare,
-            buttonVisibility = false,
-            buttonTitle = ""
-        )
-        hashMap[YFYAchievementType.SET_PROFILE_PICTURE.type] = YapForYouDataModel(
-            title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_set_your_pin_description),
             image = R.drawable.card_spare,
             buttonVisibility = false,
             buttonTitle = ""
         )
         hashMap[YFYAchievementType.ADD_CARD.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_add_money_to_account_description),
             image = R.drawable.card_spare,
-            buttonVisibility = false,
-            buttonTitle = ""
+            buttonVisibility = true,
+            buttonTitle = getString(context,Strings.screen_yfy_add_money_to_account_button_label)
+        )
+        hashMap[YFYAchievementType.SET_PROFILE_PICTURE.type] = YapForYouDataModel(
+            title = "",
+            description = getString(context,Strings.screen_yfy_set_a_profile_photo_description),
+            image = R.drawable.card_spare,
+            buttonVisibility = true,
+            buttonTitle = getString(context,Strings.screen_yfy_set_a_profile_photo_button_label)
         )
         return hashMap
     }
@@ -74,28 +81,28 @@ class YapForYouManager {
         val hashMap: HashMap<String, YapForYouDataModel> = HashMap()
         hashMap[YFYAchievementType.USE_YAP_LOCALLY.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_use_yap_locally_description),
             image = R.drawable.card_spare,
             buttonVisibility = false,
             buttonTitle = ""
         )
         hashMap[YFYAchievementType.FREEZE_AND_UNFREEZE.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_freeze_unfreeze_card_description),
             image = R.drawable.card_spare,
-            buttonVisibility = false,
-            buttonTitle = ""
+            buttonVisibility = true,
+            buttonTitle = getString(context,Strings.screen_yfy_freeze_unfreeze_card_button_label)
         )
         hashMap[YFYAchievementType.SPEND_HUNDRED_AED.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_spend_aed_description),
             image = R.drawable.card_spare,
             buttonVisibility = false,
             buttonTitle = ""
         )
         hashMap[YFYAchievementType.EXPLORE_CARD_CONTROLS.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_explore_card_control_description),
             image = R.drawable.card_spare,
             buttonVisibility = false,
             buttonTitle = ""
@@ -107,28 +114,28 @@ class YapForYouManager {
         val hashMap: HashMap<String, YapForYouDataModel> = HashMap()
         hashMap[YFYAchievementType.INVITE_A_FRIEND.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_invite_a_friend_description),
             image = R.drawable.card_spare,
-            buttonVisibility = false,
-            buttonTitle = ""
+            buttonVisibility = true,
+            buttonTitle = getString(context,Strings.screen_yfy_invite_a_friend_button_label)
         )
         hashMap[YFYAchievementType.MAKE_Y2Y_TRANSFER.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_send_money_to_someone_description),
             image = R.drawable.card_spare,
             buttonVisibility = false,
             buttonTitle = ""
         )
         hashMap[YFYAchievementType.SPLIT_BILLS.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_split_bills_description),
             image = R.drawable.card_spare,
             buttonVisibility = false,
             buttonTitle = ""
         )
         hashMap[YFYAchievementType.SEND_MONEY.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_send_money_outside_yap_description),
             image = R.drawable.card_spare,
             buttonVisibility = false,
             buttonTitle = ""
@@ -140,31 +147,31 @@ class YapForYouManager {
         val hashMap: HashMap<String, YapForYouDataModel> = HashMap()
         hashMap[YFYAchievementType.ORDER_VIRTUAL_CARD.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_order_virtual_card_description),
             image = R.drawable.card_spare,
             buttonVisibility = false,
             buttonTitle = ""
         )
         hashMap[YFYAchievementType.UPGRADE_TO_PRIME.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_upgrade_to_prime_description),
             image = R.drawable.card_spare,
-            buttonVisibility = false,
-            buttonTitle = ""
+            buttonVisibility = true,
+            buttonTitle = getString(context,Strings.screen_yfy_upgrade_to_prime_button_label)
         )
         hashMap[YFYAchievementType.GO_METAL.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_go_metal_description),
             image = R.drawable.card_spare,
-            buttonVisibility = false,
-            buttonTitle = ""
+            buttonVisibility = true,
+            buttonTitle = getString(context,Strings.screen_yfy_upgrade_to_prime_button_label)
         )
         hashMap[YFYAchievementType.SET_UP_MULTI_CURRENCY.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_set_multi_currency_account_description),
             image = R.drawable.card_spare,
-            buttonVisibility = false,
-            buttonTitle = ""
+            buttonVisibility = true,
+            buttonTitle = getString(context,Strings.screen_yfy_set_multi_currency_account_button_label)
         )
         return hashMap
     }
@@ -173,28 +180,28 @@ class YapForYouManager {
         val hashMap: HashMap<String, YapForYouDataModel> = HashMap()
         hashMap[YFYAchievementType.GET_YOUNG.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_get_yap_young_description),
             image = R.drawable.card_spare,
             buttonVisibility = false,
-            buttonTitle = ""
+            buttonTitle = getString(context,Strings.screen_yfy_get_yap_young_button_label)
         )
         hashMap[YFYAchievementType.SIGN_UP_TO_HOUSEHOLD.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_signup_to_hh_description),
             image = R.drawable.card_spare,
             buttonVisibility = false,
-            buttonTitle = ""
+            buttonTitle = getString(context,Strings.screen_yfy_signup_to_hh_button_label)
         )
         hashMap[YFYAchievementType.MISSION_ON_YOUNG.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_set_a_mission_description),
             image = R.drawable.card_spare,
             buttonVisibility = false,
             buttonTitle = ""
         )
         hashMap[YFYAchievementType.PAY_YOU_HELP.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_pay_your_help_description),
             image = R.drawable.card_spare,
             buttonVisibility = false,
             buttonTitle = ""
@@ -206,21 +213,21 @@ class YapForYouManager {
         val hashMap: HashMap<String, YapForYouDataModel> = HashMap()
         hashMap[YFYAchievementType.INVITE_TEN_FRIENDS.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_invite_ten_friends_description),
             image = R.drawable.card_spare,
-            buttonVisibility = false,
-            buttonTitle = ""
+            buttonVisibility = true,
+            buttonTitle = getString(context,Strings.screen_yfy_invite_ten_friends_button_label)
         )
         hashMap[YFYAchievementType.SPEND_THOUSAND_AED.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_spend_thousand_description),
             image = R.drawable.card_spare,
             buttonVisibility = false,
             buttonTitle = ""
         )
         hashMap[YFYAchievementType.COMPLETE_RENEWAL.type] = YapForYouDataModel(
             title = "",
-            description = "",
+            description = getString(context,Strings.screen_yfy_complete_renewal_description),
             image = R.drawable.card_spare,
             buttonVisibility = false,
             buttonTitle = ""
