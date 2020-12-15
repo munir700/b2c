b3c5dc9d9359b6b54e9927abaa0ce5903f7aecc5
habiba <habiba@digitify.com>
2020-12-15 17:54:14 +0500

    YM-9296 create custom recyclerview for shimmery effect and apply in send money section

--- yapcore/src/main/java/co/yap/widgets/skeletonlayout/ShimmerRecyclerView.kt
+++ yapcore/src/main/java/co/yap/widgets/skeletonlayout/ShimmerRecyclerView.kt
@@ -13,7 +13,6 @@ class ShimmerRecyclerView @JvmOverloads constructor(
         set(value) {
             field = value
             shimmerLayoutId?.let {
-
                 invalidate()
             }
         }
@@ -22,31 +21,39 @@ class ShimmerRecyclerView @JvmOverloads constructor(
         set(value) {
             field = value
             itemCount?.let {
-
                 invalidate()
             }
         }
 
-    private var  skeleton: Skeleton
+    var rvAdapter: RecyclerView.Adapter<out RecyclerView.ViewHolder>? = null
+        set(value) {
+            field = value
+            rvAdapter?.let {
+                rvAdapter?.registerAdapterDataObserver(MyDataObserverAdapter())
+                skeleton = applySkeleton(shimmerLayoutId, itemCount)
+                if(rvAdapter?.itemCount==0){
+                    skeleton.showSkeleton()
+                }
+                invalidate()
+            }
+        }
+
+    private lateinit var skeleton: Skeleton
 
     init {
         val attributes =
             context.theme.obtainStyledAttributes(attrs, R.styleable.ShimmerRecyclerview, 0, 0)
         shimmerLayoutId =
-            attributes.getInteger(R.styleable.ShimmerRecyclerview_shimmerLayoutId, shimmerLayoutId)
+            attributes.getResourceId(R.styleable.ShimmerRecyclerview_shimmerLayoutId, shimmerLayoutId)
         itemCount =
             attributes.getInteger(R.styleable.ShimmerRecyclerview_itemCount, itemCount)
-
-        skeleton = applySkeleton(shimmerLayoutId, itemCount)
-        adapter?.registerAdapterDataObserver(MyDataObserverAdapter())
-
         attributes.recycle()
     }
 
     inner class MyDataObserverAdapter: RecyclerView.AdapterDataObserver() {
         override fun onChanged() {
             super.onChanged()
-            if(adapter?.itemCount==0){
+            if(rvAdapter?.itemCount==0){
                 skeleton.showSkeleton()
             }else{
                 skeleton.showOriginal()
