553f263f0fd13f5ddae2da05707b45d5917cbe44
Tanveer Munir <tanveer.munir@digitify.com>
2020-12-21 17:26:37 +0500

    YM-8965 remove extra call

--- yap/src/main/java/co/yap/modules/dashboard/home/fragments/YapHomeFragment.kt
+++ yap/src/main/java/co/yap/modules/dashboard/home/fragments/YapHomeFragment.kt
@@ -284,7 +284,7 @@ class YapHomeFragment : YapDashboardChildFragment<IYapHome.ViewModel>(), IYapHom
                 }
 
                 R.id.ivSearch ->{
-                    launchActivity<AdditionalInfoActivity> {  }
+
                 }
             }
         })
