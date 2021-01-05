b79d304f3b6e34b223f14f5f3cef2d56998a58d5
sshabbir255 <sufyan@digitify.com>
2021-01-05 18:21:07 +0500

    YM-9422 Add freeze card animation.

--- yap/src/main/java/co/yap/modules/dashboard/more/yapforyou/achievementdetail/AchievementDetailFragment.kt
+++ yap/src/main/java/co/yap/modules/dashboard/more/yapforyou/achievementdetail/AchievementDetailFragment.kt
@@ -1,10 +1,13 @@
 package co.yap.modules.dashboard.more.yapforyou.achievementdetail
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import co.yap.BR
 import co.yap.R
+import co.yap.databinding.FragmentAchievementDetailsBinding
 import co.yap.modules.dashboard.more.main.activities.MoreActivity
 import co.yap.modules.dashboard.more.yapforyou.fragments.YapForYouBaseFragment
 import co.yap.modules.dashboard.yapit.addmoney.main.AddMoneyActivity
@@ -23,6 +26,17 @@ class AchievementDetailFragment : YapForYouBaseFragment<IAchievementDetail.ViewM
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         addObservers()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (viewModel.parentViewModel?.selectedAchievementTask?.title == YFYAchievementTaskType.FREEZE_UNFREEZE_CARD.title) {
+            val animationDrawable: AnimationDrawable =
+                getBindings().tvFreezeAnimationView.background as AnimationDrawable
+            animationDrawable.setEnterFadeDuration(1000)
+            animationDrawable.setExitFadeDuration(1500)
+            animationDrawable.start()
+        }
     }
 
     private val onClickObserver = Observer<Int> {
@@ -55,4 +69,7 @@ class AchievementDetailFragment : YapForYouBaseFragment<IAchievementDetail.ViewM
         super.onDestroyView()
         removeObservers()
     }
+
+    private fun getBindings(): FragmentAchievementDetailsBinding =
+        viewDataBinding as FragmentAchievementDetailsBinding
 }
