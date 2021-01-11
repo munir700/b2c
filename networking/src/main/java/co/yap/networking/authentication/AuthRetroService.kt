4a42c6baaf687d2c3be7a62f16eb80951c6ff26f
Gulwisha Ahmed <gulwisha.ahmed@digitify.com>
2021-01-11 18:29:15 +0500

    YM-10289 FCM Notification API Implementation

--- networking/src/main/java/co/yap/networking/authentication/AuthRetroService.kt
+++ networking/src/main/java/co/yap/networking/authentication/AuthRetroService.kt
@@ -1,8 +1,10 @@
 package co.yap.networking.authentication
 
 import co.yap.networking.authentication.requestdtos.LoginRequest
+import co.yap.networking.authentication.requestdtos.MsTokenRequest
 import co.yap.networking.authentication.requestdtos.TokenRefreshRequest
 import co.yap.networking.authentication.responsedtos.LoginResponse
+import co.yap.networking.authentication.responsedtos.MsTokenResponse
 import co.yap.networking.models.ApiResponse
 import retrofit2.Response
 import retrofit2.http.Body
@@ -26,6 +28,11 @@ interface AuthRetroService {
     // Logout
     @POST(AuthRepository.URL_LOGOUT)
     suspend fun logout(@Query("uuid") uuid: String): Response<ApiResponse>
+
+    @POST(AuthRepository.URL_MS_LOGIN_TOKEN)
+    suspend fun getMsToken(
+        @Body msTokenRequest : MsTokenRequest
+    ): Response<MsTokenResponse>
 
     //getJwtToken
     suspend fun getJwtToken(): String?
