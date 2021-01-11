0868881bd4f8f9edc84308c0574d75dbc3578bb1
Irfan <irfan@digitify.com>
2021-01-11 16:56:45 +0500

    get the FCM puch token from firebase and save in shared prefrence

--- yapcore/src/main/java/co/yap/yapcore/constants/Constants.kt
+++ yapcore/src/main/java/co/yap/yapcore/constants/Constants.kt
@@ -146,6 +146,7 @@ object Constants {
     const val KEY_THEME = "KEY_THEME"
     const val VERIFY_PASS_CODE_BTN_TEXT = "verify_pass_code_btn_text"
     const val KEY_IS_REMEMBER = "IS_REMEMBER"
+    const val KEY_FCM_TOKEN = "fcm_token"
 
     const val PAGE_URL = "_page_url"
     const val TOOLBAR_TITLE = "toolbar_title"
