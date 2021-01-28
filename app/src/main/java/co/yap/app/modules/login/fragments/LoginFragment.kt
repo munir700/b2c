2ef84dabcf20540a91468430b481920ebb0d20f5
Irfan <irfan@digitify.com>
2021-01-28 12:58:46 +0500

    no message

--- app/src/main/java/co/yap/app/modules/login/fragments/LoginFragment.kt
+++ app/src/main/java/co/yap/app/modules/login/fragments/LoginFragment.kt
@@ -97,8 +97,8 @@ class LoginFragment : MainChildFragment<ILogin.ViewModel>(), ILogin.View {
                     requireContext().getTestPayloadForSamsung { paylaod ->
                         val data = paylaod.toByteArray(StandardCharsets.UTF_8)
                         val finalPayload = EncodingUtils.base64Encode(data)
-//                        SamsungPayWalletManager.getInstance(requireContext())
-//                            .addYapCardToSamsungPay(finalPayload)
+                        SamsungPayWalletManager.getInstance(requireContext())
+                            .addYapCardToSamsungPay(finalPayload)
                     }
                 }
         }
