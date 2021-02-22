0914e12a8d65ece3f21c13664e7145b931dab43c
Faheem Riaz <faheem@digitify.com>
2021-02-22 13:18:53 +0500

    YM-11083 - Add a new check on Shipping stage active case

--- yap/src/main/java/co/yap/modules/dashboard/home/status/DashboardNotificationStatusHelper.kt
+++ yap/src/main/java/co/yap/modules/dashboard/home/status/DashboardNotificationStatusHelper.kt
@@ -174,7 +174,7 @@ class DashboardNotificationStatusHelper(
             when (stage) {
                 PaymentCardOnboardingStage.SHIPPING -> {
                     return (when {
-                        SessionManager.user?.partnerBankStatus == PartnerBankStatus.SIGN_UP_PENDING.status -> {
+                        SessionManager.user?.partnerBankStatus == PartnerBankStatus.SIGN_UP_PENDING.status || SessionManager.user?.partnerBankStatus == PartnerBankStatus.DOCUMENT_UPLOADED.status -> {
                             if (SessionManager.user?.notificationStatuses == NotificationStatus.FATCA_GENERATED.name) StageProgress.ACTIVE else StageProgress.INACTIVE
                         }
                         card.deliveryStatus == CardDeliveryStatus.ORDERED.name || card.deliveryStatus == CardDeliveryStatus.BOOKED.name || card.deliveryStatus == CardDeliveryStatus.SHIPPING.name -> {
