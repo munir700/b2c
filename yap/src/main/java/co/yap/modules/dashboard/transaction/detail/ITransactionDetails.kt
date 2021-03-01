6a1346c63ea7a31e39a1cdaa04a071c01a9ba30b
Gulwisha Ahmed <wishakhn@gmail.com>
2021-03-01 16:48:44 +0500

    YM-11147 UI Fixes

--- yap/src/main/java/co/yap/modules/dashboard/transaction/detail/ITransactionDetails.kt
+++ yap/src/main/java/co/yap/modules/dashboard/transaction/detail/ITransactionDetails.kt
@@ -17,8 +17,8 @@ import java.util.*
 
 interface ITransactionDetails {
     interface View : IBase.View<ViewModel> {
-      fun  setObservers()
-      fun  removeObservers()
+        fun setObservers()
+        fun removeObservers()
     }
 
     interface ViewModel : IBase.ViewModel<State> {
