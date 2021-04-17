f9e9df01e5746f358a0e7ec0ae7241f619aaca93
sshabbir255 <sufyan@digitify.com>
2021-04-18 03:01:42 +0500

    YM-12731 - Implement and integrate API for stop showing ranking message on WaitingListFragment.

--- networking/src/main/java/co/yap/networking/customers/responsedtos/WaitingRankingResponse.kt
+++ networking/src/main/java/co/yap/networking/customers/responsedtos/WaitingRankingResponse.kt
@@ -12,6 +12,8 @@ data class WaitingRankingResponse(
         @SerializedName("waitingBehind") var waitingBehind: String?,
         @SerializedName("jump") var jump: String?,
         @SerializedName("completedKyc") var completedKyc: Boolean?,
-        @SerializedName("waiting") var waiting: Boolean?
+        @SerializedName("waiting") var waiting: Boolean?,
+        @SerializedName("viewable") var viewable: Boolean?,
+        @SerializedName("gainPoints") var gainPoints: String?
     )
 }
