0db1f047f580037c593a4948f0f518216ad0de66
Najia Bokhari <najia@digitify.com>
2021-04-19 16:59:47 +0500

    YH-1122 update check from iban to fssRequestRefNo in order to fix the flow

--- app/src/main/java/co/yap/app/modules/login/fragments/VerifyPasscodeFragment.kt
+++ app/src/main/java/co/yap/app/modules/login/fragments/VerifyPasscodeFragment.kt
@@ -319,7 +319,7 @@ class VerifyPasscodeFragment : MainChildFragment<IVerifyPasscode.ViewModel>(), B
             trackEvents(this)
             trackEventWithScreenName(if (viewModel.isFingerprintLogin) FirebaseEvent.SIGN_IN_TOUCH else FirebaseEvent.SIGN_IN_PIN)
             if (!this.isWaiting) {
-                if (this.iban.isNullOrBlank()) {
+                if (this.fssRequestRefNo.isNullOrBlank()) {
                     startFragment(
                             fragmentName = ReachedTopQueueFragment::class.java.name,
                             clearAllPrevious = true
