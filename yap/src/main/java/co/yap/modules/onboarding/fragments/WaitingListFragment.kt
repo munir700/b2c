7f77382fc1823b781cbdf948f4f8ee3cc6dad143
Syed Tehrim Abbas <tehrim.abbas@digitify.com>
2021-04-19 15:49:46 +0500

    YM- 12671 - use gif view instead of videos. Convert videos to gif. - Update loadGifImageView method in ImageBinding.kt pass onAnimationComplete call back to check when the animation end. It will not effect the previous implementation.

--- yap/src/main/java/co/yap/modules/onboarding/fragments/WaitingListFragment.kt
+++ yap/src/main/java/co/yap/modules/onboarding/fragments/WaitingListFragment.kt
@@ -8,12 +8,9 @@ import co.yap.BR
 import co.yap.R
 import co.yap.modules.onboarding.interfaces.IWaitingList
 import co.yap.modules.onboarding.viewmodels.WaitingListViewModel
-import co.yap.widgets.video.ExoPlayerCallBack
 import co.yap.yapcore.BaseBindingFragment
+import co.yap.yapcore.helpers.ImageBinding
 import co.yap.yapcore.helpers.extentions.inviteFriendIntent
-import com.google.android.exoplayer2.Player
-import com.google.android.exoplayer2.source.TrackGroupArray
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray
 import kotlinx.android.synthetic.main.fragment_waiting_list.*
 
 class WaitingListFragment : BaseBindingFragment<IWaitingList.ViewModel>(), IWaitingList.View {
@@ -24,8 +21,6 @@ class WaitingListFragment : BaseBindingFragment<IWaitingList.ViewModel>(), IWait
     override val viewModel: IWaitingList.ViewModel
         get() = ViewModelProviders.of(this).get(WaitingListViewModel::class.java)
 
-    var firstVideoPlayed: Boolean = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setObservers()
@@ -33,7 +28,17 @@ class WaitingListFragment : BaseBindingFragment<IWaitingList.ViewModel>(), IWait
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setYapAnimation()
+        setupCoinAnimation()
+    }
+
+    private fun setupCoinAnimation() {
+        ImageBinding.loadGifImageView(ivYapAnimation, R.raw.wait_list_first, 1) {
+            ImageBinding.loadGifImageView(imageView = ivYapAnimation,
+                resource = R.raw.wait_list_second,
+                loopCount = 10000,
+                delayBetweenLoop = 0L) {
+            }
+        }
     }
 
     override fun setObservers() {
@@ -50,43 +55,5 @@ class WaitingListFragment : BaseBindingFragment<IWaitingList.ViewModel>(), IWait
 
     override fun removeObservers() {
         viewModel.clickEvent.removeObservers(this)
-    }
-
-    private fun setYapAnimation() {
-        andExoPlayerView.setSource(co.yap.yapcore.R.raw.waiting_list_first_part)
-        andExoPlayerView?.player?.repeatMode = Player.REPEAT_MODE_OFF
-        andExoPlayerView.setExoPlayerCallBack(object : ExoPlayerCallBack {
-            override fun onError() {
-                andExoPlayerView.setSource(co.yap.yapcore.R.raw.card_on_its_way)
-            }
-
-            override fun onTracksChanged(
-                trackGroups: TrackGroupArray,
-                trackSelections: TrackSelectionArray
-            ) {
-            }
-
-            override fun onPositionDiscontinuity(reason: Int) {}
-
-            override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
-                when (playbackState) {
-                    Player.STATE_READY -> {
-                        andExoPlayerView.visibility = View.VISIBLE
-                    }
-
-                    Player.STATE_ENDED -> {
-                        if (!firstVideoPlayed) {
-                            andExoPlayerView.visibility = View.INVISIBLE
-                            andExoPlayerView.setSource(co.yap.yapcore.R.raw.waiting_list_second_part)
-                            andExoPlayerView?.player?.repeatMode = Player.REPEAT_MODE_ALL
-                            firstVideoPlayed = true
-                        }
-                    }
-                }
-            }
-
-            override fun onRepeatModeChanged(repeatMode: Int) {
-            }
-        })
     }
 }
