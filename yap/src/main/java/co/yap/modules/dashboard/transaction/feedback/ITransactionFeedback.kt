8f554d2440c53ee4d1373d8c722e5095d9ac5e8e
Gulwisha Ahmed <wishakhn@gmail.com>
2021-04-22 11:35:19 +0500

    YM-12780 Feedback Screen

--- yap/src/main/java/co/yap/modules/dashboard/transaction/feedback/ITransactionFeedback.kt
+++ yap/src/main/java/co/yap/modules/dashboard/transaction/feedback/ITransactionFeedback.kt
@@ -2,11 +2,14 @@ package co.yap.modules.dashboard.transaction.feedback
 
 import co.yap.modules.dashboard.transaction.feedback.adaptor.TransactionFeedbackAdapter
 import co.yap.yapcore.IBase
+import co.yap.yapcore.SingleClickEvent
 
 interface ITransactionFeedback {
     interface View : IBase.View<ViewModel>
     interface ViewModel : IBase.ViewModel<State>{
         val adapter: TransactionFeedbackAdapter
+        fun handlePressOnView(id: Int)
+        var clickEvent: SingleClickEvent
     }
 
     interface State : IBase.State
