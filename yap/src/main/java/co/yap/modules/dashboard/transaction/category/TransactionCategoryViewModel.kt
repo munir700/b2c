53efd2bbf57e03574bd8546938886fcca930bf88
Gulwisha Ahmed <wishakhn@gmail.com>
2021-04-26 13:46:32 +0500

    YM-13126

--- yap/src/main/java/co/yap/modules/dashboard/transaction/category/TransactionCategoryViewModel.kt
+++ yap/src/main/java/co/yap/modules/dashboard/transaction/category/TransactionCategoryViewModel.kt
@@ -2,7 +2,6 @@ package co.yap.modules.dashboard.transaction.category
 
 import android.app.Activity
 import android.app.Application
-import android.content.Context
 import android.content.Intent
 import androidx.databinding.ObservableField
 import co.yap.networking.models.RetroApiResponse
@@ -11,8 +10,6 @@ import co.yap.networking.transactions.responsedtos.transaction.TapixCategory
 import co.yap.yapcore.BaseViewModel
 import co.yap.yapcore.SingleClickEvent
 import co.yap.yapcore.constants.Constants
-import co.yap.yapcore.leanplum.trackEventWithAttributes
-import co.yap.yapcore.managers.SessionManager
 
 class TransactionCategoryViewModel(application: Application) :
     BaseViewModel<ITransactionCategory.State>(application), ITransactionCategory.ViewModel {
@@ -44,13 +41,15 @@ class TransactionCategoryViewModel(application: Application) :
             state.loading = true
             when (val response = repository.getAllTransactionCategories()) {
                 is RetroApiResponse.Success -> {
-                    response.data.txnCategories?.let { tapixCategories.addAll(it)
+                    response.data.txnCategories?.let {
+                        tapixCategories.addAll(it)
                         tapixCategories.find { list ->
                             list.categoryName == categoryName.get()
                         }.also { category ->
                             category?.isSelected = true
+                            selectedCategory.set(category)
                         }
-                    categoryAdapter.setList(tapixCategories)
+                        categoryAdapter.setList(tapixCategories)
                     }
                     state.loading = false
                 }
@@ -60,33 +59,46 @@ class TransactionCategoryViewModel(application: Application) :
             }
         }
     }
-    override fun updateCategory(context: Activity){
-         launch {
-             state.loading = true
-             when (val response = repository.updateTransactionCategory(selectedCategory.get()?.id.toString(),transactionId = transactionId.get()?:"")) {
-                 is RetroApiResponse.Success -> {
-                     val intent = Intent()
-                     intent.putExtra(Constants.UPDATED_CATEGORY, selectedCategory.get())
-                     context.setResult(Activity.RESULT_OK,intent)
-                     context.finish()
-                     state.loading = false
-                 }
-                 is RetroApiResponse.Error -> {
-                     state.toast = response.error.message
-                     state.loading = false
-                 }
-             }
-         }
-     }
 
-    override fun selectCategory(data: TapixCategory, position : Int) {
+    override fun updateCategory(context: Activity) {
+        launch {
+            state.loading = true
+            when (val response = repository.updateTransactionCategory(
+                selectedCategory.get()?.id.toString(),
+                transactionId = transactionId.get() ?: ""
+            )) {
+                is RetroApiResponse.Success -> {
+                    val intent = Intent()
+                    intent.putExtra(Constants.UPDATED_CATEGORY, selectedCategory.get())
+                    context.setResult(Activity.RESULT_OK, intent)
+                    context.finish()
+                    state.loading = false
+                }
+                is RetroApiResponse.Error -> {
+                    state.toast = response.error.message
+                    state.loading = false
+                }
+            }
+        }
+    }
+
+    override fun setPreSelectedCategory(name: String) {
+        categoryName.set(name)
+        tapixCategories.find {
+            it.categoryName == name
+        }.also {
+            selectedCategory.set(it)
+        }
+    }
+
+    override fun selectCategory(data: TapixCategory, position: Int) {
         categoryAdapter.getDataList().find {
             it.isSelected
         }.also {
             it?.isSelected = false
         }
         categoryAdapter.getDataList()[position].isSelected = true
-        selectedCategory.set(categoryAdapter.getDataList()[position]).also{
+        selectedCategory.set(categoryAdapter.getDataList()[position]).also {
             selectedCategory.get()?.type = categoryAdapter.getDataList()[position].isGeneral
         }
         categoryAdapter.notifyDataSetChanged()
