3cfe548b9f8ede5733ebef80e585a55d84359fac
Mughees <mughees.akram@digitify.com>
2021-06-02 13:15:12 +0500

    YH-1251 - isOnBoarding() function has now user parameter

--- yapcore/src/main/java/co/yap/yapcore/managers/SessionManager.kt
+++ yapcore/src/main/java/co/yap/yapcore/managers/SessionManager.kt
@@ -170,8 +170,8 @@ object SessionManager : IRepositoryHolder<CardsRepository> {
 
     fun getCurrentUser(): AccountInfo? {
         return (if (isExistingUser()) {
-            user = getHouseholdUser()
-            if (isOnBoarded()) {
+//            user = getHouseholdUser()
+            if (isOnBoarded(getHouseholdUser())) {
                 getYapUser()
             } else
                 getHouseholdUser()
@@ -201,7 +201,7 @@ object SessionManager : IRepositoryHolder<CardsRepository> {
     /*
          isOnBoarded:  This method is used for Household user to check if it's on boarded or not
       */
-    fun isOnBoarded(): Boolean {
+    fun isOnBoarded(user : AccountInfo?): Boolean {
         // TODO Verify login with IOS team
         if (user?.notificationStatuses != AccountStatus.PARNET_MOBILE_VERIFICATION_PENDING.name &&
             user?.notificationStatuses != AccountStatus.INVITE_PENDING.name &&
