ce51ec3b1bdb9e64bc91993aec9cf261d3c6eddd
Muhammad Umar Saddique <umar.saddique@digitify.com>
2021-07-28 13:03:41 +0500

    postpaid bills  Due grey text
    show prepaid instead of paid in my bills prepaid case
    
    Add segment of Flagsmith
    
    Add - in Analytical screen
    
    Fix sorting issue in my bills
    
    Fix other UI

--- yapcore/src/main/java/co/yap/yapcore/enums/BillStatus.kt
+++ yapcore/src/main/java/co/yap/yapcore/enums/BillStatus.kt
@@ -3,6 +3,6 @@ package co.yap.yapcore.enums
 enum class BillStatus(val title: String) {
     OVERDUE("Overdue"),
     BILL_DUE("Bill Due"),
-    PAID("paid"),
+    PAID("Paid"),
     PREPAID("Prepaid")
 }
