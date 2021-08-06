9711383587f320061a70d1f4a882e9e0b3e447d1
Gulwisha Ahmed <wishakhn@gmail.com>
2021-08-06 10:37:32 +0500

    YM-15371 UI Creation -> fragment_edit_card_name.xml -> card_item_view.xml

--- yap/src/main/java/co/yap/modules/kyc/interfaces/IEditCardName.kt
+++ yap/src/main/java/co/yap/modules/kyc/interfaces/IEditCardName.kt
@@ -1,9 +1,13 @@
 package co.yap.modules.kyc.interfaces
 
 import co.yap.yapcore.IBase
+import co.yap.yapcore.SingleClickEvent
 
 interface IEditCardName {
     interface State : IBase.State
-    interface ViewModel : IBase.ViewModel<State>
+    interface ViewModel : IBase.ViewModel<State>{
+        fun handleOnPressView(id: Int)
+        var clickEvent: SingleClickEvent
+    }
     interface View : IBase.View<ViewModel>
 }
