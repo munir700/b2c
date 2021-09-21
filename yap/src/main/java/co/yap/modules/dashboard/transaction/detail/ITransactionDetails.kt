ed79be41548c4876dc3cd684b15bbe67b581b83d
MuhammadUmarSaddique <qwerty@123456>
2021-09-21 10:47:25 +0500

    Merge Dev into features/bill payments 8-A-2

--- yap/src/main/java/co/yap/modules/dashboard/transaction/detail/ITransactionDetails.kt
+++ yap/src/main/java/co/yap/modules/dashboard/transaction/detail/ITransactionDetails.kt
@@ -1,8 +1,11 @@
 package co.yap.modules.dashboard.transaction.detail
 
+import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.databinding.ObservableBoolean
 import androidx.databinding.ObservableField
 import androidx.databinding.ObservableInt
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import co.yap.modules.dashboard.transaction.detail.adaptor.TransactionDetailItemAdapter
 import co.yap.modules.dashboard.transaction.detail.composer.TransactionDetailComposer
@@ -17,6 +20,7 @@ import co.yap.widgets.CoreCircularImageView
 import co.yap.widgets.bottomsheet.BottomSheetItem
 import co.yap.yapcore.IBase
 import co.yap.yapcore.SingleClickEvent
+import com.google.android.gms.maps.GoogleMap
 
 interface ITransactionDetails {
     interface View : IBase.View<ViewModel> {
@@ -30,6 +34,7 @@ interface ITransactionDetails {
         var transaction: ObservableField<Transaction>
         var adapter: TransactionReceiptAdapter
         var responseReciept: MutableLiveData<ArrayList<String>>
+        var gMap: GoogleMap?
         fun deleteReceipt(position: Int)
         fun getReceiptTitle(list: List<ReceiptModel>): String
         fun getAddReceiptOptions(): ArrayList<BottomSheetItem>
@@ -42,7 +47,15 @@ interface ITransactionDetails {
         var itemsComposer: TransactionDetailComposer
         var transactionAdapter: TransactionDetailItemAdapter
         var totalPurchase: ObservableField<TotalPurchases>
-        fun setMerchantImage(view : CoreCircularImageView)
+        fun setMerchantImage(view: CoreCircularImageView)
+        fun setContentDataColor(
+            transaction: Transaction?,
+            tvTotalAmountValue: AppCompatTextView,
+            tvCurrency: AppCompatTextView
+        )
+
+        fun setMap()
+        fun setMapVisibility(ivMap: ImageView, map: Fragment, isShowMap: Boolean)
     }
 
     interface State : IBase.State {
--- yap/src/main/java/co/yap/modules/dashboard/transaction/detail/models/TransactionDetail.kt
+++ yap/src/main/java/co/yap/modules/dashboard/transaction/detail/models/TransactionDetail.kt
@@ -22,7 +22,8 @@ data class TransactionDetail(
     val categoryType: Boolean?,
     val categoryDescription: String?,
     val tapixCategory: TapixCategory?,
-    val showFeedBack: Boolean?
+    val showFeedBack: Boolean?,
+    val isYTYTransfer: Boolean? = false,
+    val isDeclinedTransaction: Boolean? = false,
+    val isMApVisible: Boolean? = false
 )
-
-
