db684b724c664eaac98f682ba6a671fe3d19c055
MuhammadUmarSaddique <qwerty@123456>
2021-09-29 11:56:33 +0500

    YM-15693 -Capitalize the first letter in the nickname field and allow special characters during add bill -Nickname is not a mandatory field now

--- billpayments/src/main/java/co/yap/billpayments/addbiller/addbillerdetail/AddBillerDetailViewModel.kt
+++ billpayments/src/main/java/co/yap/billpayments/addbiller/addbillerdetail/AddBillerDetailViewModel.kt
@@ -66,8 +66,8 @@ class AddBillerDetailViewModel(application: Application) :
     val listener = object : OnItemClickListener {
         override fun onItemClick(view: View, data: Any, pos: Int) {
             when (view.id) {
-                R.id.etUserInput, R.id.etNickName -> {
-                    if (validate() && state.nickNameValue.get()?.length ?: 0 > 1) {
+                R.id.etUserInput -> {
+                    if (validate()) {
                         state.valid.set(true)
                     } else {
                         state.valid.set(false)
