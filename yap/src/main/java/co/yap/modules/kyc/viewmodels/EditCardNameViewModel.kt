04fcc0c7a1aae3bc6eeb105b6e2549d1b93237d8
Gulwisha Ahmed <wishakhn@gmail.com>
2021-10-13 13:37:00 +0500

    Merge branch 'dev' into Stage/Support-Core-Merged-Sep-2021
    # Conflicts:
    #	app/src/main/cpp/native-lib.cpp

--- yap/src/main/java/co/yap/modules/kyc/viewmodels/EditCardNameViewModel.kt
+++ yap/src/main/java/co/yap/modules/kyc/viewmodels/EditCardNameViewModel.kt
@@ -30,7 +30,7 @@ class EditCardNameViewModel(application: Application) :
             state.loading = true
             when (val response = repository.updateCardName(
                 CardNameRequest(
-                    customerIDNumber = SessionManager.user?.currentCustomer?.customerId,
+                    customerIDNumber = parentViewModel?.identity?.citizenNumber,
                     customerNationality = parentViewModel?.state?.nationality?.get(),
                     customerIDFirstName = parentViewModel?.state?.firstName?.get(),
                     customerIDLastName = parentViewModel?.state?.lastName?.get(),
@@ -40,7 +40,7 @@ class EditCardNameViewModel(application: Application) :
                 )
             )) {
                 is RetroApiResponse.Success -> {
-                    SessionManager.getDebitCard{
+                    SessionManager.getDebitCard {
                         success(true)
                         state.loading = false
                     }
