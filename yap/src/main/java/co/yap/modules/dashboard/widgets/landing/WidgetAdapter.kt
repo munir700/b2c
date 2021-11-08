e682bea48d4cf3ee749436e2904785e6a32223d6
Irfan Arshad <irfan@digitify.com>
2021-11-08 16:24:24 +0500

    drag and swipe both work

--- yap/src/main/java/co/yap/modules/dashboard/widgets/landing/WidgetAdapter.kt
+++ yap/src/main/java/co/yap/modules/dashboard/widgets/landing/WidgetAdapter.kt
@@ -59,7 +59,25 @@ class WidgetAdapter(mValue: MutableList<WidgetData>, navigation: NavController?)
 
     override fun getViewModel(viewType: Int) = WidgetLandingItemViewModel()
     override fun getVariableId() = BR.viewModel
-    override fun onCheckCanStartDrag(holder: ViewHolder, position: Int, x: Int, y: Int) = editWidget
+    override fun onCheckCanStartDrag(holder: ViewHolder, position: Int, x: Int, y: Int):Boolean
+    {
+       val  dragHandleView =  holder.binding.image
+        val containerView = holder.binding.constraintLayout
+        val offsetX: Int = containerView.left + (containerView.translationX + 0.5f) as Int
+        val offsetY: Int = containerView.top + (containerView.translationY + 0.5f) as Int
+        return hitTest(dragHandleView, x - offsetX, y - offsetY)
+    }
+    fun hitTest( v:View,  x:Int,  y:Int):Boolean {
+        val tx = (v.translationX + 0.5f).toInt()
+        val ty = (v.translationY + 0.5f).toInt()
+        val left = v.left + tx
+        val right = v.right + tx
+        val top = v.top + ty
+        val bottom = v.bottom + ty
+
+        return (x in left..right && y >= top && y <= bottom)
+
+    }
     override fun onGetItemDraggableRange(holder: ViewHolder, position: Int): Nothing? = null
     override fun onMoveItem(fromPosition: Int, toPosition: Int) {
         if (fromPosition == toPosition) {
