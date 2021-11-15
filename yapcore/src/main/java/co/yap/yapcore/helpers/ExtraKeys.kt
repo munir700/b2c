55fb1801000ed4df66a07ea4299bcf0f614099f9
Farooq007 <muhammad.farooq@digitify.com>
2021-11-15 15:47:50 +0500

    YM-14014 Remove space

--- yapcore/src/main/java/co/yap/yapcore/helpers/ExtraKeys.kt
+++ yapcore/src/main/java/co/yap/yapcore/helpers/ExtraKeys.kt
@@ -1,6 +1,6 @@
 package co.yap.yapcore.helpers
 
-enum class ExtraKeys {
+enum class ExtraKeys{
     TRANSACTION_OBJECT_STRING,
     TRANSACTION_OBJECT_CHILD_POSITION,
     TRANSACTION_OBJECT_GROUP_POSITION,
