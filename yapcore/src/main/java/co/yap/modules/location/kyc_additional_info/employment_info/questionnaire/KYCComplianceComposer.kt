35fe2d2e4944eb1fb9dc0fcdf58630fa8f9bfb1e
Fahad Nasrullah <fahad.nasrullah@digitify.com>
2021-11-24 11:01:34 +0500

    YM-16115 - Status renamed in KYCComplianceComposer.kt

--- yapcore/src/main/java/co/yap/modules/location/kyc_additional_info/employment_info/questionnaire/KYCComplianceComposer.kt
+++ yapcore/src/main/java/co/yap/modules/location/kyc_additional_info/employment_info/questionnaire/KYCComplianceComposer.kt
@@ -4,7 +4,6 @@ import androidx.databinding.ObservableField
 import co.yap.modules.location.kyc_additional_info.employment_info.questionnaire.enums.QuestionType
 import co.yap.modules.location.kyc_additional_info.employment_info.questionnaire.models.Question
 import co.yap.modules.location.kyc_additional_info.employment_info.questionnaire.models.QuestionUiFields
-import co.yap.networking.customers.responsedtos.employment_amendment.EmploymentInfoAmendmentResponse
 import co.yap.yapcore.enums.EmploymentQuestionIdentifier
 import co.yap.yapcore.enums.EmploymentStatus
 import co.yap.yapcore.enums.EmploymentStatus.*
@@ -14,7 +13,8 @@ interface ComplianceQuestionsItemsComposer {
     fun compose(employmentStatus: EmploymentStatus): ArrayList<QuestionUiFields>
 }
 
-class KYCComplianceComposer(val response : co.yap.networking.customers.responsedtos.employment_amendment.EmploymentStatus?) : ComplianceQuestionsItemsComposer {
+class KYCComplianceComposer(val status: co.yap.networking.customers.responsedtos.employment_amendment.EmploymentStatus?) :
+    ComplianceQuestionsItemsComposer {
     override fun compose(employmentStatus: EmploymentStatus): ArrayList<QuestionUiFields> {
         return when (employmentStatus) {
             EMPLOYED -> arrayListOf(
@@ -23,7 +23,7 @@ class KYCComplianceComposer(val response : co.yap.networking.customers.responsed
                         questionTitle = "Tell us where you work?",
                         placeholder = "Employer name",
                         questionType = QuestionType.EDIT_TEXT_FIELD,
-                        answer = ObservableField(response?.employerName ?: "")
+                        answer = ObservableField(status?.employerName ?: "")
                     )
                 ),
                 QuestionUiFields(
@@ -31,7 +31,7 @@ class KYCComplianceComposer(val response : co.yap.networking.customers.responsed
                         questionTitle = "What is your monthly salary? Don’t worry there is no minimum salary requirement.",
                         placeholder = "Enter the amount",
                         questionType = QuestionType.EDIT_TEXT_FIELD_WITH_AMOUNT,
-                        answer = ObservableField(response?.monthlySalary ?: "")
+                        answer = ObservableField(status?.monthlySalary ?: "")
                     ),
                     key = EmploymentQuestionIdentifier.SALARY_AMOUNT
                 ), QuestionUiFields(
@@ -39,7 +39,7 @@ class KYCComplianceComposer(val response : co.yap.networking.customers.responsed
                         questionTitle = "How much cash do you plan to deposit or receive monthly in a cash deposit machine (ATM)? If you don’t deal with cash, then enter AED 0.00",
                         placeholder = "Enter the amount",
                         questionType = QuestionType.EDIT_TEXT_FIELD_WITH_AMOUNT,
-                        answer = ObservableField(response?.expectedMonthlyCredit ?: "")
+                        answer = ObservableField(status?.expectedMonthlyCredit ?: "")
                     ),
                     key = EmploymentQuestionIdentifier.DEPOSIT_AMOUNT
                 )
