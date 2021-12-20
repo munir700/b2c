894e167d2f614430f754bef867569e7535084a17
wisha Khan <gulwisha.ahmed@digitify.com>
2021-12-20 11:02:51 +0500

    YM-16721 Topup System Para meters updated

--- networking/src/main/java/co/yap/networking/transactions/responsedtos/FundTransferLimits.kt
+++ networking/src/main/java/co/yap/networking/transactions/responsedtos/FundTransferLimits.kt
@@ -2,11 +2,8 @@ package co.yap.networking.transactions.responsedtos
 
 import com.google.gson.annotations.SerializedName
 
-class FundTransferLimits(
-    @SerializedName("minLimit") val minLimit: String,
+class FundTransferLimits(@SerializedName("minLimit") val minLimit: String,
     @SerializedName("maxLimit") val maxLimit: String,
     @SerializedName("active") val active: Boolean,
-    @SerializedName("dailyAccumulativeMinLimit") val dailyAccumulativeMinLimit: String?,
-    @SerializedName("dailyAccumulativeMaxLimit") val dailyAccumulativeMaxLimit: String?,
-    @SerializedName("remainingAvailableLimit") val remainingAvailableLimit: String?
-)
+    @SerializedName("dailyAccumulativeMaxTopUpLimit") val dailyAccumulativeMaxLimit: String?,
+    @SerializedName("remainingDailyAccumulativeTopUpLimit") val remainingAvailableLimit: String?)
