7b3248c371db88617b7822e9f4a6bd7ca626bb1d
saifi369 <=>
2022-01-06 18:03:52 +0500

    YM-17043
    -> IdentityScannerActivity.java updated to use commitAllowingStateLoss() instead of commit()
    -> If EID scan is completed while application was in background, commit() will throw exception because activity is in stopped state and onSaveInstanceState() has already been called.
    -> commitAllowingStateLoss() allows this fragment transaction to complete without throwing an exception.

--- identityscanner_v2/src/main/java/com/digitify/identityscanner/docscanner/activities/IdentityScannerActivity.java
+++ identityscanner_v2/src/main/java/com/digitify/identityscanner/docscanner/activities/IdentityScannerActivity.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -63,7 +65,7 @@ public class IdentityScannerActivity extends DefaultActivity implements IIdentit
 
     protected void showFragment(Fragment fragment) {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment_holder, fragment).commit();
+        ft.replace(R.id.fragment_holder, fragment).commitAllowingStateLoss();
     }
 
     @Override
