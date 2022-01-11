5f651aee56a727493613b86364c8c9c07b967dcb
saifi369 <=>
2022-01-11 10:30:11 +0500

    YM-17020
    YapHomeFragment.kt updated access the view model only if the fragment is attached.

--- identityscanner_v2/src/main/java/com/digitify/identityscanner/docscanner/activities/IdentityScannerActivity.java
+++ identityscanner_v2/src/main/java/com/digitify/identityscanner/docscanner/activities/IdentityScannerActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
