b8e7826d7c1f93c85d4515f789a51efa77894f71
Breera <breera.hanif@digitify.com>
2022-01-17 11:21:28 +0500

    YAP-Feedback-Sprint-24 Added missing map code, which was skipped by merging

--- yap/src/main/java/co/yap/modules/dashboard/transaction/detail/TransactionDetailsActivity.kt
+++ yap/src/main/java/co/yap/modules/dashboard/transaction/detail/TransactionDetailsActivity.kt
@@ -2,7 +2,6 @@ package co.yap.modules.dashboard.transaction.detail
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Paint
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -30,17 +29,20 @@ import co.yap.yapcore.constants.Constants
 import co.yap.yapcore.constants.Constants.FILE_PATH
 import co.yap.yapcore.constants.RequestCodes
 import co.yap.yapcore.enums.PhotoSelectionType
-import co.yap.yapcore.enums.TransactionStatus
 import co.yap.yapcore.enums.TxnType
 import co.yap.yapcore.helpers.DateUtils
 import co.yap.yapcore.helpers.ExtraKeys
 import co.yap.yapcore.helpers.extentions.*
 import co.yap.yapcore.helpers.showReceiptSuccessDialog
 import co.yap.yapcore.interfaces.OnItemClickListener
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import kotlinx.android.synthetic.main.activity_transaction_details.*
 import pl.aprilapps.easyphotopicker.MediaFile
 
 class TransactionDetailsActivity : BaseBindingImageActivity<ITransactionDetails.ViewModel>(),
-    ITransactionDetails.View {
+    ITransactionDetails.View, OnMapReadyCallback {
 
     override fun getBindingVariable(): Int = BR.viewModel
     override fun getLayoutId(): Int = R.layout.activity_transaction_details
@@ -50,7 +52,17 @@ class TransactionDetailsActivity : BaseBindingImageActivity<ITransactionDetails.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setObservers()
-        setContentDataColor(viewModel.transaction.get())
+        viewModel.setContentDataColor(
+            viewModel.transaction.get(),
+            getBindings().tvTotalAmountValue,
+            getBindings().tvCurrency
+        )
+        viewModel.state.transactionData.get()?.isMApVisible?.let { showMAp ->
+            if (showMAp) initMap()
+            viewModel.setMapVisibility(
+                getBindings().ivMap, map, showMAp
+            )
+        }
     }
 
     override fun setObservers() {
@@ -203,14 +215,6 @@ class TransactionDetailsActivity : BaseBindingImageActivity<ITransactionDetails.
         )
     }
 
-    private fun setContentDataColor(transaction: Transaction?) {
-        //strike-thru textview
-        transaction?.let {
-            getBindings().tvTotalAmountValue.paintFlags =
-                if (transaction.isTransactionRejected() || transaction.status == TransactionStatus.FAILED.name) getBindings().tvTotalAmountValue.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG else 0
-        }
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK) {
@@ -312,4 +316,14 @@ class TransactionDetailsActivity : BaseBindingImageActivity<ITransactionDetails.
         )
     }
 
+    private fun initMap() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.view?.visibility = View.VISIBLE
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        viewModel.gMap = googleMap
+        viewModel.setMap()
+    }
 }
