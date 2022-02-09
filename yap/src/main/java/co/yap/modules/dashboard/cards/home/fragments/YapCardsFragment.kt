963a35d463c764ac1c0494482201440b30553368
Breera <breera.hanif@digitify.com>
2022-02-09 14:54:45 +0500

    YM-17278 Upadte count after removing a card

--- yap/src/main/java/co/yap/modules/dashboard/cards/home/fragments/YapCardsFragment.kt
+++ yap/src/main/java/co/yap/modules/dashboard/cards/home/fragments/YapCardsFragment.kt
@@ -359,7 +359,10 @@ class YapCardsFragment : YapDashboardChildFragment<IYapCards.ViewModel>(), IYapC
                     when {
                         true == removed -> {
                             viewModel.removeCard(updatedCard)
-                        }
+                           viewModel.state.cardIndicator.set(
+                                "${viewPager2.currentItem.plus(1)} of ${viewModel.state.totalCardsCount.get()}")
+
+                       }
                         true == cardBlocked -> {
                             viewModel.adapter.removeAllItems()
                             viewModel.getCards()
@@ -399,7 +402,7 @@ class YapCardsFragment : YapDashboardChildFragment<IYapCards.ViewModel>(), IYapC
                             card = paymentCard
                         )
                         viewModel.getCards()
-                        SessionManager.updateCardBalance{}
+                        SessionManager.updateCardBalance {}
                     }
                 }
             }
