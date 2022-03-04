6e614096a913c32c3e7cc19e13abcd68dcc45486
Irfan Arshad <irfan@digitify.com>
2022-03-04 18:29:07 +0500

    YSD-3948 Crash fix

--- yapcore/src/main/java/co/yap/countryutils/country/Country.kt
+++ yapcore/src/main/java/co/yap/countryutils/country/Country.kt
@@ -61,7 +61,7 @@ data class Country(
                     break
                 }
             }
-            if (currency == null) {
+            if (currency == null && supportedCurrencies?.isNotEmpty()==true) {
                 val c = supportedCurrencies!![0]
                 // find currency from utils with flag and symbol etc
                 currency =
