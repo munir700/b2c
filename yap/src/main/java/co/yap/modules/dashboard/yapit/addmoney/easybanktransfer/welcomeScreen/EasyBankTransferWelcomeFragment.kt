f0d95c6a20c7a8435f234b4af22462c40d850a19
Breera <breera.hanif@digitify.com>
2022-04-01 15:27:37 +0500

    17543 set lean builder

--- yap/src/main/java/co/yap/modules/dashboard/yapit/addmoney/easybanktransfer/welcomeScreen/EasyBankTransferWelcomeFragment.kt
+++ yap/src/main/java/co/yap/modules/dashboard/yapit/addmoney/easybanktransfer/welcomeScreen/EasyBankTransferWelcomeFragment.kt
@@ -1,14 +1,17 @@
 package co.yap.modules.dashboard.yapit.addmoney.easybanktransfer.welcomeScreen
 
 import android.os.Bundle
+import androidx.lifecyacle.ViewModelProvider
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import co.yap.BR
 import co.yap.R
+import co.yap.app.YAPApplication
 import co.yap.databinding.FragmentEasyBankTransferWelcomeBinding
 import co.yap.modules.dashboard.yapit.addmoney.easybanktransfer.banklist.BankListFragment
 import co.yap.modules.dashboard.yapit.addmoney.main.AddMoneyBaseFragment
 import co.yap.yapcore.helpers.extentions.startFragment
+import me.leantech.link.android.Lean
 
 class EasyBankTransferWelcomeFragment : AddMoneyBaseFragment<IEasyBankTransferWelcome.ViewModel>(),
     IEasyBankTransferWelcome.View {
@@ -23,6 +26,13 @@ class EasyBankTransferWelcomeFragment : AddMoneyBaseFragment<IEasyBankTransferWe
         super.onCreate(savedInstanceState)
         setObservers()
         viewModel.setDataFormat()
+        val lean: Lean = Lean.Builder()
+            // .setAppToken("eyJraWQiOiI3OTE3YzAyNC0xN2M1LTRjOWYtYWI1OC05ZmFjZWI2Njg2NzYiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJqYXZhQHlhcC5jb218ZGFjZTk5N2ItYmUxNy00YmY4LWEyMmYtNmVkOTk0ZDY3YmNmIiwiYXVkIjoiZTNjN2RiYWQtZjE3Zi00OGY2LTkxOTMtNTc2OGNkYjQ2NmU4IiwiaXNzIjoiaHR0cHM6Ly9zZWxmLWlzc3VlZC5tZSIsImV4cCI6MTY0ODgwNzk2MiwiaWF0IjoxNjQ4ODA2NzYyLCJqdGkiOiI4NjNlMTZiNy04YjRkLTQ4MmUtYWUxMy1hZmI1MWZjNjVmMzgiLCJ1dCI6Ik1PQklMRV9BUFAifQ.MTiKc8ojY9ljtfXMESD6UXCpn4HDDlkJvhyo32zzqC0NmMrVILonEhtjIGw5RCX1V2wp22RZ3-ZKN3pyABrvZjQ_v8I9B9sgHBdIcu_6qzl3X_nPDoX_9baMUMtFaUTxYVyF9Rdz8VTkrSIyr425bPyjeoxkZ6heyUMhNHeqe9kSCtU4cEjISFJ9NgXG88tGgk2KXbS7fQlQ75d90VN4q0xW9HK8ePGK6ivxi6g515kOpuSWamXvI3h-PdJyGW3VTt5XslIrsHqnWdQxLmgLG_vmHlylicMWKzFSed9Yc7C3xVYe_wi5JIhsPaUWFDjjyB_n6MMBqRF04o6ZMyruGA")
+            // .setAppToken(CookiesManager.jwtToken?:"")
+            .setVersion(YAPApplication.configManager?.versionCode ?: "")
+            .showLogs()
+            .sandboxMode(true)
+            .build()
     }
 
     override fun setObservers() {
