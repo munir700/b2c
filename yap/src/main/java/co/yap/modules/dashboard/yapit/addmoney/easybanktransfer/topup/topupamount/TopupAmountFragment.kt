889fbf0a94c1801962a8863dbae855bf3215ba38
mhd_Aamir046 <muhammad.aamir@digitify.com>
2022-04-07 13:27:21 +0500

    YM-17552 PR comments resolved

--- yap/src/main/java/co/yap/modules/dashboard/yapit/addmoney/easybanktransfer/topup/topupamount/TopupAmountFragment.kt
+++ yap/src/main/java/co/yap/modules/dashboard/yapit/addmoney/easybanktransfer/topup/topupamount/TopupAmountFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import co.yap.BR
 import co.yap.R
+import co.yap.databinding.FragmentEasyBankTransferBankListBinding
 import co.yap.databinding.FragmentTopupAmountBinding
 import co.yap.modules.dashboard.yapit.addmoney.main.AddMoneyBaseFragment
 import co.yap.yapcore.helpers.extentions.generateChipViews
@@ -33,5 +34,5 @@ class TopupAmountFragment : AddMoneyBaseFragment<ITopupAmount.ViewModel>(),
         )
     }
 
-    private fun getBinding() = viewDataBinding as FragmentTopupAmountBinding
+    private fun getBinding() = getDataBindingView<FragmentTopupAmountBinding>()
 }
